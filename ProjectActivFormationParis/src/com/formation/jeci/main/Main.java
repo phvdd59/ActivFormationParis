d92384b8c3933d949824eb6858809ee2dca5a84c
ACTIVjeci <jeremyciamous@gmail.com>
2016-07-18 17:49:42 +0200

    yolo

--- ProjectActivFormationParis/src/com/formation/jeci/main/Main.java
+++ ProjectActivFormationParis/src/com/formation/jeci/main/Main.java
@@ -3,8 +3,8 @@ package com.formation.jeci.main;
 public class Main {
 
 	public static void main(String[] args) {
-	
-		System.out.println("hello world");
+
+		System.out.println("yolo swag");
 	}
 
 }
