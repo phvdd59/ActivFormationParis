d3d516ec9049ad4c62eeed512e987a26edc9a713
active_admin <active_admin@LAPTOP-FP1TSI7G>
2016-07-18 17:50:46 +0200

    ptet que ça va marcher là ?

--- ProjectActivFormationParis/src/com/formation/thde/main/IWannaBeTheVeryBest.java
+++ ProjectActivFormationParis/src/com/formation/thde/main/IWannaBeTheVeryBest.java
@@ -3,7 +3,7 @@ package com.formation.thde.main;
 public class IWannaBeTheVeryBest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("dum dee dum");
 
 	}
 
