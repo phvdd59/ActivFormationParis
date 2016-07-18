2cdf390ced409212bae4ab581bac8a040f9b6837
Thomas <Thomas@DESKTOP-JJT3DHP>
2016-07-18 17:46:09 +0200

    HELLO WORLD

--- ProjectActivFormationParis/src/com/formation/thcr/main/MainActiveFormationParis.java
+++ ProjectActivFormationParis/src/com/formation/thcr/main/MainActiveFormationParis.java
@@ -3,8 +3,14 @@ package com.formation.thcr.main;
 public class MainActiveFormationParis {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		MainActiveFormationParis main = new MainActiveFormationParis();
+		main.init();
 
+	}
+
+	private void init() {
+		System.out.println("Hello World");
+		
 	}
 
 }
