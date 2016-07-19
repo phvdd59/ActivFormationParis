7cb1e47f0de68ec08bacaf88b66eb8b30d509108
activ_admin <activ_admin@activ_admin-PC>
2016-07-19 12:27:05 +0200

    salut

---
+++ ProjectActivFormationParis/src/com/formation/ambr/exo/ExoSimple.java
@@ -0,0 +1,22 @@
+package com.formation.ambr.exo;
+
+public class ExoSimple {
+
+	/**
+	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
+	 * 
+	 * @param tab
+	 * @return
+	 */
+
+	public int sumTableau(int[] tab) {
+		int somme = 0;
+		for (int i = 0; i < tab.length; i++) {
+			somme = somme + tab[i];
+		}
+
+		return somme;
+
+	}
+
+}
--- ProjectActivFormationParis/src/com/formation/ambr/main/Main.java
+++ ProjectActivFormationParis/src/com/formation/ambr/main/Main.java
@@ -1,10 +1,23 @@
 package com.formation.ambr.main;
 
+import com.formation.ambr.exo.ExoSimple;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Main main = new Main();
+		main.init();
+
+	}
+
+	private void init() {
+		int[] tab = { 1, 5, 7, 6, 8, 9, 4, 2, 3 };
+		//int a = 5;
+		ExoSimple exoSimple = new ExoSimple(); // on construit un ExoSimple qui s'appelle exoSimple
+
+		int autreSomme = exoSimple.sumTableau(tab);// on dit que autreSomme c'est sumTableau de tab, dans la méthode exoSimple, qui est une méthode (un patron) de type ExoSimple
+		System.out.println(autreSomme); // on lui demande de nous afficher autreSomme
 	}
 
 }
