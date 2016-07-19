b5a4a2ed252a01813f4d8564eace2741530c2e9d
active_admin <active_admin@LAPTOP-1IMLDR5T>
2016-07-19 11:35:46 +0200

    New Commit

--- ProjectActivFormationParis/src/com/formation/anfr/main/MainGit.java
+++ ProjectActivFormationParis/src/com/formation/anfr/main/MainGit.java
@@ -13,7 +13,7 @@ public class MainGit {
 	private void init() {
 		ExoSimple exo = new ExoSimple();
 		System.out.println("Résultat : " + exo.sumTableau());
-		 int[] tableau = {-1,3,5,9,5,14,8,3,6,9};
+		 int[] tableau = {-1,3,5,9,5,14,28,3,6,9};
 		ExoSimple exo2 = new ExoSimple(tableau);
 		System.out.println("Résultat : " + exo2.sumTableau());
 	}
