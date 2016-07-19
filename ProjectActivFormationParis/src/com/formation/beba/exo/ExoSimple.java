49d46544cd73d8d87e182e47b1d70569db379251
active_admin <active_admin@LAPTOP-88DE7C7M>
2016-07-19 12:17:50 +0200

    test2

--- ProjectActivFormationParis/src/com/formation/beba/exo/ExoSimple.java
+++ ProjectActivFormationParis/src/com/formation/beba/exo/ExoSimple.java
@@ -18,5 +18,4 @@ public class ExoSimple {
 		}
 		return sum;
 	}
-
 }
--- ProjectActivFormationParis/src/com/formation/beba/main/MainGit.java
+++ ProjectActivFormationParis/src/com/formation/beba/main/MainGit.java
@@ -16,5 +16,4 @@ public class MainGit {
 		ExoSimple exotab = new ExoSimple();
 		System.out.println(exotab.sumTableau(tab));
 	}
-
 }
