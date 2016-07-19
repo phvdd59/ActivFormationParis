4d66ced0424e6756d35d7e904e9995c9a2bb3302
Thomas <Thomas@DESKTOP-JJT3DHP>
2016-07-19 11:50:51 +0200

    Init Thomas

---
+++ ProjectActivFormationParis/src/com/formation/thcr/exo/ExoSimple.java
@@ -0,0 +1,16 @@
+package com.formation.thcr.exo;
+
+public class ExoSimple {
+
+	/**
+	 * Exemple 
+	 * 		tab = [1,5,7,6,8,9,4,2,3]
+	 * 		résultat : 45
+	 * 
+	 * @param tab
+	 * @return
+	 */
+	public int sumTableau(int[] tab) {
+		return 0;
+	}
+}
