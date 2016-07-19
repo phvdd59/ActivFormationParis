125858eb5373854a932069d6b6cefa2216da14f4
active_admin <active_admin@LAPTOP-88DE7C7M>
2016-07-19 11:10:14 +0200

    PremierExo

--- ProjectActivFormationParis/src/com/formation/beba/exo/ExoSimple.java
+++ ProjectActivFormationParis/src/com/formation/beba/exo/ExoSimple.java
@@ -1,16 +1,33 @@
 package com.formation.beba.exo;
 
 public class ExoSimple {
+	private int tab[];
+	// tab[] = { 1, 5, 7, 6, 8, 9, 4, 2, 3 };
+
+	public ExoSimple(int[] tab) {
+		this.setTab(tab);
+	}
 
 	/**
-	 * Exemple 
-	 * 		tab = [1,5,7,6,8,9,4,2,3]
-	 * 		résultat : 45
+	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
 	 * 
 	 * @param tab
 	 * @return
 	 */
-	public int sumTableau(int[] tab) {
-		return 0;
+
+	public int sumTableau(int[] w) {
+		int sum = 0;
+		for (int i = 0; i < tab.length; i++) {
+			sum = sum + tab[i];
+		}
+		return sum;
+	}
+
+	public int[] getTab() {
+		return tab;
+	}
+
+	public void setTab(int tab[]) {
+		this.tab = tab;
 	}
 }
--- ProjectActivFormationParis/src/com/formation/beba/main/MainGit.java
+++ ProjectActivFormationParis/src/com/formation/beba/main/MainGit.java
@@ -1,10 +1,23 @@
 package com.formation.beba.main;
 
+import com.formation.beba.exo.ExoSimple;
+
 public class MainGit {
 
 	public static void main(String[] args) {
+		MainGit mainGit = new MainGit();
+		mainGit.init();
+	}
+
+	private void init() {
 		// TODO Auto-generated method stub
 
+
+		int tab[] = { 1, 5, 7, 6, 8, 9, 4, 2, 3 };
+		ExoSimple exotab = new ExoSimple(tab);
+		System.out.println(exotab.sumTableau(tab));
 	}
+
+
 
 }
