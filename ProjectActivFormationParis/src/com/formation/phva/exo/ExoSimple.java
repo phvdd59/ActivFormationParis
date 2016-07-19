55c3830a69a337e6d3e217ba8f23a9057e8e5b45
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-07-19 22:03:14 +0200

    Ajout interface

--- ProjectActivFormationParis/src/com/formation/phva/exo/ExoSimple.java
+++ ProjectActivFormationParis/src/com/formation/phva/exo/ExoSimple.java
@@ -1,11 +1,9 @@
 package com.formation.phva.exo;
 
-public class ExoSimple {
+public class ExoSimple implements InterExoSimple {
 
 	/**
-	 * Exemple 
-	 * 		tab = [1,5,7,6,8,9,4,2,3]
-	 * 		résultat : 45
+	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
 	 * 
 	 * @param tab
 	 * @return
---
+++ ProjectActivFormationParis/src/com/formation/phva/exo/InterExoSimple.java
@@ -0,0 +1,6 @@
+package com.formation.phva.exo;
+
+public interface InterExoSimple {
+
+	public int sumTableau(int[] tab);
+}
