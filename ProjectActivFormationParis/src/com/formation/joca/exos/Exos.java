6addb8cdc83678fe3df318cbd45fe8fa93d51158
activ_admin <activ_admin@activ_admin-PC>
2016-07-19 11:48:17 +0200

    MAJ

--- ProjectActivFormationParis/src/com/formation/joca/exos/Exos.java
+++ ProjectActivFormationParis/src/com/formation/joca/exos/Exos.java
@@ -2,8 +2,17 @@ package com.formation.joca.exos;
 
 public class Exos {
 
-	public Exos() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
+	 * 
+	 * @param tab
+	 * @return
+	 */
+	public int sumTableau(int[] tab) {
+		int result = 0;
+		for (int i = 0; i < tab.length; i++) {
+			result = result + tab[i];
+		}
+		return result;
 	}
-
 }
