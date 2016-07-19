69c916f9b3764b18d52ff5b32696ecafd8404431
VDDMichel <VDDMichel@VDDMichel-PC>
2016-07-19 12:36:52 +0200

    test

---
+++ ProjectActivFormationParis/src/com/formation/made/exo/ExoSimple.java
@@ -0,0 +1,18 @@
+package com.formation.made.exo;
+
+public class ExoSimple {
+
+	/**
+	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
+	 * 
+	 * @param tab
+	 * @return
+	 */
+	public int sumTableau(int[] tab) {
+		int somme = 0; // on initialise le tableau somme
+		for (int i = 0; i < tab.length; i++) { // on balaie les valeurs du tableau tab
+			somme = somme + tab[i]; // on ajoute la valeur d'un élément du tableau a la somme déja obtenue
+		}
+		return somme;
+	}
+}
---
+++ ProjectActivFormationParis/src/com/formation/made/exo/MainBidon.java
@@ -0,0 +1,17 @@
+package com.formation.made.exo;
+
+public class MainBidon {
+
+	public static void main(String[] args) {
+		MainBidon test = new MainBidon();
+		test.init();
+	}
+
+	private void init() {
+		ExoSimple sommetableau = new ExoSimple();
+		int[] nouvotablo = { 1, 2, 3, 18, 35, 42 };
+		System.out.println(sommetableau.sumTableau(nouvotablo));
+
+	}
+
+}
