53276f38b57cc34ca76ce8512b8ce6c438484a0c
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-07-19 09:14:30 +0200

    c'est tjs la faute de thomas

--- ProjectActivFormationParis/src/com/formation/thde/main/IWannaBeTheVeryBest.java
+++ ProjectActivFormationParis/src/com/formation/thde/main/IWannaBeTheVeryBest.java
@@ -2,6 +2,8 @@ package com.formation.thde.main;
 
 public class IWannaBeTheVeryBest {
 
+	// c'est la faute de thomas
+
 	public static void main(String[] args) {
 		System.out.println("dum dee dum");
 
