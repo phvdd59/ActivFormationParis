6576f114023857ec62ce147b14fb317694719462
activ_admin <activ_admin@activ_admin-PC>
2016-07-20 12:15:51 +0200

    ExoTableau1Isma

---
+++ ProjectActivFormationParis/src/com/formation/issa/exo/ExoTableau1.java
@@ -0,0 +1,100 @@
+package com.formation.issa.exo;
+
+import com.formation.phva.exo.InterExoTableau1;
+
+public class ExoTableau1 implements InterExoTableau1 {
+
+	/** Exemple
+	 * d=7 , n=5 p=3 --> [7,10,13,16,19]
+	 * 
+	 * @param d = valeur de départ
+	 * @param n = nb d'élément
+	 * @param p = pas entre chaque élément
+	 * @param  
+	 * @return = nouveau tableau rempli
+	 */
+	public int[] remplirTableau(int d, int n, int p) {
+		int[]tab=new int[n];
+		tab[0]=7;
+		for (int i = 1; i < tab.length; i++) {
+			tab[i]=tab[i-1]+p;
+		}
+		
+		return tab;
+	}
+
+	/** Exemple
+	 * tab = [7,10,13,16,19,21,24,27,30] i1=3 , i2=8 tab = [7,10,27,16,19,21,24,13,30]
+	 * @param tab tableau en entrée
+	 * @param i1 première position pour l'interversion
+	 * @param i2 deuxième position pour l'interversion
+	 * @return 
+	 */
+	public int[] intervertirDeuxElementsTableau(int[] tab, int i1, int i2) {
+		i1=3;
+		i2=8;
+		
+		
+		
+		return null;
+	}
+
+	/** Exemple
+	 *  [1,4,6,2,4,6] : p=3,a=8 --> [1,4,6,8,2,4,6]
+	 * @param tab tableau de départ
+	 * @param p position après laquelle il faut ajouter la valeur a 
+	 * @param a valeur à ajouter
+	 * @return
+	 */
+	public int[] insererUnElementDansTableau(int[] tab, int p, int a) {
+		
+		
+	
+		a=8;
+		tab[p]=a;
+		
+		int[]tab1=new int[2];
+		int[]tab2=new int[2];
+		int[]tab3=new int[3];
+		
+		tab=new int[6];
+		for (int i = 0; i < tab1.length/2; i++) {
+			tab1[i]=tab1[i];
+		}
+		for (int i = 4; i < tab2.length; i++) {
+			tab3[i]=a+tab2[i];
+		}
+		//tab[i]=tab1[i]+tab3[i];
+		
+		return tab;
+	}
+
+	/** Exemple 1
+	 *  [1,4,6,2,4,7] : p=2, tab1=[4,9,0] --> [1,4,4,9,0,7]
+	 *  Exemple 2
+	 *  [22,11,0,77,88,99] : p=2, tab1=[3,4,5,6] --> [22,11,3,4,5,6]
+	 * @param tab tableau de départ
+	 * @param p position où il faut remplacer les valeurs du premier tableau par celles du second     
+	 * @param tab1 tableau contenant les valeurs de remplacement
+	 * @return tableau 
+	 */
+	public int[] insererUnTableauDansUnAutreAvecRemplacement(int[] tab, int p, int[] tab1) {
+		
+		p=2;
+		
+		return null;
+	}
+
+	/** Exemple 1
+	 *  tab [1,4,6,2,4,6] : p = 2 --> tab résultat [4,6,1,4,6,2]
+	 *  Exemple 2
+	 *  tab [1,4,6,2,4,6] : p = 4 --> tab résultat [6,2,4,6,1,4]
+	 * @param tab tableau de départ
+	 * @param p position : décaler le tableau vers la droite de p positions 
+	 * 			et les p élements de la fin du tableau reviennent en début de tableau
+	 * @return
+	 */
+	public int[] rotationTableau(int[] tab, int p) {
+		return null;
+	}
+}
