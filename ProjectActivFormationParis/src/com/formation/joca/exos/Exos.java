7fdf9b89cda2456f2b3f5f2631cdfb36821a31de
activ_admin <activ_admin@activ_admin-PC>
2016-07-20 09:10:31 +0200

    Koko

--- ProjectActivFormationParis/src/com/formation/joca/exos/Exos.java
+++ ProjectActivFormationParis/src/com/formation/joca/exos/Exos.java
@@ -1,6 +1,8 @@
 package com.formation.joca.exos;
 
-public class Exos {
+import com.formation.phva.exo.InterExoSimple;
+
+public class Exos implements InterExoSimple {
 
 	/**
 	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
