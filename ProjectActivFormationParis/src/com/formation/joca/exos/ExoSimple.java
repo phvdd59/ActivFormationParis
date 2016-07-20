a5647fb87ad3bdf17fae0333fae97c9d5b54449b
activ_admin <activ_admin@activ_admin-PC>
2016-07-20 09:35:49 +0200

    Modif

--- ProjectActivFormationParis/src/com/formation/joca/exos/ExoSimple.java
+++ ProjectActivFormationParis/src/com/formation/joca/exos/ExoSimple.java
@@ -2,7 +2,7 @@ package com.formation.joca.exos;
 
 import com.formation.phva.exo.InterExoSimple;
 
-public class Exos implements InterExoSimple {
+public class ExoSimple implements InterExoSimple {
 
 	/**
 	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
