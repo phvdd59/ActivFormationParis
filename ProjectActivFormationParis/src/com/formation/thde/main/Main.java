d78f9482c688cb1d8143ee285e5069c878fe8e46
active_admin <active_admin@LAPTOP-FP1TSI7G>
2016-07-20 09:39:28 +0200

    tr

--- ProjectActivFormationParis/src/com/formation/thde/main/Main.java
+++ ProjectActivFormationParis/src/com/formation/thde/main/Main.java
@@ -2,11 +2,12 @@ package com.formation.thde.main;
 
 public class Main {
 
-	
-
 	public static void main(String[] args) {
-		System.out.println("dum dee dum");
-
+		Main main = new Main();
+		main.init();
 	}
 
+	private void init() {
+
+	}
 }
