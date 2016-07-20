bbfc005614299b3a1fd77b31e28d9e33a20078a2
activ_admin <activ_admin@activ_admin-PC>
2016-07-20 09:10:22 +0200

    ExoSimple

---
+++ ProjectActivFormationParis/src/com/formation/issa/exo/ExoSimple.java
@@ -0,0 +1,26 @@
+package com.formation.issa.exo;
+
+import com.formation.phva.exo.InterExoSimple;
+
+public class ExoSimple implements InterExoSimple {
+
+	/**
+	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
+	 * 
+	 * @param tab
+	 * @return
+	 */
+
+	public int sumTableau(int[] tab) {
+
+		int sumTableau = 0;
+
+		for (int i = 0; i < tab.length; i++) {
+			sumTableau = sumTableau + tab[i];
+
+		}
+		return sumTableau;
+
+	}
+
+}
