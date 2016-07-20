3e49915c19f970da289b5bc903ff1e4fb9e61b84
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-07-20 12:15:39 +0200

    Commit ExoTableau1

--- ProjectActivFormationParis/src/com/formation/etga/main/MainGasp.java
+++ ProjectActivFormationParis/src/com/formation/etga/main/MainGasp.java
@@ -1,8 +1,20 @@
 package com.formation.etga.main;
 
+import com.formation.etga.exo.ExoTableau1;
+
 public class MainGasp {
 
 	public static void main(String[] args) {
+		MainGasp mainGasp = new MainGasp();
+		mainGasp.init();
+	}
+
+	private void init() {
+
+		ExoTableau1 exo1 = new ExoTableau1();
+		int[] tab = exo1.remplirTableau(7, 5, 3);
+		int[] test = { 1, 2, 4, 6, 47, 28, 26 };
+		int tab1 = exo1.insererUnElementDansTableau(test, 2, 5);
 
 	}
 }
