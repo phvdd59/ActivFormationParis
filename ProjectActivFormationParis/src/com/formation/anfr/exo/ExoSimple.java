2d900e77d31fe14670e536b6c9bb20f026f7612b
active_admin <active_admin@LAPTOP-1IMLDR5T>
2016-07-20 09:08:52 +0200

    Corrections 2

--- ProjectActivFormationParis/src/com/formation/anfr/exo/ExoSimple.java
+++ ProjectActivFormationParis/src/com/formation/anfr/exo/ExoSimple.java
@@ -1,5 +1,8 @@
 package com.formation.anfr.exo;
-public class ExoSimple {
+
+import com.formation.phva.exo.InterExoSimple;
+
+public class ExoSimple implements InterExoSimple {
 
 	/**
 	 * Exemple tab = [1,5,7,6,8,9,4,2,3] résultat : 45
@@ -7,19 +10,6 @@ public class ExoSimple {
 	 * @param tab
 	 * @return
 	 */
-	public static void main(String[] args) {
-		ExoSimple mainGit = new ExoSimple();
-		mainGit.init();
-
-	}
-	
-	
-	public void init() {
-		int[] tab = {1,5,7,6,8,9,4,2,3};
-		System.out.println("Résultat : " + sumTableau(tab));
-		 int[] tableau = {-1,3,5,9,5,14,28,3,6,9};
-		System.out.println("Résultat : " + sumTableau(tableau));
-	}
 
 	public int sumTableau(int[] tab) {
 		int somme = 0;
--- ProjectActivFormationParis/src/com/formation/anfr/main/MainGit.java
+++ ProjectActivFormationParis/src/com/formation/anfr/main/MainGit.java
@@ -9,8 +9,12 @@ public class MainGit {
 		mainGit.init();
 
 	}
-
-	private void init() {
+	public void init() {
+		int[] tab = { 1, 5, 7, 6, 8, 9, 4, 2, 3 };
+		System.out.println("Résultat : " + new ExoSimple().sumTableau(tab));
+		int[] tableau = { -1, 3, 5, 9, 5, 14, 28, 3, 6, 9 };
+		System.out.println("Résultat : " + new ExoSimple().sumTableau(tableau));
 	}
+
 
 }
