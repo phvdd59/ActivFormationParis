720faaed0fa3b694b15ca576aae3a50718a7cf99
activ_admin <activ_admin@activ_admin-PC>
2016-07-20 12:19:19 +0200

    ExoTableau1Isma

---
+++ ProjectActivFormationParis/src/com/formation/issa/main/MainExoTableau1.java
@@ -0,0 +1,24 @@
+package com.formation.issa.main;
+
+import com.formation.issa.exo.ExoTableau1;
+
+public class MainExoTableau1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainExoTableau1 tableau1=new MainExoTableau1();
+		tableau1.init();
+		
+		
+
+	}
+
+	private void init() {
+		ExoTableau1 exoTableau1=new ExoTableau1();
+		
+		System.out.println(exoTableau1.remplirTableau(7, 6, 3));
+		
+		
+	}
+
+}
