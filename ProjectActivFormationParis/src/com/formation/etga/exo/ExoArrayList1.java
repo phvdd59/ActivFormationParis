a5d3b392bc118ff2e4f0c0e3d096c92fd1349b7c
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-07-22 11:28:38 +0200

    commit ExoArrayList1

--- ProjectActivFormationParis/src/com/formation/etga/exo/ExoArrayList1.java
+++ ProjectActivFormationParis/src/com/formation/etga/exo/ExoArrayList1.java
@@ -1,8 +1,10 @@
 package com.formation.etga.exo;
 
-import com.formation.phva.exo.InterExoTableau1;
+import java.util.ArrayList;
 
-public class ExoTableau1 implements InterExoTableau1 {
+import com.formation.phva.exo.InterExoArrayList1;
+
+public class ExoArrayList1 implements InterExoArrayList1 {
 
 	/**
 	 * Exemple d=7 , n=5 p=3 --> [7,10,13,16,19]
@@ -15,31 +17,28 @@ public class ExoTableau1 implements InterExoTableau1 {
 	 *                = pas entre chaque élément
 	 * @return = nouveau tableau rempli
 	 */
-
-	private int[] tab;
-
-	public int[] remplirTableau(int d, int n, int p) {
-		int[] tab = new int[n];
+	public ArrayList<Integer> remplirTableau(int d, int n, int p) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		int lastValue = d + n * p;
 		int min = Integer.MIN_VALUE;
 		int max = Integer.MAX_VALUE;
 
 		if ((n > 0) && (min < lastValue) && (lastValue < max)) {
 			for (int i = 0; i < n; i++) {
-				tab[i] = d;
+				list.add(d);
 				d = d + p;
 			}
 		} else {
 			System.out.println("Les valeurs sont incorrectes");
 		}
-		return tab;
+		return list;
 	}
 
 	/**
 	 * Exemple tab = [7,10,13,16,19,21,24,27,30] i1=3 , i2=8 tab =
 	 * [7,10,27,16,19,21,24,13,30]
 	 * 
-	 * @param tab
+	 * @param list
 	 *                tableau en entrée
 	 * @param i1
 	 *                première position pour l'interversion
@@ -47,23 +46,24 @@ public class ExoTableau1 implements InterExoTableau1 {
 	 *                deuxième position pour l'interversion
 	 * @return
 	 */
-	public int[] intervertirDeuxElementsTableau(int[] tab, int i1, int i2) {
-		int[] tabFinal = tab.clone();
-		int a = tab[i1];
-		int b = tab[i2];
-		if ((tab != null) && (0 <= i1) && (0 <= i2) && (i1 < tab.length) && (i2 < tab.length)) {
-			tabFinal[i1] = b;
-			tabFinal[i2] = a;
+	public ArrayList<Integer> intervertirDeuxElementsTableau(ArrayList<Integer> list, int i1, int i2) {
+		ArrayList<Integer> listFinal = new ArrayList<Integer>();
+		listFinal.addAll(list);
+		int a = list.get(i1);
+		int b = list.get(i2);
+		if ((list != null) && (0 <= i1) && (0 <= i2) && (i1 < list.size()) && (i2 < list.size())) {
+			listFinal.set(i1, b);
+			listFinal.set(i2, a);
 		} else {
 			System.out.println("Les valeurs sont incorrectes");
 		}
-		return tabFinal;
+		return listFinal;
 	}
 
 	/**
 	 * Exemple [1,4,6,2,4,6] : p=3,a=8 --> [1,4,6,8,2,4,6]
 	 * 
-	 * @param tab
+	 * @param list
 	 *                tableau de départ
 	 * @param p
 	 *                position après laquelle il faut ajouter la valeur a
@@ -71,55 +71,56 @@ public class ExoTableau1 implements InterExoTableau1 {
 	 *                valeur à ajouter
 	 * @return
 	 */
-	public int[] insererUnElementDansTableau(int[] tab, int p, int a) {
-		int[] tabFinal = new int[tab.length + 1];
-		if ((tab != null) && (0 <= p) && (p < tab.length)) {
-			for (int i = 0; i < tabFinal.length; i++) {
+	public ArrayList<Integer> insererUnElementDansTableau(ArrayList<Integer> list, int p, int a) {
+		ArrayList<Integer> listFinal = new ArrayList<Integer>(list.size() + 1);
+		if ((list != null) && (0 <= p) && (p < list.size())) {
+			for (int i = 0; i < listFinal.size(); i++) {
 				if (i < p) {
-					tabFinal[i] = tab[i];
+					listFinal.set(i, list.get(i));
 				} else if (i == p) {
-					tabFinal[i] = a;
+					listFinal.set(i, a);
 				} else {
-					tabFinal[i] = tab[i - 1];
+					listFinal.set(i, list.get(i - 1));
 				}
 			}
 		} else {
 			System.out.println("Les valeurs sont incorrectes");
 		}
-		return tabFinal;
+		return listFinal;
 	}
 
 	/**
 	 * Exemple 1 [1,4,6,2,4,7] : p=2, tab1=[4,9,0] --> [1,4,4,9,0,7] Exemple
 	 * 2 [22,11,0,77,88,99] : p=2, tab1=[3,4,5,6] --> [22,11,3,4,5,6]
 	 * 
-	 * @param tab
+	 * @param list
 	 *                tableau de départ
 	 * @param p
 	 *                position où il faut remplacer les valeurs du premier
 	 *                tableau par celles du second
-	 * @param tab1
+	 * @param list1
 	 *                tableau contenant les valeurs de remplacement
 	 * @return tableau
 	 */
-	public int[] insererUnTableauDansUnAutreAvecRemplacement(int[] tab, int p, int[] tab1) {
-		int[] tabFinal = tab.clone();
-		if ((tab != null) && (tab1 != null) && (0 <= p) && (tab1.length <= tab.length) && (p <= (tab.length - tab1.length))) {
-			for (int i = p; i < tab1.length; i++) {
-				tabFinal[i] = tab1[p];
+	public ArrayList<Integer> insererUnTableauDansUnAutreAvecRemplacement(ArrayList<Integer> list, int p, ArrayList<Integer> list1) {
+		ArrayList<Integer> listFinal = new ArrayList<Integer>();
+		listFinal.addAll(list);
+		if ((list != null) && (list1 != null) && (0 <= p) && (list1.size() <= list.size()) && (p <= (list.size() - list1.size()))) {
+			for (int i = p; i < list1.size(); i++) {
+				listFinal.set(i, list1.get(p));
 				p++;
 			}
 		} else {
 			System.out.println("Les valeurs sont incorrectes");
 		}
-		return tabFinal;
+		return listFinal;
 	}
 
 	/**
 	 * Exemple 1 tab [1,4,6,2,4,6] : p = 2 --> tab résultat [4,6,1,4,6,2]
 	 * Exemple 2 tab [1,4,6,2,4,6] : p = 4 --> tab résultat [6,2,4,6,1,4]
 	 * 
-	 * @param tab
+	 * @param list
 	 *                tableau de départ
 	 * @param p
 	 *                position : décaler le tableau vers la droite de p
@@ -127,23 +128,24 @@ public class ExoTableau1 implements InterExoTableau1 {
 	 *                reviennent en début de tableau
 	 * @return
 	 */
-	public int[] rotationTableau(int[] tab, int p) {
-		int[] tabFinal = tab.clone();
-		if ((tab != null) && (p > tabFinal.length)) {
-			p = p % tabFinal.length;
+	public ArrayList<Integer> rotationTableau(ArrayList<Integer> list, int p) {
+		ArrayList<Integer> listFinal = new ArrayList<Integer>();
+		listFinal.addAll(list);
+		if ((list != null) && (p > listFinal.size())) {
+			p = p % listFinal.size();
 		}
 		if (0 <= p) {
-			for (int i = 0; i < tabFinal.length; i++) {
+			for (int i = 0; i < listFinal.size(); i++) {
 				if (i < p) {
-					tabFinal[i] = tab[i + (tab.length - p)];
+					listFinal.set(i, list.get(list.size() - p));
 				} else {
-					tabFinal[i] = tab[i - p];
+					listFinal.set(i, list.get(i - p));
 				}
 			}
 		} else {
 			System.out.println("Les valeurs sont incorrectes");
 		}
 
-		return tabFinal;
+		return listFinal;
 	}
 }
