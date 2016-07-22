8fa9a80e1a82b7cae93ae6ba28440c7bd7d42673
active_admin <active_admin@LAPTOP-FP1TSI7G>
2016-07-22 12:29:51 +0200

    exos

--- ProjectActivFormationParis/src/com/formation/thde/exo/ExoTableau1.java
+++ ProjectActivFormationParis/src/com/formation/thde/exo/ExoTableau1.java
@@ -46,7 +46,9 @@ public class ExoTableau1 implements InterExoTableau1 {
 	 */
 	public int[] intervertirDeuxElementsTableau(int[] tab, int i1, int i2) {
 		int[] tab2 = null;
-		if (tab == null || i1 < 0 || i2 < 0 || i1 >= tab.length || i2 >= tab.length) {
+		if (tab == null) {
+		} else if (i1 < 0 || i2 < 0 || i1 >= tab.length || i2 >= tab.length) {
+			tab2 = tab.clone();
 		} else {
 			tab2 = tab.clone();
 			tab2[i1 - 1] = tab[i2 - 1];
@@ -69,7 +71,9 @@ public class ExoTableau1 implements InterExoTableau1 {
 	 */
 	public int[] insererUnElementDansTableau(int[] tab, int p, int a) {
 		int tab2[] = null;
-		if (tab == null || p < 0 || p >= tab.length) {
+		if (tab == null) {
+		} else if (p < 0 || p >= tab.length) {
+			tab2 = tab.clone();
 		} else {
 			tab2 = new int[tab.length + 1];
 			for (int i = 0; i < tab2.length; i++) {
@@ -101,7 +105,9 @@ public class ExoTableau1 implements InterExoTableau1 {
 	 */
 	public int[] insererUnTableauDansUnAutreAvecRemplacement(int[] tab, int p, int[] tab1) {
 		int[] tab2 = null;
-		if (tab == null || tab1 == null || p < 0 || p > tab.length - tab1.length || tab1.length > tab.length) {
+		if (tab == null) {
+		} else if (tab1 == null || p < 0 || p > tab.length - tab1.length || tab1.length > tab.length) {
+			tab2 = tab.clone();
 		} else {
 			int k = 0;
 			tab2 = tab.clone();
@@ -128,7 +134,7 @@ public class ExoTableau1 implements InterExoTableau1 {
 	 */
 	public int[] rotationTableau(int[] tab, int p) {
 		int[] tab2 = null;
-		if (tab == null || p < 0) {
+		if (tab == null) {
 		} else {
 			if (p >= tab.length) {
 				p = p % tab.length;
