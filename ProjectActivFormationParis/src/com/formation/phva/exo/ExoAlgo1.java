b75c074beddb2b7d892cd779218ee092e1187fa6
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-07-22 16:56:33 +0200

    Exo pour le week end du 23 et 24 juillet 2016

---
+++ ProjectActivFormationParis/src/com/formation/phva/exo/ExoAlgo1.java
@@ -0,0 +1,33 @@
+package com.formation.phva.exo;
+
+public class ExoAlgo1 implements ImpAlgo1 {
+
+	/**
+	 * soit p entier tel que : 0 <= p < n
+	 * tableau tab de n elements
+	 * 
+	 * la somme des elements des plus petits indice 
+	 * doit etre egal a la somme des elements des plus grand indice
+	 * 
+	 * la somme d'aucun element est egal a zero (p==0 || p==n-1)
+	 * exemple
+	 * 
+	 * n=8
+	 * tab=[-1,3,-4,5,1,-6,2,1]
+	 * 
+	 * p=1 est un point d'equilibre ==> 
+	 * 			-1 = somme(-4,5,1,-6,2,1)
+	 * p=3 est un point d'equilibre
+	 * 			somme(-1,3,-4) = somme(1,-6,2,1) = -2
+	 * p=7 est un point d'equilibre
+	 *  		somme(-1,3,-4,5,1,-6,2) = somme() = 0
+	 *  p=8 n'est pas un point d'equilibre  
+	 *  
+	 *  n [0 --> 100000]
+	 *  chaque element du tableau est un int
+	 */
+	public int solution(int[] tab, int n) {
+		return 0;
+	}
+
+}
---
+++ ProjectActivFormationParis/src/com/formation/phva/exo/ImpAlgo1.java
@@ -0,0 +1,6 @@
+package com.formation.phva.exo;
+
+public interface ImpAlgo1 {
+
+	public int solution(int[] tab,int n);
+}
