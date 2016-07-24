e71bace8ea19577e4c459a839732d5b8dbfbb5c0
activ_admin <activ_admin@activ_admin-PC>
2016-07-25 01:15:59 +0200

    final

--- ProjectActivFormationParis/src/com/formation/joca/main/MainActiveFormationParisJoca.java
+++ ProjectActivFormationParis/src/com/formation/joca/main/MainActiveFormationParisJoca.java
@@ -127,7 +127,7 @@ public class MainActiveFormationParisJoca {
 	
 		private void init5(){
 			ExoAlgo1 exo5=new ExoAlgo1();
-			int[] tab={-1,3,-4,5,1,-6,2,1};
+			int[] tab={-1,3,-4,5,-3,-2,5,-3,1,1,-6,2,1};
 			int p1=exo5.solution(tab);
 			int p2=exo5.solution2(tab);
 			ArrayList<Integer> liste=exo5.solution3(tab);
