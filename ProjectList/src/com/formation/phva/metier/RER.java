ce529e20449e88869cf302ca5c115c6b879679b8
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-07-25 18:41:11 +0200

    res

--- ProjectList/src/com/formation/phva/metier/RER.java
+++ ProjectList/src/com/formation/phva/metier/RER.java
@@ -3,9 +3,19 @@ package com.formation.phva.metier;
 import java.util.ArrayList;
 
 public class RER extends ArrayList<Wagon> {
-	
-	
-	
+
+	public void creTrain(EntrepotMap entrepotMap, int nbWagon) {
+		for (int i = 0; i < nbWagon; i++) {
+			if (i % 4 == 0) {
+				add(entrepotMap.get(Wagon.WAGON_SIMPLE1).remove(entrepotMap.trouverLeDernierWagonDuMemeType(Wagon.WAGON_SIMPLE1)));
+			} else {
+				add(entrepotMap.get(Wagon.WAGON_SIMPLE2).remove(entrepotMap.trouverLeDernierWagonDuMemeType(Wagon.WAGON_SIMPLE2)));
+			}
+			if (i == nbWagon / 2) {
+				add(entrepotMap.get(Wagon.WAGON_RESTO).remove(entrepotMap.trouverLeDernierWagonDuMemeType(Wagon.WAGON_RESTO)));
+			}
+		}
+	}
 
 	public void creTrain(Entrepot entrepot, int nbWagon) {
 		for (int i = 0; i < nbWagon; i++) {
@@ -19,14 +29,4 @@ public class RER extends ArrayList<Wagon> {
 			}
 		}
 	}
-	
-	public void ajoutWagon(Entrepot entrepot) {
-		add(entrepot.remove(0));
-	}
-
-	@Override
-	public boolean add(Wagon e) {
-		return super.add(e);
-	}
-	
 }
