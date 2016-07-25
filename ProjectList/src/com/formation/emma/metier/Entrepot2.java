d4d0a77dc190a44028449cf2c9a91f7329046792
activ_admin <activ_admin@activ_admin-PC>
2016-07-25 16:49:23 +0200

    exo final

---
+++ ProjectList/src/com/formation/emma/metier/Entrepot2.java
@@ -0,0 +1,108 @@
+package com.formation.emma.metier;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Entrepot2 extends TreeMap<Integer, ArrayList<Wagon>> {
+	private static int[] POURCENT_TYPE = { 14, 14, 44, 28 };
+
+	public Entrepot2() {
+		put(Wagon.WAGON_RESTO, new ArrayList<Wagon>());
+		put(Wagon.WAGON_SIMPLE1, new ArrayList<Wagon>());
+		put(Wagon.WAGON_SIMPLE2, new ArrayList<Wagon>());
+		put(Wagon.WAGON_DOUBLE, new ArrayList<Wagon>());
+	}
+
+	public Entrepot2(int nb) {
+		put(Wagon.WAGON_RESTO, new ArrayList<Wagon>());
+		put(Wagon.WAGON_SIMPLE1, new ArrayList<Wagon>());
+		put(Wagon.WAGON_SIMPLE2, new ArrayList<Wagon>());
+		put(Wagon.WAGON_DOUBLE, new ArrayList<Wagon>());
+		fabrique(nb);
+	}
+
+	public void fabrique(int nbWagon) {
+
+		int nbWagonRestoARajouter = (nbWagon * POURCENT_TYPE[Wagon.WAGON_RESTO]) / 100;
+		int nTotal = nbWagonRestoARajouter;
+		for (int i = 0; i < nbWagonRestoARajouter; i++) {
+			get(Wagon.WAGON_RESTO).add(new WagonResto());
+
+			//			Autre méthode 
+			//			ArrayList<Wagon> listWagon=get(Wagon.WAGON_RESTO);
+			//			Wagon wagonResto=new WagonResto();
+			//			listWagon.add(wagonResto);
+		}
+		int nbWagon1ereARajouter = (nbWagon * POURCENT_TYPE[Wagon.WAGON_SIMPLE1]) / 100;
+		nTotal += nbWagon1ereARajouter;
+		for (int i = 0; i < nbWagon1ereARajouter; i++) {
+			get(Wagon.WAGON_SIMPLE1).add(new WagonSimple(WagonSimple.PREMIERE_CLASSE));
+		}
+		int nbWagon2ereARajouter = (nbWagon * POURCENT_TYPE[Wagon.WAGON_SIMPLE2]) / 100;
+		nTotal += nbWagon2ereARajouter;
+		for (int i = 0; i < nbWagon2ereARajouter; i++) {
+			get(Wagon.WAGON_SIMPLE2).add(new WagonSimple(WagonSimple.SECONDE_CLASSE));
+		}
+		int nbWagonDoubleARajouter = nbWagon - nTotal;
+		for (int i = 0; i < nbWagonDoubleARajouter; i++) {
+			get(Wagon.WAGON_DOUBLE).add(new WagonDouble());
+		}
+	}
+
+	public int trouverLeDernierWagonDuMemeType(int type) {
+		return get(type).size() - 1;
+	}
+
+	public boolean add(Wagon e) {
+		return get(e.getType()).add(e);
+	}
+
+	public void add(int index, Wagon element) {
+		get(element.getType()).add(index, element);
+	}
+
+	public void permuter1en1(int indice1, int type) {
+		this.add(indice1, get(type).remove(indice1 + 1));
+	}
+
+	public void tri1() {
+		Set<java.util.Map.Entry<Integer, ArrayList<Wagon>>> set = entrySet();
+		// permit de creer liste sequentielle
+		for (java.util.Map.Entry<Integer, ArrayList<Wagon>> entry : set) {
+			ArrayList<Wagon> lst = entry.getValue();
+			boolean permuteAuMoinsUneFois = true;
+			while (permuteAuMoinsUneFois) {
+				permuteAuMoinsUneFois = false;
+				for (int i = 0; i < lst.size() - 1; i++) {
+					if (lst.get(i).compareTo(lst.get(i + 1)) == 1) {
+						permuter1en1(entry.getKey().intValue(), i);
+						permuteAuMoinsUneFois = true;
+					}
+				}
+			}
+		}
+
+		// autre methode
+		//		for (int k = 0; k < this.size(); k++) { // size donne le nombre de cle et non le nombre d'elements
+		//		ArrayList<Wagon> lst=this.get(k);
+		//			boolean permuteAuMoinsUneFois = true;
+		//			while (permuteAuMoinsUneFois) {
+		//				permuteAuMoinsUneFois = false;
+		//				for (int i = 0; i < lst.size() - 1; i++) {
+		//					if (lst.get(i).compareTo(lst.get(i + 1)) == 1) {
+		//						permuter1en1(k,i);
+		//						permuteAuMoinsUneFois = true;
+		//					}
+		//				}
+		//			}
+		//		}
+
+	}
+
+	public Wagon remove(int type, int index) {
+		ArrayList<Wagon> lst = get(type);
+		return lst.remove(index);
+	}
+
+}
---
+++ ProjectList/src/com/formation/emma/metier/Wagon.java
@@ -0,0 +1,74 @@
+package com.formation.emma.metier;
+
+public abstract class Wagon implements Comparable<Wagon> {
+	public static final int WAGON_RESTO = 0;
+	public static final int WAGON_SIMPLE1 = 1;
+	public static final int WAGON_SIMPLE2 = 2;
+	public static final int WAGON_DOUBLE = 3;
+
+	public static int idIdentification = 0;
+
+	private int id;
+	private int nbSieges;
+	private float lng;
+
+	public Wagon() {
+		this.id = idIdentification;
+		idIdentification++;
+		nbSieges = 80;
+		lng = 20;
+	}
+
+	public Wagon(int nbSieges) {
+		this.id = idIdentification;
+		idIdentification++;
+		setNbSieges(nbSieges);
+		lng = ((float) nbSieges) / 4f;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNbSieges() {
+		return nbSieges;
+	}
+
+	public void setNbSieges(int nbSieges) {
+		if (nbSieges >= 0) {
+			this.nbSieges = nbSieges;
+			lng = ((float) nbSieges) / 4f;
+		}
+	}
+
+	public float getLng() {
+		return lng;
+	}
+
+	public void setLng(float lng) {
+		this.lng = lng;
+	}
+
+	@Override
+	public String toString() {
+		return "Wagon " + id + " : (" + nbSieges + "," + lng + ")";
+	}
+
+	public abstract int getType();
+
+	@Override
+	public int compareTo(Wagon w) {
+		int[] ordreDeTri = { 3, 2, 0, 1 };
+		if (ordreDeTri[getType()] < ordreDeTri[w.getType()]) {
+			return -1;
+		} else if (ordreDeTri[getType()] > ordreDeTri[w.getType()]) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
