00b91b35e497a9bb7af2e364254403fc7edab96f
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-07-26 15:34:14 +0200

    Algo 2

---
+++ ProjectActivFormationParis/src/com/formation/phva/exo/ExoAlgo2.java
@@ -0,0 +1,20 @@
+package com.formation.phva.exo;
+
+public class ExoAlgo2 {
+
+	/**
+	 * Un entier positif d est un diviseur de n si il existe un entier m tel que
+	 * n = d * m;
+	 * 
+	 * par exemple 6 est un diviseur de 24 car m = 4 24=6*4
+	 * 
+	 * ecrire : int fonction(int n) qui retour le nombre de diviseur de n.
+	 * 
+	 * par exemple 24 possede 8 diviseur : [1,2,3,4,6,8,12,24]
+	 * 
+	 */
+
+	public int solution(int n) {
+		return 0;
+	}
+}
