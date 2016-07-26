26c1af28663c5e75551a160267c16cef78e2af10
active_admin <active_admin@LAPTOP-88DE7C7M>
2016-07-26 16:51:58 +0200

    Algo2

--- ProjectActivFormationParis/src/com/formation/beba/main/MainAlgo2.java
+++ ProjectActivFormationParis/src/com/formation/beba/main/MainAlgo2.java
@@ -1,6 +1,7 @@
 package com.formation.beba.main;
 
 import com.formation.beba.exo.ExoAlgo2;
+import com.formation.beba.exo.ExoAlgo2bis;
 
 public class MainAlgo2 {
 
@@ -12,8 +13,13 @@ public class MainAlgo2 {
 	private void init() {
 		ExoAlgo2 exoAlgo2 = new ExoAlgo2();
 
-		int test = exoAlgo2.solution(13);
+		int test = exoAlgo2.solution(24);
 		System.out.println(test);
+
+		ExoAlgo2bis exoAlgo2bis = new ExoAlgo2bis();
+
+		int test2 = exoAlgo2bis.solution(24);
+		System.out.println(test2);
 
 	}
 
