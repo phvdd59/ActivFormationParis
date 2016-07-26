846318f9eb5aac9d0951e452c7880a59abb70cb7
active_admin <active_admin@LAPTOP-173K5L7F>
2016-07-26 09:48:14 +0200

    ok

--- ProjectActivFormationParis/src/com/formation/soka/exo/ExoAlgo1.java
+++ ProjectActivFormationParis/src/com/formation/soka/exo/ExoAlgo1.java
@@ -24,27 +24,27 @@ public class ExoAlgo1 implements ImpAlgo1 {
 	 * la solution doit retourner la position d'un point d'equilibre si il
 	 * existe. sinon retourner -1
 	 */
-	//	public int solution(int[] tab) {
-	//		int position = -1;
-	//		if (tab == null) {
-	//		} else {
-	//			for (int i = 0; i < tab.length; i++) {
-	//				int somme = 0;
-	//				int somme1 = 0;
-	//				for (int j = 0; j < i; j++) {
-	//					somme += tab[j];
-	//				}
-	//				for (int k = i + 1; k < tab.length; k++) {
-	//					somme1 += tab[k];
-	//				}
-	//				if (somme == somme1) {
-	//					position = i;
-	//					break;
-	//				}
-	//			}
-	//		}
-	//		return position;
-	//	}
+	public int solution(int[] tab) {
+		int position = -1;
+		if (tab == null) {
+		} else {
+			for (int i = 0; i < tab.length; i++) {
+				long somme = 0;
+				long somme1 = 0;
+				for (int j = 0; j < i; j++) {
+					somme += tab[j];
+				}
+				for (int k = i + 1; k < tab.length; k++) {
+					somme1 += tab[k];
+				}
+				if (somme == somme1) {
+					position = i;
+					break;
+				}
+			}
+		}
+		return position;
+	}
 
 	//	public int solution2(int[] tab) {
 	//		
@@ -68,34 +68,33 @@ public class ExoAlgo1 implements ImpAlgo1 {
 	//	}
 	//	
 	//
-	public int solution(int[] tab) {
-		int p;
-		if (tab == null) {
-			p = -1;
-		} else {
-			for (p = 0; p < tab.length; p++) {
-				int somme = 0;
-				int somme1 = 0;
-				for (int i = 0; i < tab.length; i++) {
-					if (i < p) {
-						for (int j = 0; j < p; j++) {
-							somme += tab[j];
-						}
-					}
-					if (i > p) {
-						for (int j = p + 1; j < tab.length; j++) {
-							somme1 += tab[j];
-						}
-					}
-					break;
-
-				}
-				if (somme != somme1) {
-					p = -1;
-				}
-
-			}
-		}
-		return p;
-	}
+	//	public int solution3(int[] tab) {
+	//		int p;
+	//		if (tab == null) {
+	//			p = -1;
+	//		} else {
+	//			for (p = 0; p < tab.length; p++) {
+	//				int somme = 0;
+	//				int somme1 = 0;
+	//				for (int i = 0; i < tab.length; i++) {
+	//					if (i < p) {
+	//						for (int j = 0; j < p; j++) {
+	//							somme += tab[j];
+	//						}
+	//					}
+	//					if (i > p) {
+	//						for (int j = p + 1; j < tab.length; j++) {
+	//							somme1 += tab[j];
+	//						}
+	//					}
+	//					break;
+	//
+	//				}
+	//				if (somme != somme1) {
+	//					p = -1;
+	//				}
+	//			}
+	//		}
+	//		return p;
+	//	}
 }
