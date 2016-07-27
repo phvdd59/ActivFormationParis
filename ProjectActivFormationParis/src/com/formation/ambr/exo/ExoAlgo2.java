6535e22624f9d816f38e4b093fc6149fa8ae0bbf
activ_admin <activ_admin@activ_admin-PC>
2016-07-27 13:40:37 +0200

    ExoAlgo2

---
+++ ProjectActivFormationParis/src/com/formation/ambr/exo/ExoAlgo2.java
@@ -0,0 +1,26 @@
+package com.formation.ambr.exo;
+
+import com.formation.phva.exo.ImpAlgo2;
+
+public class ExoAlgo2 implements ImpAlgo2 {
+
+	/**
+	 * Un entier positif d est un diviseur de n si il existe un entier m tel que
+	 * n = d * m;
+	 * 
+	 * par exemple 6 est un diviseur de 24 car m = 4 24=6*4
+	 * 
+	 * ecrire : int fonction(int n) qui retour le nombre de diviseur de n.
+	 * 
+	 * par exemple 24 possede 8 diviseur : [1,2,3,4,6,8,12,24]
+	 * 
+	 */
+
+	public int solution(int n) {
+		int d;
+		int m;
+		//n=d*m;
+		
+		return 0;
+	}
+}
