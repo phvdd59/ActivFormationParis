555ef1b2540f8e5cbb1717be6d4b888167c0740f
VDDMichel <VDDMichel@VDDMichel-PC>
2016-07-28 10:02:10 +0200

    Animal made

---
+++ ProjectAnimal/src/com/formation/made/main/MainAnimal.java
@@ -0,0 +1,16 @@
+package com.formation.made.main;
+
+public class MainAnimal {
+
+	public static void main(String[] args) {
+		MainAnimal mainAnimal=new MainAnimal();
+		mainAnimal.init();
+
+	}
+
+	private void init() {
+		
+		
+	}
+
+}
