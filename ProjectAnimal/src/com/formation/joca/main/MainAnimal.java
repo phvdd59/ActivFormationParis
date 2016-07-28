027de29152fa60ca88b425187e3351186b94b57a
activ_admin <activ_admin@activ_admin-PC>
2016-07-28 09:31:53 +0200

    kk

--- ProjectAnimal/src/com/formation/joca/main/MainAnimal.java
+++ ProjectAnimal/src/com/formation/joca/main/MainAnimal.java
@@ -2,4 +2,9 @@ package com.formation.joca.main;
 
 public class MainAnimal {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
---
+++ ProjectAnimal/src/com/formation/joca/metier/Animal.java
@@ -0,0 +1,5 @@
+package com.formation.joca.metier;
+
+public class Animal {
+
+}
