97599e530082e535ea122289b8f7d1a3cc0ad765
activ_admin <activ_admin@activ_admin-PC>
2016-07-28 10:37:02 +0200

    g

---
+++ ProjectAnimal/src/com/formation/issa/main/MainAnimal.java
@@ -0,0 +1,14 @@
+package com.formation.issa.main;
+
+public class MainAnimal {
+
+	public MainAnimal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ProjectAnimal/src/com/formation/issa/metier/Loup.java
@@ -0,0 +1,25 @@
+package com.formation.issa.metier;
+
+import com.formation.joca.metier.Canin;
+
+public class Loup extends Canin {
+
+	@Override
+	public void manger() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void boire() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void crier() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
