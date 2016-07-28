bb698dba3cb0bb02b42494992bdf29dd187317ea
activ_admin <activ_admin@activ_admin-PC>
2016-07-28 10:06:53 +0200

    chien

---
+++ ProjectAnimal/src/com/formation/emma/metier/Chien.java
@@ -0,0 +1,35 @@
+package com.formation.emma.metier;
+
+import com.formation.thde.metier.Animal;
+
+public class Chien extends Animal {
+
+	public Chien() {
+		super(8, "blanc, marron, noir, gris...");
+	}
+
+	@Override
+	public void manger() {
+			System.out.println("J'engloutis");
+	}
+
+	@Override
+	public void boire() {
+		System.out.println("Je lappe");
+		
+	}
+
+	@Override
+	public void deplacement() {
+		System.out.println("Je peux être seul, à deux, à trois... ou en meute");
+		
+	}
+
+	@Override
+	public void crier() {
+		System.out.println("J'aboie");
+		
+	}
+	
+
+}
