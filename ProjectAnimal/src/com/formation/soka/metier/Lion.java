494650bdfb88634283e64b179c61a595a31fb53e
active_admin <active_admin@LAPTOP-173K5L7F>
2016-07-28 10:00:17 +0200

    Lion

--- ProjectAnimal/src/com/formation/soka/metier/Lion.java
+++ ProjectAnimal/src/com/formation/soka/metier/Lion.java
@@ -1,24 +1,37 @@
 package com.formation.soka.metier;
 
+import com.formation.thde.metier.Animal;
+
 public class Lion extends Animal {
+
+	public Lion() {
+		super(180, "rouge");
+	}
+
+	public Lion(int poids, String couleur) {
+		super(poids, couleur);
+	}
 
 	@Override
 	public void manger() {
-
+		System.out.println("Je mange tous les animaux de la Jungle");
 	}
 
 	@Override
 	public void boire() {
+		System.out.println("Je bois, je bois");
 
 	}
 
 	@Override
 	public void deplacement() {
+		System.out.println("Je me déplace pour mieux digérer");
 
 	}
 
 	@Override
 	public void crier() {
+		System.out.println("Je rugis aprés avoir bien mangé");
 
 	}
 
