69dfea998da14e71a79a23d23c2061254322f288
active_admin <active_admin@LAPTOP-1IMLDR5T>
2016-07-28 09:54:51 +0200

    Pony

---
+++ ProjectAnimal/src/com/formation/anfr/metier/Poney.java
@@ -0,0 +1,29 @@
+package com.formation.anfr.metier;
+
+import com.formation.thde.metier.Animal;
+
+public class Poney extends Animal {
+
+	@Override
+	public void manger() {
+		System.out.println("Le poney mange de l'herbe.");
+		
+	}
+
+	@Override
+	public void boire() {
+		System.out.println("Le poney boit avec la langue.");
+		
+	}
+
+	@Override
+	public void deplacement() {
+		System.out.println("Le poney galope.");
+		
+	}
+
+	@Override
+	public void crier() {
+		System.out.println("Le poney hénnit.");
+	}
+}
