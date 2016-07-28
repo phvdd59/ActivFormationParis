cc59b70292f2c7274293a9fd99da896d2840fee2
Thomas <Thomas@DESKTOP-JJT3DHP>
2016-07-28 10:10:53 +0200

    //

--- ProjectAnimal/src/com/formation/thcr/metier/Saumon.java
+++ ProjectAnimal/src/com/formation/thcr/metier/Saumon.java
@@ -2,38 +2,34 @@ package com.formation.thcr.metier;
 
 import com.formation.thde.metier.Animal;
 
-public class Poisson extends Animal {
+public class Saumon extends Animal {
 
-	public Poisson() {
+	public Saumon() {
 		super(3, "vert fluo");
 	}
 
-	public Poisson(int poids, String str) {
+	public Saumon(int poids, String str) {
 		super(poids, str);
 	}
 
 	@Override
 	public void manger() {
 		System.out.println("Je mange d'autres poissons");
-
 	}
 
 	@Override
 	public void boire() {
 		System.out.println("Je ne bois pas");
-
 	}
 
 	@Override
 	public void deplacement() {
 		System.out.println("Je nage");
-
 	}
 
 	@Override
 	public void crier() {
 		System.out.println("Dans l'eau, personne ne vous entendra crier !!!");
-
 	}
 
 }
