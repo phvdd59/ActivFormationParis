ad0a2503ccc65518b6b3c75c64f6d61beb21e2d8
activ_admin <activ_admin@activ_admin-PC>
2016-07-28 10:59:31 +0200

    exo

---
+++ ProjectActivFormationParis/src/com/formation/joca/exo/Dico.java
@@ -0,0 +1,23 @@
+package com.formation.joca.exo;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import com.formation.phva.exo.CleDico;
+import com.formation.phva.exo.InterExoDico1;
+
+public class Dico extends TreeMap<CleDico, ArrayList<Mot>> implements InterExoDico1 {
+
+	@Override
+	public void ranger(String texte) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArrayList<String> getListeMot(String lettre, int lngMot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ProjectActivFormationParis/src/com/formation/joca/exo/Mot.java
@@ -0,0 +1,53 @@
+package com.formation.joca.exo;
+
+import com.formation.phva.exo.InterExoMot1;
+
+public class Mot implements InterExoMot1, Comparable<Mot> {
+
+	private String mot;
+	private int nb;
+
+	public Mot(String mot) {
+		// TODO
+	}
+
+	public String getMot() {
+		return mot;
+	}
+
+	public void setMot(String mot) {
+		this.mot = mot;
+	}
+
+	public int getNb() {
+		return nb;
+	}
+
+	public void setNb(int nb) {
+		this.nb = nb;
+	}
+
+	@Override
+	public int compareTo(Mot o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void plusUn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
