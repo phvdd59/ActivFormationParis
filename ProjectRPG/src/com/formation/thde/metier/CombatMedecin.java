627525a18d29f6e3448a83b40cb058ba0b8a6ef0
active_admin <active_admin@LAPTOP-FP1TSI7G>
2016-07-29 15:29:41 +0200

    combat

--- ProjectRPG/src/com/formation/thde/metier/CombatMedecin.java
+++ ProjectRPG/src/com/formation/thde/metier/CombatMedecin.java
@@ -2,10 +2,10 @@ package com.formation.thde.metier;
 
 import com.formation.thde.inter.EspritCombatif;
 
-public class CombatPistolet implements EspritCombatif {
+public class CombatMedecin implements EspritCombatif {
 
 	@Override
 	public void combat() {
-		System.out.println("Combat avec un fusil de sniper comme un lache");
+		System.out.println("Combat avec un stethoscope");
 	}
 }
