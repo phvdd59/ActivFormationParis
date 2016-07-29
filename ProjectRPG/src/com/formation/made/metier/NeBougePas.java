df12e3ed8d9b00cac8ea3bad5be70c9f512aacc8
VDDMichel <VDDMichel@VDDMichel-PC>
2016-07-29 15:10:22 +0200

    made

---
+++ ProjectRPG/src/com/formation/made/metier/NeBougePas.java
@@ -0,0 +1,13 @@
+package com.formation.made.metier;
+
+import com.formation.made.inter.Deplacement;
+
+public class NeBougePas implements Deplacement {
+
+	@Override
+	public void deplacer() {
+		System.out.println("Je suis immobile");
+
+	}
+
+}
