58b8e1d7b5bc141267133d23eaeb66f1f24471c0
activ_admin <activ_admin@activ_admin-PC>
2016-07-29 14:35:09 +0200

    Medic

--- ProjectRPG/src/com/formation/ambr/metier/Medecin.java
+++ ProjectRPG/src/com/formation/ambr/metier/Medecin.java
@@ -1,8 +1,31 @@
 package com.formation.ambr.metier;
 
-public class Medecin {
+import com.formation.emma.inter.Soin;
+import com.formation.joca.metier.Personnage;
+import com.formation.made.inter.Deplacement;
+import com.formation.thde.inter.EspritCombatif;
+
+public class Medecin extends Personnage implements Soin, Deplacement, EspritCombatif {
 
 	public Medecin() {
+
+	}
+
+	@Override
+	public void combat() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deplacer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void soigne() {
+		// TODO Auto-generated method stub
 
 	}
 
