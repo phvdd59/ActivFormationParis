834509fc2fd3339dd63167ba27642bf43f8758ee
active_admin <active_admin@LAPTOP-FP1TSI7G>
2016-07-29 15:10:40 +0200

    combat

--- ProjectRPG/src/com/formation/thde/metier/CombatPistolet.java
+++ ProjectRPG/src/com/formation/thde/metier/CombatPistolet.java
@@ -2,12 +2,10 @@ package com.formation.thde.metier;
 
 import com.formation.thde.inter.EspritCombatif;
 
-public class CombatPistolet implements EspritCombatif{
+public class CombatPistolet implements EspritCombatif {
 
 	@Override
 	public void combat() {
-
-		
+		System.out.println("Combat avec une arme à feu");
 	}
-
 }
