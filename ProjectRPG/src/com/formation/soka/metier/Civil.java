5e83ab8cc1549893b0913d301187ac2658518691
active_admin <active_admin@LAPTOP-173K5L7F>
2016-07-29 14:51:22 +0200

    ok

--- ProjectRPG/src/com/formation/soka/metier/Civil.java
+++ ProjectRPG/src/com/formation/soka/metier/Civil.java
@@ -1,7 +1,32 @@
 package com.formation.soka.metier;
 
+import com.formation.emma.inter.Soin;
 import com.formation.joca.metier.Personnage;
+import com.formation.made.inter.Deplacement;
+import com.formation.thde.inter.EspritCombatif;
 
-public class Civil extends Personnage {
+public class Civil extends Personnage implements EspritCombatif, Deplacement, Soin {
+
+	public Civil(EspritCombatif combat, Deplacement deplacement, Soin soin) {
+		super(combat, deplacement, soin);
+	}
+
+	@Override
+	public void soigne() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deplacer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void combat() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
