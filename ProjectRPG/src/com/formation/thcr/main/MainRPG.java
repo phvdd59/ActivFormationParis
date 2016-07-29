e42d920fcf558d26b529a72457662926e4db9767
Thomas <Thomas@DESKTOP-JJT3DHP>
2016-07-29 15:13:12 +0200

    //

--- ProjectRPG/src/com/formation/thcr/main/MainRPG.java
+++ ProjectRPG/src/com/formation/thcr/main/MainRPG.java
@@ -3,13 +3,17 @@ package com.formation.thcr.main;
 import java.util.ArrayList;
 
 import com.formation.ambr.metier.Medecin;
-import com.formation.emma.inter.Soin;
 import com.formation.etga.metier.Sniper;
 import com.formation.issa.metier.Chirurgien;
+import com.formation.jeci.metier.Guerrier;
 import com.formation.joca.metier.Personnage;
-import com.formation.made.inter.Deplacement;
+import com.formation.made.metier.Courrir;
+import com.formation.made.metier.Marcher;
+import com.formation.made.metier.NeBougePas;
 import com.formation.soka.metier.Civil;
-import com.formation.thde.inter.EspritCombatif;
+import com.formation.thde.metier.CombatCouteau;
+import com.formation.thde.metier.CombatPistolet;
+import com.formation.thde.metier.PasDeCombat;
 
 public class MainRPG {
 
@@ -20,11 +24,11 @@ public class MainRPG {
 
 	private void init() {
 		//TODO instanciation de tous les persos et mis en pllace des méthodes
-		Personnage civil = new Civil(new EspritCombatif(), new Deplacement(), new Soin());
-		Personnage sniper = new Sniper(new EspritCombatif(), new Deplacement(), new Soin());
-		Personnage guerrier = new Guerrier(new EspritCombatif(), new Deplacement(), new Soin());
-		Personnage medecin = new Medecin(new EspritCombatif(), new Deplacement(), new Soin());
-		Personnage chirurgien = new Chirurgien(new EspritCombatif(), new Deplacement(), new Soin());
+		Personnage civil = new Civil(new PasDeCombat(), new Courrir(), new Sevanouir());
+		Personnage sniper = new Sniper(new CombatPistolet(), new NeBougePas(), new PremierSoin());
+		Personnage guerrier = new Guerrier(new CombatCouteau(), new Marcher(), new SoignePas());
+		Personnage medecin = new Medecin(new CombatCouteau(), new Courrir(), new PremierSoin());
+		Personnage chirurgien = new Chirurgien(new CombatCouteau(), new Courrir(), new Opeartion());
 
 		ArrayList<Personnage> list = new ArrayList<Personnage>();
 		list.add(civil);
