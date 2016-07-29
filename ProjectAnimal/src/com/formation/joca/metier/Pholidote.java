1fc3c67ebc4dc5eef4e0f84e84972aa27c3a7691
activ_admin <activ_admin@activ_admin-PC>
2016-07-29 13:26:26 +0200

    correct

--- ProjectAnimal/src/com/formation/joca/metier/Pholidote.java
+++ ProjectAnimal/src/com/formation/joca/metier/Pholidote.java
@@ -1,6 +1,7 @@
 package com.formation.joca.metier;
 
 import com.formation.etga.Interface.Lapper;
+import com.formation.thde.metier.Animal;
 
 public class Pholidote extends com.formation.thde.metier.Animal implements Lapper {
 
@@ -38,6 +39,17 @@ public class Pholidote extends com.formation.thde.metier.Animal implements Lappe
 	public void crier() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public boolean egale(Animal animal) {
+		boolean result = false;
+		if (animal instanceof Pholidote) {
+			if (this.getPoids() == animal.getPoids() && this.getCouleur().equals(animal.getCouleur())) {
+				result = true;
+			}
+		}
+		return result;
 	}
 
 }
