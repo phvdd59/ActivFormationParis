536dcf345abc49febca14a001a8b2ca93a2fac98
VDDMichel <VDDMichel@VDDMichel-PC>
2016-07-29 15:05:31 +0200

    deplacement made

---
+++ ProjectRPG/src/com/formation/made/metier/Sauter.java
@@ -0,0 +1,13 @@
+package com.formation.made.metier;
+
+import com.formation.made.inter.Deplacement;
+
+public class Sauter implements Deplacement {
+
+	@Override
+	public void deplacer() {
+		System.out.println("Jump Around");
+
+	}
+
+}
