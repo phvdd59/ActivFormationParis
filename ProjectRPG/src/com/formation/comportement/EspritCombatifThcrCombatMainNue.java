ea698879b071227ce161efebf9fe2f10f06c7972
Thomas <Thomas@DESKTOP-JJT3DHP>
2016-07-29 15:42:22 +0200

    //

---
+++ ProjectRPG/src/com/formation/comportement/EspritCombatifThcrCombatMainNue.java
@@ -0,0 +1,12 @@
+package com.formation.comportement;
+
+import com.formation.thde.inter.EspritCombatif;
+
+public class EspritCombatifThcrCombatMainNue implements EspritCombatif {
+
+	@Override
+	public void combat() {
+		System.out.println("Je combat à main nue");
+	}
+
+}
