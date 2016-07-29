f129758f473cff1be488ce3ca38322492feee13a
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-07-29 12:37:33 +0200

    override

--- ProjectAnimal/src/com/formation/etga/main/MainAnimal.java
+++ ProjectAnimal/src/com/formation/etga/main/MainAnimal.java
@@ -1,12 +1,5 @@
 package com.formation.etga.main;
 
-import com.formation.beba.metier.Tigre;
-import com.formation.jeci.metier.Aigle;
-import com.formation.joca.metier.Pangolin;
-import com.formation.made.metier.Panda;
-import com.formation.soka.metier.Lion;
-import com.formation.thcr.metier.Saumon;
-
 public class MainAnimal {
 
 	public static void main(String[] args) {
@@ -16,41 +9,7 @@ public class MainAnimal {
 	}
 
 	private void init() {
-		Saumon poisson = new Saumon();
-		poisson.deplacement();
-		poisson.crier();
-		poisson.boire();
-		poisson.manger();
 
-		Tigre tigre = new Tigre();
-		tigre.deplacement();
-		tigre.crier();
-		tigre.boire();
-		tigre.manger();
-
-		Aigle aigle = new Aigle(25, "Maron");
-		aigle.deplacement();
-		aigle.boire();
-		aigle.crier();
-		aigle.manger();
-
-		Pangolin pango = new Pangolin(25, "Marron");
-		pango.deplacement();
-		pango.boire();
-		pango.crier();
-		pango.manger();
-
-		Lion lion = new Lion();
-		lion.deplacement();
-		lion.boire();
-		lion.manger();
-		lion.crier();
-
-		Panda panda = new Panda();
-		panda.deplacement();
-		panda.boire();
-		panda.manger();
-		panda.crier();
 	}
 
 }
