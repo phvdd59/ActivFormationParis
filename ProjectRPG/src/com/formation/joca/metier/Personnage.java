f1c7313c665730421187de37dba1808c84c4f8b7
activ_admin <activ_admin@activ_admin-PC>
2016-07-29 14:40:34 +0200

    dd

--- ProjectRPG/src/com/formation/joca/metier/Personnage.java
+++ ProjectRPG/src/com/formation/joca/metier/Personnage.java
@@ -4,7 +4,7 @@ import com.formation.emma.inter.Soin;
 import com.formation.made.inter.Deplacement;
 import com.formation.thde.inter.EspritCombatif;
 
-public class Personnage implements EspritCombatif, Deplacement, Soin {
+public abstract class Personnage {
 
 	private EspritCombatif combat;
 	private Deplacement deplacement;
@@ -17,58 +17,16 @@ public class Personnage implements EspritCombatif, Deplacement, Soin {
 		this.soin = soin;
 	}
 
-	public EspritCombatif getCombat() {
-		return combat;
-	}
-
-	public void setCombat(EspritCombatif combat) {
-		this.combat = combat;
-	}
-
-	public Deplacement getDeplacement() {
-		return deplacement;
-	}
-
-	public void setDeplacement(Deplacement deplacement) {
-		this.deplacement = deplacement;
-	}
-
-	public Soin getSoin() {
-		return soin;
-	}
-
-	public void setSoin(Soin soin) {
-		this.soin = soin;
-	}
-
 	public void seDeplacer() {
-
+		deplacement.deplacer();
 	}
 
 	public void combattre() {
-
+		combat.combat();
 	}
 
 	public void soigner() {
-
-	}
-
-	@Override
-	public void soigne() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deplacer() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void combat() {
-		// TODO Auto-generated method stub
-
+		soin.soigne();
 	}
 
 }
