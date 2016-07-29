f94d0f648fb6377bc0d4a9ea7e0ce6694d4e07eb
active_admin <active_admin@LAPTOP-173K5L7F>
2016-07-29 16:21:17 +0200

    ok

--- ProjectRPG/src/com/formation/soka/metier/Civil.java
+++ ProjectRPG/src/com/formation/soka/metier/Civil.java
@@ -1,19 +1,19 @@
 package com.formation.soka.metier;
 
+import com.formation.comportement.EspritCombatifSokaPacifiste;
 import com.formation.emma.inter.Soin;
 import com.formation.emma.metier.Sevanouir;
 import com.formation.joca.metier.Personnage;
 import com.formation.made.inter.Deplacement;
 import com.formation.made.metier.NeBougePas;
 import com.formation.thde.inter.EspritCombatif;
-import com.formation.thde.metier.PasDeCombat;
 
 public class Civil extends Personnage {
 
 	public Civil() {
-		super(new PasDeCombat(), new NeBougePas(), new Sevanouir());
+		super(new EspritCombatifSokaPacifiste(), new NeBougePas(), new Sevanouir());
 	}
-	
+
 	public Civil(EspritCombatif combat, Deplacement deplacement, Soin soin) {
 		super(combat, deplacement, soin);
 	}
