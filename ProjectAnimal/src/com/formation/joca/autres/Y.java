6ed4b51cd733b8be6032027f9cfd623af35378c6
activ_admin <activ_admin@activ_admin-PC>
2016-07-29 11:50:52 +0200

    correction dico

---
+++ ProjectAnimal/src/com/formation/joca/autres/Y.java
@@ -0,0 +1,17 @@
+package com.formation.joca.autres;
+
+public class Y implements I1 {
+
+	@Override
+	public void methodeA() {
+		System.out.println("methodeA de Y");
+
+	}
+
+	@Override
+	public void methodeB() {
+		System.out.println("methodeB de Y");
+
+	}
+
+}
