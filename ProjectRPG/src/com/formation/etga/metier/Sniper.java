86cd66260229e3a550a6070048c0401282287186
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-07-29 15:01:09 +0200

    sfsd

--- ProjectRPG/src/com/formation/etga/metier/Sniper.java
+++ ProjectRPG/src/com/formation/etga/metier/Sniper.java
@@ -10,17 +10,27 @@ public class Sniper extends Personnage {
 	public Sniper() {
 		super();
 	}
-	
+
 	public Sniper(EspritCombatif combat, Deplacement deplacement, Soin soin) {
 		this.combat = combat;
 		this.deplacement = deplacement;
 		this.soin = soin;
-		
-		
+
 	}
 
+	@Override
 	public void combattre() {
 		System.out.println("Je combats avec mon snip aaiiiiggght #GrosCampeur");
 
+	}
+
+	@Override
+	public void seDeplacer() {
+		System.out.println("Pas trop trop");
+	}
+
+	@Override
+	public void soigner() {
+		System.out.println("Je me soigne tout seul avec mon super kit si c'est pas trop grave");
 	}
 }
