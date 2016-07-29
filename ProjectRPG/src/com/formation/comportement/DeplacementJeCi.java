a1c2a9aa94246699487aa7a32d5b97101fe7a542
ACTIVjeci <jeremyciamous@gmail.com>
2016-07-29 16:07:08 +0200

    a

---
+++ ProjectRPG/src/com/formation/comportement/DeplacementJeCi.java
@@ -0,0 +1,13 @@
+package com.formation.comportement;
+
+import com.formation.made.inter.Deplacement;
+
+public class DeplacementJeCi implements Deplacement{
+
+	@Override
+	public void deplacer() {
+		System.out.println("Je me déplace tout seul car je suis solide");
+		
+	}
+
+}
---
+++ ProjectRPG/src/com/formation/comportement/SoinJeCI.java
@@ -0,0 +1,14 @@
+package com.formation.comportement;
+
+import com.formation.emma.inter.Soin;
+
+public class SoinJeCI implements Soin{
+
+	@Override
+	public void soigne() {
+	System.out.println("Les pansements c'est pour les enfants!!");
+	
+		
+	}
+
+}
