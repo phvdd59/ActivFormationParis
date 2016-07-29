bcc036a59bf4a86627eed659522c30576c2529b3
activ_admin <activ_admin@activ_admin-PC>
2016-07-29 16:12:28 +0200

    SoinsCh

--- ProjectRPG/src/com/formation/issa/metier/Chirurgien.java
+++ ProjectRPG/src/com/formation/issa/metier/Chirurgien.java
@@ -1,21 +1,21 @@
 package com.formation.issa.metier;
 
+import com.formation.comportement.DeplacementChirurgien;
+import com.formation.comportement.EspritCombatifChirugien;
+import com.formation.comportement.SoinScalpel;
 import com.formation.emma.inter.Soin;
-import com.formation.emma.metier.Operation;
 import com.formation.joca.metier.Personnage;
 import com.formation.made.inter.Deplacement;
-import com.formation.made.metier.Marcher;
 import com.formation.thde.inter.EspritCombatif;
-import com.formation.thde.metier.CombatScalpel;
 
 public class Chirurgien extends Personnage  {
 
 	
 	public Chirurgien() {
 		super();
-		this.combat= new CombatScalpel();
-		this.deplacement=new Marcher();
-		this.soin=new Operation();
+		this.combat= new EspritCombatifChirugien();
+		this.deplacement=new DeplacementChirurgien();
+		this.soin=new SoinScalpel();
 		// TODO Auto-generated constructor stub
 	}
 	public Chirurgien(EspritCombatif combat, Deplacement deplacement, Soin soin) {
