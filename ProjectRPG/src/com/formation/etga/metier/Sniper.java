7c5de5cfd5a72c554a7ecf2533a84887296a07d4
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-07-29 15:30:52 +0200

    fyj

--- ProjectRPG/src/com/formation/etga/metier/Sniper.java
+++ ProjectRPG/src/com/formation/etga/metier/Sniper.java
@@ -1,14 +1,20 @@
 package com.formation.etga.metier;
 
 import com.formation.emma.inter.Soin;
+import com.formation.emma.metier.PremierSoin;
 import com.formation.joca.metier.Personnage;
 import com.formation.made.inter.Deplacement;
+import com.formation.made.metier.NeBougePas;
 import com.formation.thde.inter.EspritCombatif;
+import com.formation.thde.metier.CombatPistolet;
 
-public class Sniper extends Personnage implements EspritCombatif, Deplacement, Soin {
+public class Sniper extends Personnage {
 
 	public Sniper() {
 		super();
+		this.combat = new CombatPistolet();
+		this.deplacement = new NeBougePas();
+		this.soin = new PremierSoin();
 	}
 
 	public Sniper(EspritCombatif combat, Deplacement deplacement, Soin soin) {
@@ -18,17 +24,14 @@ public class Sniper extends Personnage implements EspritCombatif, Deplacement, S
 
 	}
 
-	@Override
 	public void soigne() {
 		System.out.println("Je me soigne tout seul avec mon super kit si c'est pas trop grave");
 	}
 
-	@Override
 	public void deplacer() {
 		System.out.println("Pas trop trop");
 	}
 
-	@Override
 	public void combat() {
 		System.out.println("Je combats avec mon snip aaiiiiggght #GrosCampeur");
 	}
