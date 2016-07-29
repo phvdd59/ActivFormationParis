8a4fa2ca43292578bc34eeff4c39c1b33a983c18
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-07-29 15:44:48 +0200

    Comportement

---
+++ ProjectRPG/src/com/formation/comportement/DeplacementPhvaEcrouler.java
@@ -0,0 +1,13 @@
+package com.formation.comportement;
+
+import com.formation.made.inter.Deplacement;
+
+public class DeplacementPhvaEcrouler implements Deplacement {
+
+	@Override
+	public void deplacer() {
+		System.out.println("Je m'écroule.");
+	}
+	
+
+}
