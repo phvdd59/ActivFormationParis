55cf24fd638a6c4dfa2c63cbac6ba38ce60e014d
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-07-29 10:37:35 +0200

    azer

--- ProjectAnimal/src/com/formation/phva/main/MainAnimal.java
+++ ProjectAnimal/src/com/formation/phva/main/MainAnimal.java
@@ -2,7 +2,9 @@ package com.formation.phva.main;
 
 import com.formation.anfr.metier.Poney;
 import com.formation.issa.metier.Loup;
+import com.formation.jeci.interfacea.Recueille;
 import com.formation.jeci.metier.Aigle;
+import com.formation.jeci.metier.Oiseaux;
 import com.formation.soka.metier.Lion;
 import com.formation.thde.metier.Animal;
 
@@ -10,9 +12,14 @@ public class MainAnimal {
 	public static void main(String[] args) {
 		Animal poney = new Poney(500, "Maron");
 		poney.crier();
-		Animal aigle = new Aigle(1, "blanc");
+		Oiseaux aigle = new Aigle(1, "blanc");
 		aigle.crier();
 		Animal loup = new Loup(80, "noir");
 		Animal lion = new Lion(250, "jaune");
+		Recueille re = aigle;
+		re.recueille();
+		Recueille re1 = new Aigle(2, "noir");
+		
+		
 	}
 }
