147913219d956eade4dede11ffbbdbe05081b84a
VDDMichel <VDDMichel@VDDMichel-PC>
2016-07-29 15:50:49 +0200

    you "made" bro?

---
+++ ProjectRPG/src/com/formation/comportement/DeplacementMadeVoler.java
@@ -0,0 +1,13 @@
+package com.formation.comportement;
+
+import com.formation.made.inter.Deplacement;
+
+public class DeplacementMadeVoler implements Deplacement {
+
+	@Override
+	public void deplacer() {
+		System.out.println("Je vole! Je vooooleeee!");
+
+	}
+
+}
