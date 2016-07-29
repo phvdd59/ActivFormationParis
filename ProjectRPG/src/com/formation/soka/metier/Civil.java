7086217fdcd210f23abcd5a852198a4842dad684
active_admin <active_admin@LAPTOP-173K5L7F>
2016-07-29 15:14:33 +0200

    ok

--- ProjectRPG/src/com/formation/soka/metier/Civil.java
+++ ProjectRPG/src/com/formation/soka/metier/Civil.java
@@ -1,33 +1,20 @@
 package com.formation.soka.metier;
 
 import com.formation.emma.inter.Soin;
+import com.formation.emma.metier.Sevanouir;
 import com.formation.joca.metier.Personnage;
 import com.formation.made.inter.Deplacement;
+import com.formation.made.metier.NeBougePas;
 import com.formation.thde.inter.EspritCombatif;
+import com.formation.thde.metier.PasDeCombat;
 
-public class Civil extends Personnage implements EspritCombatif, Deplacement, Soin {
+public class Civil extends Personnage {
 
 	public Civil() {
-
+		this(new PasDeCombat(), new NeBougePas(), new Sevanouir());
 	}
-
+	
 	public Civil(EspritCombatif combat, Deplacement deplacement, Soin soin) {
 		super(combat, deplacement, soin);
 	}
-
-	@Override
-	public void soigne() {
-		System.out.println("Prend du doliprane quand il a mal à la tête");
-	}
-
-	@Override
-	public void deplacer() {
-		System.out.println("Il gambade");
-	}
-
-	@Override
-	public void combat() {
-		System.out.println("Il a peur → → →");
-	}
-
 }
