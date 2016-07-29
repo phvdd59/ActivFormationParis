0b170434d749b6c708828d5b4d8ecf7fd1c9205f
VDDMichel <VDDMichel@VDDMichel-PC>
2016-07-29 13:47:35 +0200

    dico made

---
+++ ProjectActivFormationParis/src/com/formation/made/exo/CleDico.java
@@ -0,0 +1,61 @@
+package com.formation.made.exo;
+
+public class CleDico implements Comparable<CleDico> {
+
+	private String lettre;
+	private int lngMot;
+
+	public CleDico(String lettre, int lngMot) {
+		super();
+		this.lettre = lettre;
+		this.lngMot = lngMot;
+	}
+
+	public String getLettre() {
+		return lettre;
+	}
+
+	public void setLettre(String lettre) {
+		this.lettre = lettre;
+	}
+
+	public int getLngMot() {
+		return lngMot;
+	}
+
+	public void setLngMot(int lngMot) {
+		this.lngMot = lngMot;
+	}
+
+	@Override
+	public int compareTo(CleDico o) {
+		int compare = 0;
+		if (this.getLettre().compareTo(this.getLettre()) == 0) {
+			if (this.getLngMot() == o.getLngMot()) {
+				compare = 0;
+			} else if (this.getLngMot() >= o.getLngMot()) {
+				compare = 1;
+			} else {
+				compare = -1;
+			}
+		} else if (this.getLettre().compareTo(this.getLettre()) == 1) {
+			if (this.getLngMot() == o.getLngMot()) {
+				compare = 1;
+			} else if (this.getLngMot() >= o.getLngMot()) {
+				compare = 1;
+			} else {
+				compare = -1;
+			}
+		} else {
+			if (this.getLngMot() == o.getLngMot()) {
+				compare = -1;
+			} else if (this.getLngMot() >= o.getLngMot()) {
+				compare = 1;
+			} else {
+				compare = -1;
+			}
+		}
+
+		return compare;
+	}
+}
--- ProjectActivFormationParis/src/com/formation/made/exo/Dico.java
+++ ProjectActivFormationParis/src/com/formation/made/exo/Dico.java
@@ -3,46 +3,58 @@ package com.formation.made.exo;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
+import org.hamcrest.core.SubstringMatcher;
+
 import com.formation.phva.exo.InterExoDico1;
 
 public class Dico extends TreeMap<CleDico, ArrayList<Mot>> implements InterExoDico1 {
 
 	@Override
 	public void ranger(String texte) {
-		ArrayList<String> listeDeString = new ArrayList<>();
-		String textecopie=new String();
-		textecopie=texte;
-		String texteSansMaj = texte.toLowerCase(); //on met le texte en minuscule
-		String[] caracspeciaux = { "!", "?", ".", "'", "  " }; //creer le tableau de la ponctuation pour le remplacer
+
+		String TexteAEpurer = epuration(texte); // on utilise la méthode epurer pour epurer le texte
+		String[] tabStringTexte = TexteAEpurer.split(" "); //on split 
+
+		//for (int i = 0; i < tabStringTexte.length; i++) {
+			// System.out.println(tabStringTexte[i]);
+		//}
+		
+		for (int i = 0; i < tabStringTexte.length; i++) {
+			Mot mot=new Mot(tabStringTexte[i]);
+			for (int j = 0; j < tabStringTexte[i].length(); j++) {
+				String lettre=tabStringTexte[i].substring(j, j+1);
+				CleDico cleDico=new CleDico(lettre, tabStringTexte[i].length());
+				ArrayList<Mot> lst=get(cleDico); 
+				if (lst.contains(mot)) {
+					mot.plusUn();		
+				}
+				else{
+					lst.add(mot);
+				}
+			}for (int j = 0; j < tabStringTexte.length; j++) {
+				
+			}
+			System.out.println();
+		}
+	}
+
+	public String epuration(String texte) {
+		String texteepure = new String();
+		String texteSansMaj = texte.toLowerCase(); // on met le texte en minuscule
+		String[] caracspeciaux = { "!", "?", ".", "'", "  " }; // creer le tableau de la ponctuation pour le remplacer
 		for (String string : caracspeciaux) {
-			textecopie = texteSansMaj.replace(string, " ");
+			texte = texteSansMaj.replace(string, " ");
 			texteSansMaj = texte;
 		}
-		textecopie = texte.replace("é", "e");
-		textecopie = texte.replace("è", "e");
-		textecopie = texte.replace("ê", "e");
-		textecopie = texte.replace("à", "a");
-		textecopie = texte.replace("â", "a");
-		textecopie = texte.replace("ù", "u");
-		textecopie = texte.replace("ô", "o");
-		String[] tabStringTexte = textecopie.split(" ");
-		for (int i = 0; i < tabStringTexte.length; i++) {
-			//System.out.println(tabStringTexte[i]);
-		}
-		for (int i = 0; i < tabStringTexte.length; i++) {
-			listeDeString.add(tabStringTexte[i]);
-			
-		}
-		System.out.println(listeDeString);
-		ArrayList<Mot> listeDeMots=new ArrayList<Mot>();
-		for (String string : listeDeString) {
-			Mot mot=new Mot(string);
-			if (listeDeMots.contains(mot)) {
-				mot.plusUn();
-			}
-			listeDeMots.add(mot);
-		}
-
+		texte = texte.replace("é", "e");
+		texte = texte.replace("è", "e");
+		texte = texte.replace("ê", "e");
+		texte = texte.replace("à", "a");
+		texte = texte.replace("â", "a");
+		texte = texte.replace("ù", "u");
+		texte = texte.replace("ô", "o");
+		texteepure = texte;
+		return texteepure;
 	}
 
 	@Override
