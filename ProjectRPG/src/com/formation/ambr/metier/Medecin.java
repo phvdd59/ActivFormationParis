f492b296deb3db3ae5bc57741a8e6461841b8d89
activ_admin <activ_admin@activ_admin-PC>
2016-07-29 15:25:31 +0200

    medecin

--- ProjectRPG/src/com/formation/ambr/metier/Medecin.java
+++ ProjectRPG/src/com/formation/ambr/metier/Medecin.java
@@ -1,30 +1,25 @@
 package com.formation.ambr.metier;
 
 import com.formation.emma.inter.Soin;
+import com.formation.emma.metier.PremierSoin;
 import com.formation.joca.metier.Personnage;
 import com.formation.made.inter.Deplacement;
+import com.formation.made.metier.Marcher;
 import com.formation.thde.inter.EspritCombatif;
+import com.formation.thde.metier.CombatCouteau;
 
-public class Medecin extends Personnage implements Soin, Deplacement, EspritCombatif {
+public class Medecin extends Personnage {
+
+	public Medecin() {
+		super();
+		this.combat = new CombatCouteau();
+		this.soin = new PremierSoin();
+		this.deplacement = new Marcher();
+	}
 
 	public Medecin(EspritCombatif combat, Deplacement deplacement, Soin soin) {
 		super(combat, deplacement, soin);
 		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void combat() {
-		System.out.println("J'injecte une solution de curare");
-	}
-
-	@Override
-	public void deplacer() {
-		System.out.println("Je me déplace super vite grâce à mes prothèses bioniques");
-	}
-
-	@Override
-	public void soigne() {
-		System.out.println("Je diagnostique et réalise les soins nécessaires");
 	}
 
 }
