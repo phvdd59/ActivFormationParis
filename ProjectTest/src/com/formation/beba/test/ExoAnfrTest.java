278ca695e292e75c6b10bba4cdf72e5ec6b9b69c
active_admin <active_admin@LAPTOP-88DE7C7M>
2016-08-02 17:30:38 +0200

    re

---
+++ ProjectTest/src/com/formation/beba/test/ExoAnfrTest.java
@@ -0,0 +1,67 @@
+package com.formation.beba.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExoAnfrTest {
+
+	@Before
+
+	@Test
+	public void testExoAnfr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExoAnfrString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDateEmbauche() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCoordonee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMdpCache() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAdresseMail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDateEmbauche() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMdp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNom() {
+		fail("Not yet implemented");
+	}
+
+}
