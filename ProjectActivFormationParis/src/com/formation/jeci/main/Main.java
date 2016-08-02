832204ea4da171da7f48a3a3073ebc33a47b3727
ACTIVjeci <jeremyciamous@gmail.com>
2016-08-02 15:42:11 +0200

    éé

--- ProjectActivFormationParis/src/com/formation/jeci/main/Main.java
+++ ProjectActivFormationParis/src/com/formation/jeci/main/Main.java
@@ -1,14 +1,51 @@
 package com.formation.jeci.main;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import com.formation.jeci.exo.CleDico;
+import com.formation.jeci.exo.Dico;
 import com.formation.jeci.exo.ExoTableau1;
+import com.formation.jeci.exo.Mot;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Main tab = new Main();
-		tab.init();
+//		tab.init();
+		tab.init2();
+		
 
 	}
+
+	private void init2() {
+		
+		Dico test=new Dico();
+	String test1="SAlut a les thunes yya yya olive lès t'as reçu les  les les thùnés?";
+	
+	test.ranger(test1);
+	ArrayList<String> tralala = test.getListeMot("l", 5);
+	System.out.println(tralala);
+	
+//	TreeMap<Integer,ArrayList<Integer>> map=new TreeMap<Integer,ArrayList<Integer>>();
+//		for (int j = 1; j <= 5; j++) {
+//		int key=j;
+//			ArrayList<Integer> dico = new ArrayList<Integer>();
+//
+//			for (int k = 0; k < 10; k++) {
+//				
+//					dico.add(k);
+//					
+//				}map.put(key, dico);
+//				
+//			}System.out.print(map);
+//			
+//	
+//
+	}
+		
+	
+
 
 	private void init() {
 		// ExoSimple exo1 = new ExoSimple();
---
+++ ProjectTest/src/com/formation/jeci/main/MainTest.java
@@ -0,0 +1,10 @@
+package com.formation.jeci.main;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
