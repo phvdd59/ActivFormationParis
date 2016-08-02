24cd134381cf83e95bcc23133abc19fec3146ec8
active_admin <active_admin@LAPTOP-1IMLDR5T>
2016-08-02 15:41:11 +0200

    Come Eat Me Bro!

---
+++ ProjectTest/src/com/formation/anfr/main/MainTest.java
@@ -0,0 +1,15 @@
+package com.formation.anfr.main;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		MainTest mainTest = new MainTest();
+		mainTest.init();
+
+	}
+
+	private static void init() {
+
+	}
+
+}
