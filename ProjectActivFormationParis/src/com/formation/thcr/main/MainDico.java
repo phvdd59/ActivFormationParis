84b028d6458a1ebe818937523ce1d25b63604b01
Thomas <Thomas@DESKTOP-JJT3DHP>
2016-08-02 09:19:56 +0200

    //
    Thomas Cresson

--- ProjectActivFormationParis/src/com/formation/thcr/main/MainDico.java
+++ ProjectActivFormationParis/src/com/formation/thcr/main/MainDico.java
@@ -1,9 +1,5 @@
 package com.formation.thcr.main;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
 import com.formation.thcr.exo.CleDico;
 import com.formation.thcr.exo.Dico;
 
@@ -16,21 +12,19 @@ public class MainDico {
 	}
 
 	private void init() {
-		
-		
+
 		String s = "Bonjour, bananes, abricot, melons, origan"
 				+ " JAsmin?, lARd, lard, lard, Loup, lArd, louP, loup.";
-//		String s2 = "a aa aaa abb aa aaa b abb abb aaa b bb bbb abb aaba bab bbab";
+		//		String s2 = "a aa aaa abb aa aaa b abb abb aaa b bb bbb abb aaba bab bbab";
 		Dico dico = new Dico();
-		
+
 		System.out.println(s);
 		dico.ranger(s);
 		System.out.println();
-		CleDico cle = new CleDico("l",4);
+		CleDico cle = new CleDico("l", 4);
 		System.out.println(dico.getListeMot(cle.getLettre(), cle.getLngMot()));
 		System.out.println(dico.get(cle));
-		
-		
+
 	}
-	
+
 }
