dbb1b492622dc9c690c6f4ebf56d5dc1a23ca503
VDDMichel <VDDMichel@VDDMichel-PC>
2016-08-02 15:43:24 +0200

    m	de

--- ProjectActivFormationParis/src/com/formation/made/exo/Mot.java
+++ ProjectActivFormationParis/src/com/formation/made/exo/Mot.java
@@ -9,6 +9,7 @@ public class Mot implements InterExoMot1, Comparable<Mot> {
 
 	public Mot(String mot) {
 		this.mot = mot;
+		nb=1;
 	}
 
 	public String getMot() {
@@ -29,8 +30,11 @@ public class Mot implements InterExoMot1, Comparable<Mot> {
 
 	@Override
 	public int compareTo(Mot o) {// longeur du mot si c'est superieur c'est positif, si c'est inférieur c'est négatif, si c'est égal c'est 0
-
-		return this.compareTo(o);
+		int res = -Integer.valueOf(getNb()).compareTo((Integer.valueOf(o.getNb()))); // c'est pour trier l'arrayliste de ot l'array liste. on met un met car en temps normal il classe du plus petit au plus grand et la on veut du plus grand au plus petit.
+		if (res == 0) {
+			res = getMot().compareTo(o.getMot());
+		}
+		return res;
 
 	}
 
