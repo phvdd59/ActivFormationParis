2171d8000707242e96f79cdc980a50ade16c3c8c
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-08-02 16:59:44 +0200

    tedvj

--- ProjectTest/src/com/formation/etga/metier/ExoEtga.java
+++ ProjectTest/src/com/formation/etga/metier/ExoEtga.java
@@ -1,40 +1,87 @@
 package com.formation.etga.metier;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ExoEtga {
-	
+
 	private String nom;
 	private String adresseMail;
-	private String dateEmbauche; // jj-mm-yyyy
+	private String dateEmbauche; // jj-MM-yyyy
 	private String tel; // +33601020304
 	private String mdp; // majuscule minuscule chiffre
-	
+
 	public ExoEtga() {
-		
 	}
-	
-	
-	public ExoEtga (String nom) {
-		//TODO
+
+	public ExoEtga(String nom) {
+		this.nom = nom;
 	}
-	
-	public void setDate (Date date) {
-		
+
+	public void setTel(int numero) {
+		tel = Integer.toString(numero);
 	}
-	
-	public void setCoordonnee (String adresseMail, String tel) {
-		
+
+	public void setDate(Date date) {
+		SimpleDateFormat dateEmbauche = new SimpleDateFormat("dd-MM-yyyy");
+		//		String date = dateFormat.format(new Date());
+		//		date = dateFormat.parse(dateEmbauche);
+		//		return dateEmbauche;
+
 	}
-	
+
+	public void setCoordonnee(String adresseMail, String tel) {
+		this.adresseMail = adresseMail;
+		this.tel = tel;
+	}
+
 	/**
-	 * permutation 2 en 2 du mdp
-	 * mdp : ABcdEF --> BAdcFE
+	 * permutation 2 en 2 du mdp mdp : cache ---> clair BAdcFE --> ABcdEF
 	 * 
-	 *  @param mdpCache
+	 * @param mdpCache
 	 */
-	
-	public void setMdpCache (String mdpCache) {
-		
+
+	public void setMdpCache(String mdpCache) {
+
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getAdresseMail() {
+		return adresseMail;
+	}
+
+	public String getDateEmbauche() {
+		return dateEmbauche;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setAdresseMail(String adresseMail) {
+		this.adresseMail = adresseMail;
+	}
+
+	public void setDateEmbauche(String dateEmbauche) {
+		this.dateEmbauche = dateEmbauche;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
 	}
 }
