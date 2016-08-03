1e559db0573505a255b38ec46fa046524d1062c9
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-03 14:00:23 +0200

    exo4

--- ProjectActivFormationParis/src/com/formation/phva/cor/ExoAlgo4.java
+++ ProjectActivFormationParis/src/com/formation/phva/cor/ExoAlgo4.java
@@ -1,4 +1,4 @@
-package com.formation.phva.exo;
+package com.formation.phva.cor;
 
 import java.util.ArrayList;
 
@@ -7,12 +7,33 @@ import com.formation.phva.exception.CruciException;
 import com.formation.phva.exception.CruciHeightException;
 import com.formation.phva.exception.CruciNullException;
 import com.formation.phva.exception.CruciWidthException;
+import com.formation.phva.exo.InterAlgo4;
 import com.formation.phva.exo.Terme;
 
 public class ExoAlgo4 implements com.formation.phva.exo.InterAlgo4 {
 
 	/**
+	 * ma methode solution doit intégrer une liste de terme sous forme 
+	 * d'un tableau à deux dimensions 
 	 * 
+	 * les cas annormaux seront traité en grande partie sur la base des 
+	 * exeption cruciExeption.
+	 * 
+	 * example :
+	 * 
+     * "DATTIERS"
+     * "EREINTEE"
+     * "COMEDIEN"
+     * "AMENER T"
+     * " ART API"
+     * "OTA MIAM"
+     * "BEIGE RE"
+     * "S REGAIN"
+     * "EPELER T"
+     * "DES VELA"
+     * "ET METAL"
+     * "REPU ESE"
+     *  
 	 */
 	@Override
 	public char[][] solution(int width, int height, ArrayList<Terme> lst) throws CruciException {
