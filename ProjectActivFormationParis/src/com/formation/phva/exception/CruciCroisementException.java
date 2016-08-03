d09b76b677ff8d4ed2296bb7346d8d940a23314e
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-03 11:43:05 +0200

    Modif exoAlgo4

---
+++ ProjectActivFormationParis/src/com/formation/phva/exception/CruciCroisementException.java
@@ -0,0 +1,24 @@
+package com.formation.phva.exception;
+
+import com.formation.phva.exo.Terme;
+
+public class CruciCroisementException extends CruciException {
+
+	private com.formation.phva.exo.Terme terme;
+	private int pos;
+
+	public CruciCroisementException() {
+		super();
+	}
+
+	public CruciCroisementException(Terme terme,int pos) {
+		super();
+		this.terme = terme;
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage() + "Croisement incorrect : "+terme.toString()+" position : "+pos+" ==> Lettre remplacé.";
+	}
+
+}
