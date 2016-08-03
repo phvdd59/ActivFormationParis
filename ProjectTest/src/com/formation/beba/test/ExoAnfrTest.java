b6ef4eda6f8f5b31e80042ccd32efec4200cafa5
active_admin <active_admin@LAPTOP-88DE7C7M>
2016-08-03 08:55:25 +0200

    junit

--- ProjectTest/src/com/formation/beba/test/ExoAnfrTest.java
+++ ProjectTest/src/com/formation/beba/test/ExoAnfrTest.java
@@ -1,7 +1,6 @@
 package com.formation.beba.test;
 
-import static org.junit.Assert.fail;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,58 +9,62 @@ public class ExoAnfrTest {
 	@Before
 
 	@Test
-	public void testExoAnfr() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExoAnfrString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetNom() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetDateEmbauche() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetCoordonee() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testSetMdpCache() {
-		fail("Not yet implemented");
+		Assert.assertEquals("Gros nab ça ne marche pas", "AbCdEfg", "bAdCfEg");
 	}
-
-	@Test
-	public void testGetAdresseMail() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetDateEmbauche() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTel() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetMdp() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetNom() {
-		fail("Not yet implemented");
-	}
+	// @Test
+	// public void testExoAnfr() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testExoAnfrString() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testGetNom() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testSetDateEmbauche() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testSetCoordonee() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testSetMdpCache() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testGetAdresseMail() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testGetDateEmbauche() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testGetTel() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testGetMdp() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testSetNom() {
+	// fail("Not yet implemented");
+	// }
 
 }
