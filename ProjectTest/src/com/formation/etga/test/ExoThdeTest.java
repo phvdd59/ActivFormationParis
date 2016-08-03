6a2ac13de9e6f447d1e2a87396c1a2a83477159a
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-08-03 12:12:57 +0200

    dfgdgd

--- ProjectTest/src/com/formation/etga/test/ExoThdeTest.java
+++ ProjectTest/src/com/formation/etga/test/ExoThdeTest.java
@@ -2,6 +2,10 @@ package com.formation.etga.test;
 
 import static org.junit.Assert.fail;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,7 +24,7 @@ public class ExoThdeTest {
 	@Test
 	public void testSetTel() {
 		exoThde.setTel(0601020304);
-		Assert.assertEquals("num de tel incorrect", "+33601020304", exoThde.getTel());
+		Assert.assertEquals("num de tel incorrect", "+33600000000", exoThde.getTel());
 	}
 
 	@Test
@@ -30,17 +34,59 @@ public class ExoThdeTest {
 
 	@Test
 	public void testSetDate() {
-		fail("Not yet implemented");
+		SimpleDateFormat dateEmbauche = new SimpleDateFormat("dd-MM-yyyy");
+		try {
+			Date d = dateEmbauche.parse("01-02-2001");
+			exoThde.setDate(d);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		Assert.assertEquals("date OK", "01-02-2001", exoThde.getDateEmbauche());
+	}
+
+	@Test
+	public void testSetDateHS() {
+		SimpleDateFormat dateEmbauche = new SimpleDateFormat("dd-MM-yyyy");
+		try {
+			Date d1 = dateEmbauche.parse("13-12-2050");
+			exoThde.setDate(d1);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+		Assert.assertEquals("date pas OK", "01-01-1900", exoThde.getDateEmbauche());
 	}
 
 	@Test
 	public void testSetCoordonnees() {
-		fail("Not yet implemented");
+		exoThde.setCoordonnees("thde@gmail.com", "+33600123000");
+		Assert.assertEquals("Mail OK", "thde@gmail.com", exoThde.getAdresseMail());
+		Assert.assertEquals("Tel OK", "+33600123000", exoThde.getTel());
+	}
+
+	@Test
+	public void testSetCoordonneesMailHS() {
+		exoThde.setCoordonnees("thdegmail", "+33600123000");
+		Assert.assertEquals("Mail Pas OK", "pasbon@wanadoo.fr", exoThde.getAdresseMail());
+		Assert.assertEquals("Tel OK", "+33600123000", exoThde.getTel());
+	}
+
+	@Test
+	public void testSetCoordonneesTelHS() {
+		exoThde.setCoordonnees("thde@gmail.com", "060000000");
+		Assert.assertEquals("Mail OK", "thde@gmail.com", exoThde.getAdresseMail());
+		Assert.assertEquals("Tel Pas OK", "+33600000000", exoThde.getTel());
 	}
 
 	@Test
 	public void testSetMdpCache() {
-		fail("Not yet implemented");
+		exoThde.setMdpCache("ABcd08");
+		Assert.assertEquals("Mdp OK", "BAdc80", exoThde.getMdp());
+	}
+
+	@Test
+	public void testSetMdpCacheMdpHS() {
+		exoThde.setMdpCache("@zi!!");
+		Assert.assertEquals("Mdp Pas OK", "Abc0", exoThde.getMdp());
 	}
 
 }
