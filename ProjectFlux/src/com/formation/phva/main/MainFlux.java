8bcd2a362a9e5dbb72cca3bb11f0d4dc56bb708a
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-04 13:53:26 +0200

    Image

--- ProjectFlux/src/com/formation/phva/main/MainFlux.java
+++ ProjectFlux/src/com/formation/phva/main/MainFlux.java
@@ -2,15 +2,67 @@ package com.formation.phva.main;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
+import java.io.PrintWriter;
 
 public class MainFlux {
 
 	public static void main(String[] args) {
 		MainFlux m = new MainFlux();
-		m.init();
+		m.initLecture();
+		m.initImage();
+	}
+
+	public void initImage() {
+
+	}
+
+	public void initLecture() {
+		File file = new File("./src/com/formation/phva/data/texte.txt");
+		BufferedReader bIn = null;
+		InputStreamReader inputStreamReader = null;
+		try {
+			inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+			bIn = new BufferedReader(inputStreamReader);
+			String line = bIn.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = bIn.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bIn.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void initEcriture() {
+		File file = new File("./src/com/formation/phva/data/texte.txt");
+		PrintWriter out = null;
+		try {
+			System.out.println(file.getCanonicalPath());
+			out = new PrintWriter(file);
+			out.println("Bonjour tout le monde.");
+			out.println("ceci doit apparaitre dans le fichier");
+			out.flush();
+			out.print("Suite de la phrase");
+			out.print("Autre Suite de la phrase");
+			out.println("32Autre Suite de la phrase");
+		} catch (IOException e) {
+			System.out.println("recommencer le traitement");
+		} finally {
+			out.close();
+		}
+
 	}
 
 	public void init() {
@@ -28,17 +80,13 @@ public class MainFlux {
 						File[] fn = null;
 						fn = f.listFiles();
 						for (int i = 0; i < fn.length; i++) {
-							String nomDuFile=null;
-							nomDuFile=fn[i].getName();
+							String nomDuFile = null;
+							nomDuFile = fn[i].getName();
 							System.out.println(nomDuFile);
-							
+
 						}
 					}
 				}
-				// f exists ?
-				// f repertoire
-				// si c'est le cas >>>>>>>
-
 				System.out.println("existe : " + f.exists() + " repertoire " + ((f.isDirectory()) ? "OUI" : "NON") + " path " + f.getCanonicalPath());
 				ligne = stdIn.readLine();
 			}
