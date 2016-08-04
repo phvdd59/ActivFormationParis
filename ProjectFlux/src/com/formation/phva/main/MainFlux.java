7035fc53eb5f3f4f8a12c62748a0c1c523f2460c
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-04 08:16:44 +0200

    exo4

--- ProjectFlux/src/com/formation/phva/main/MainFlux.java
+++ ProjectFlux/src/com/formation/phva/main/MainFlux.java
@@ -1,14 +1,55 @@
 package com.formation.phva.main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+
 public class MainFlux {
 
 	public static void main(String[] args) {
-		MainFlux m=new MainFlux();
+		MainFlux m = new MainFlux();
 		m.init();
 	}
-	
-	public void init() {
-		
-	}
 
+	public void init() {
+		BufferedReader stdIn = null;
+		stdIn = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			File fileTest = new File("c:/DevFormation");
+			System.out.println(fileTest.exists());
+			String ligne = stdIn.readLine();
+			while (!ligne.equals("")) {
+				System.out.println(ligne);
+				File f = new File(ligne);
+				if (f.exists()) {
+					if (f.isDirectory()) {
+						File[] fn = null;
+						fn = f.listFiles();
+						for (int i = 0; i < fn.length; i++) {
+							String nomDuFile=null;
+							nomDuFile=fn[i].getName();
+							System.out.println(nomDuFile);
+							
+						}
+					}
+				}
+				// f exists ?
+				// f repertoire
+				// si c'est le cas >>>>>>>
+
+				System.out.println("existe : " + f.exists() + " repertoire " + ((f.isDirectory()) ? "OUI" : "NON") + " path " + f.getCanonicalPath());
+				ligne = stdIn.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stdIn.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
