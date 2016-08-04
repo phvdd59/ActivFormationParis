3d34ed9789a2b01b0b80ce63d226591749a680da
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-08-04 10:31:43 +0200

    algo4

---
+++ ProjectActivFormationParis/src/com/formation/etga/exo/ExoAlgo4.java
@@ -0,0 +1,40 @@
+package com.formation.etga.exo;
+
+import java.util.ArrayList;
+
+import com.formation.phva.exception.CruciException;
+import com.formation.phva.exo.Terme;
+
+public class ExoAlgo4 implements com.formation.phva.exo.InterAlgo4 {
+
+	/**
+	 * ma methode solution doit intégrer une liste de terme sous forme d'un
+	 * tableau à deux dimensions
+	 * 
+	 * 
+	 * example :
+	 * 
+	 * "DATTIERS" "EREINTEE" "COMEDIEN" "AMENER T" " ART API" "OTA MIAM"
+	 * "BEIGE RE" "S REGAIN" "EPELER T" "DES VELA" "ET METAL" "REPU ESE"
+	 *
+	 * les cas annormaux seront traité en grande partie sur la base des
+	 * exeption cruciExeption.
+	 * 
+	 */
+	@Override
+	public char[][] solution(int width, int height, ArrayList<Terme> lst) throws CruciException {
+
+		char[][] tabSolution = new char[height][width];
+
+		int val = 0;
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				tabSolution[i][j] = lst.get(val).getNom();
+				val++;
+			}
+
+		}
+
+		return null;
+	}
+}
