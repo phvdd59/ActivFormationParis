4bf690106a456c6005cfb4573f02b2981dcfa85c
active_admin <active_admin@LAPTOP-FP1TSI7G>
2016-08-04 12:02:02 +0200

    thde

--- ProjectFlux/src/com/formation/thde/main/MainFlux.java
+++ ProjectFlux/src/com/formation/thde/main/MainFlux.java
@@ -53,8 +53,7 @@ public class MainFlux {
 			out.println("Bonjour tout le monde");
 			out.println("ceci doit apparaitre dans le fichier");
 			out.flush();
-			out.print("(ノಠ益ಠ)ノ彡┻━┻");
-			out.print("Autre suite de la phrase");
+			out.print("Test Test");
 		} catch (IOException e) {
 			System.out.println("recommencer le traitement");
 		} finally {
