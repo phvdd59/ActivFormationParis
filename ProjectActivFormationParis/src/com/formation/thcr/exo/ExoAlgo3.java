aea2d6dd90c8a09a21b7c73723b80401769b1e52
Thomas <Thomas@DESKTOP-JJT3DHP>
2016-08-04 09:05:07 +0200

    Dico
    Thomas Cresson

--- ProjectActivFormationParis/src/com/formation/thcr/exo/ExoAlgo3.java
+++ ProjectActivFormationParis/src/com/formation/thcr/exo/ExoAlgo3.java
@@ -8,18 +8,20 @@ import com.formation.phva.exo.Mouvement;
 /**
  * Réaliser un algorithme qui respecte le comportement des tours de Hanoi
  * 
- * il n'est possible de bouger que 1 élément a la fois
- * un élément ne peut être déplacé que sur la base d'une autre tour ou d'un élément de taille plus grand
+ * il n'est possible de bouger que 1 élément a la fois un élément ne peut être
+ * déplacé que sur la base d'une autre tour ou d'un élément de taille plus grand
  * 
- * Déplacé tout les éléments d'une tour vers une autre tour pour les retrouver dans l'ordre.
+ * Déplacé tout les éléments d'une tour vers une autre tour pour les retrouver
+ * dans l'ordre.
  * 
  * @author philippe
  *
  */
 public class ExoAlgo3 implements ImplAlgo3 {
-	
-	public ArrayList<Mouvement> solution(ArrayList<Mouvement> lst, int nbElements, String a, String b, String c) {
-		
+
+	public ArrayList<Mouvement> solution(ArrayList<Mouvement> lst, int nbElements, String a,
+			String b, String c) {
+
 		ArrayList<Integer> A = new ArrayList<Integer>();
 		for (int i = 1; i <= nbElements; i++) {
 			A.add(Integer.valueOf(nbElements - i));
@@ -27,9 +29,9 @@ public class ExoAlgo3 implements ImplAlgo3 {
 		// Initialisation de la première colonne
 		ArrayList<Integer> B = new ArrayList<Integer>();
 		ArrayList<Integer> C = new ArrayList<Integer>();
-		while(B.size() != nbElements || C.size() != nbElements){
-			if(B.size()==0){
-				if(A.get(A.size()-1)< B.get(B.size())){
+		while (B.size() != nbElements || C.size() != nbElements) {
+			if (B.size() == 0) {
+				if (A.get(A.size() - 1) < B.get(B.size())) {
 					B.add(B.size(), A.remove(A.size()));
 				}
 			}
