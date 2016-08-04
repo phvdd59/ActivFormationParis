dc206c677e08eca648dca4347f1a0946868b815f
activ_admin <activ_admin@activ_admin-PC>
2016-08-04 11:53:25 +0200

    texteissa

--- ProjectFlux/src/com/formation/issa/main/MainFlux.java
+++ ProjectFlux/src/com/formation/issa/main/MainFlux.java
@@ -2,14 +2,65 @@ package com.formation.issa.main;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class MainFlux {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		MainFlux m = new MainFlux();
-		m.init();
+		m.initEcriture();
+
+	}
+
+	public void initLecture() {
+		File file = new File("./src/com/formation/issa/data/texte.txt");
+		BufferedReader bIn = null;
+		try {
+			bIn = new BufferedReader(new FileReader(file));
+			String line = bIn.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = bIn.readLine();
+			}
+
+		} catch (
+
+		FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+
+				bIn.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void initEcriture() throws IOException {
+		File file = new File("./src/com/formation/issa/data/texte.txt");
+		PrintWriter out = null;
+		try {
+			System.out.println(file.getCanonicalPath());
+			out = new PrintWriter(file);
+			out.println("Bonjour tout le monde.");
+			out.println("ceci doit apparaitre dans le fichier");
+			out.flush();// transmission du flux sur le périphérique
+						// correspondant
+			out.print("Suite de la phrase");
+			out.print(" Et un de plus.");
+		} catch (IOException e) {
+			System.out.println("recommencer le traitement");
+
+		} finally {
+			out.close();
+		}
 
 	}
 
@@ -34,7 +85,7 @@ public class MainFlux {
 							System.out.println(nomDuFile);
 							if (fn[i].isFile()) {
 								if (nomDuFile.contains(".")) {
-									System.out.println(nomDuFile.substring(nomDuFile.lastIndexOf(".")+1));
+									System.out.println(nomDuFile.substring(nomDuFile.lastIndexOf(".") + 1));
 								}
 							}
 
