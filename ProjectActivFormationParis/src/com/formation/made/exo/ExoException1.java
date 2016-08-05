319c0d1c44d5cda3c657db0aad67b8e3431ab757
VDDMichel <VDDMichel@VDDMichel-PC>
2016-08-05 09:59:01 +0200

    made

--- ProjectActivFormationParis/src/com/formation/made/exo/ExoException1.java
+++ ProjectActivFormationParis/src/com/formation/made/exo/ExoException1.java
@@ -14,20 +14,21 @@ import com.formation.phva.exo.Terme;
 public class ExoException1 implements InterException1 {
 
 	public char[][] solution(int width, int height, ArrayList<com.formation.phva.exo.Terme> lst) throws CruciException {
-
-		if (lst == null) {
-			throw new CruciNullException();
-		} else if (width <= 0) {
+		if (width <= 0) {
 			throw new CruciWidthException();
 		} else if (height <= 0) {
 			throw new CruciHeightException();
-		} else {
-			char[][] cruci = new char[height][width];
-			for (int i = 0; i < cruci.length; i++) {
-				for (int j = 0; j < cruci[0].length; j++) {
-					cruci[i][j] = ' ';
-				}
+		}
+		char[][] cruci = new char[height][width];
+		for (int i = 0; i < cruci.length; i++) {
+			for (int j = 0; j < cruci[0].length; j++) {
+				cruci[i][j] = ' ';
 			}
+		}
+		if (lst == null) {
+			throw new CruciNullException();
+		} else {
+
 			for (Terme terme : lst) {
 				char[] tab = terme.getNom().toCharArray();
 				if (terme.isSens() == Terme.HORIZONTAL) {
