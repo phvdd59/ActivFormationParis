8cbfa5426a92d0b641563eb68dd03738dddda79d
activ_admin <activ_admin@activ_admin-PC>
2016-08-05 10:10:25 +0200

    ExoException1

--- ProjectFlux/src/com/formation/ambr/main/MainFlux.java
+++ ProjectFlux/src/com/formation/ambr/main/MainFlux.java
@@ -1,26 +1,128 @@
 package com.formation.ambr.main;
 
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+
+import com.formation.ambr.metier.Terme;
 
 public class MainFlux {
 
 	public static void main(String[] args) throws IOException {
 		MainFlux mainFlux = new MainFlux();
-		mainFlux.initEcriture();
+		mainFlux.initEcritureObject();
+		mainFlux.initLectureObject();
+
+	}
+
+	private void initLectureObject() {
+		File file = new File("./src/com/formation/ambr/data/terme1.ser");
+		ObjectInputStream iStream = null;
+
+		try {
+			iStream = new ObjectInputStream(new FileInputStream(file));
+			while (true) {
+				Object o = iStream.readObject();
+				if (o instanceof String) {
+					String s = (String) o;
+					System.out.println(s);
+				} else if (o instanceof Terme) {
+					Terme s = (Terme) o;
+					System.out.println(s);
+				}
+				Object a = iStream.readObject();
+				if (a instanceof String) { // instanceof c'est pour vérifier le type
+					String s = (String) a;
+					System.out.println(s);
+				}
+			}
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				iStream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public void initEcritureObject() {
+		Terme terme = new Terme("wagon", new Point(2, 9), Terme.HORIZONTAL);
+		File file = new File("./src/com/formation/ambr/data/terme1.ser"); // ser comme serializer
+		ObjectOutputStream oStream = null;
+		try {
+			oStream = new ObjectOutputStream(new FileOutputStream(file));
+			oStream.writeObject("amaury");
+			oStream.writeObject(terme);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				oStream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public void initImage() {
+		File fileIn = new File("./src/com/formation/phva/data/image2.bmp"); // image: c'est en binaire. Donc du input/output stream
+		File fileOut = new File("./src/com/formation/ambr/data/image2.bmp");
+		FileInputStream fluxDentree = null;
+		FileOutputStream fluxDeSortie = null;
+		int readUnByte;
+
+		try {
+			fluxDentree = new FileInputStream(fileIn);
+			fluxDeSortie = new FileOutputStream(fileOut);
+			readUnByte = fluxDentree.read();
+			while (readUnByte != -1) {
+				//readUnByte = (-1 ^ readUnByte) & 0xFF; // 0xFF c'est un int. readUnByte c'est un octet de notre image
+				fluxDeSortie.write(readUnByte);
+				readUnByte = fluxDentree.read();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fluxDentree.close();
+				fluxDeSortie.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
 	public void initLecture() {
-		File file = new File("./src/com/formation/ambr/data/texte.txt");
-		BufferedReader bIn = null; // déclaration
+		File file = new File("./src/com/formation/phva/data/texte.txt");
+		BufferedReader bIn = null; // dÃ©claration
+		InputStreamReader inputStreamReader = null;
 		try {
-			bIn = new BufferedReader(new FileReader(file)); // instanciation
+			inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8"); // InputStreamReader lit en binaire
+			bIn = new BufferedReader(new FileReader(file)); // instanciation. On buffer pour pouvoir ligne par ligne
 			String line = bIn.readLine();
 			while (line != null) {
 				System.out.println(line);
@@ -34,7 +136,7 @@ public class MainFlux {
 			e.printStackTrace();
 		} finally {
 			try {
-				bIn.close(); // on l'a surround par un try-catch, au cas où ça planterait
+				bIn.close(); // on l'a surround par un try-catch, au cas oÃ¹ Ã§a planterait
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -50,11 +152,11 @@ public class MainFlux {
 			out = new PrintWriter(file);
 			out.println("bonjour tout le monde.");
 			out.println("ceci doit apparaitre dans le fichier.");
-			out.flush(); // flush permet de sauvegarder sur le périphérique spécifié (chemin de fichier spécifié dans File) avec garantie de cette sauvegarde. Ici c'est sur C:
+			out.flush(); // flush permet de sauvegarder sur le pÃ©riphÃ©rique spÃ©cifiÃ© (chemin de fichier spÃ©cifiÃ© dans File) avec garantie de cette sauvegarde. Ici c'est sur C:
 			out.print("suite de la phrase");
 			out.print("autre suite de la phrase");
 			out.print("autre suite de la phrase 2");
-			out.print("autre suite de la phrase 3");
+			out.print("autre suite de la phrase 3â™¥");
 		} catch (IOException e) {
 			System.out.println("recommencer le traitement");
 		} finally {
@@ -65,7 +167,7 @@ public class MainFlux {
 	public void init() {
 
 		BufferedReader stdIn = null;
-		stdIn = new BufferedReader(new InputStreamReader(System.in)); // system.in envoie données du clavier qui sont chiffrées donc fait InputStreamReader pour transformer binaire en character
+		stdIn = new BufferedReader(new InputStreamReader(System.in)); // system.in envoie donnÃ©es du clavier qui sont chiffrÃ©es donc fait InputStreamReader pour transformer binaire en character
 		try {
 			File fileTest = new File("c:/DevFormation");
 			System.out.println(fileTest.exists());
@@ -78,8 +180,8 @@ public class MainFlux {
 				System.out.println("existe : " + f.exists() + " ; repertoire : " + (f.isDirectory() ? " oui" : "non") + " ; path " + f.getCanonicalPath());
 				if (f.exists()) {
 					if (f.isDirectory()) {
-						File[] fn = null; // ça c'est de la déclaration. Là il n'existe pas. Il n'y aa que le compilateur qui sait que fn existe
-						fn = f.listFiles(); // ça c'est l'instanciation. Là il existe. Si on fait pas ça, il ne met rien en mémoire
+						File[] fn = null; // Ã§a c'est de la dÃ©claration. LÃ  il n'existe pas. Il n'y aa que le compilateur qui sait que fn existe
+						fn = f.listFiles(); // Ã§a c'est l'instanciation. LÃ  il existe. Si on fait pas Ã§a, il ne met rien en mÃ©moire
 						for (int i = 0; i < fn.length; i++) {
 							String nomDuFile = null;
 							nomDuFile = fn[i].getName();
@@ -90,7 +192,7 @@ public class MainFlux {
 							if (fn[i].isFile() && nomDuFile.contains(".")) {
 								int a = nomDuFile.lastIndexOf(".");
 								String nomExtens = null;
-								nomExtens = nomDuFile.substring(a + 1); // automatiquement il va de a jusqu'à la fin du mot, donc pas besoin d'écrire "nomDuFile.length()"
+								nomExtens = nomDuFile.substring(a + 1); // automatiquement il va de a jusqu'Ã  la fin du mot, donc pas besoin d'Ã©crire "nomDuFile.length()"
 								System.out.println(nomExtens);
 							}
 
