1c2dfff350e6556d7a2452f29134c9755c60f291
active_admin <active_admin@LAPTOP-88DE7C7M>
2016-08-05 09:33:30 +0200

    exo exception

--- ProjectFlux/src/com/formation/beba/main/MainFlux.java
+++ ProjectFlux/src/com/formation/beba/main/MainFlux.java
@@ -1,36 +1,112 @@
 package com.formation.beba.main;
 
+import java.awt.Point;
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.Arrays;
+
+import com.formation.beba.Metier.Terme;
 
 public class MainFlux {
 
 	public static void main(String[] args) throws Exception {
 		MainFlux mainFlux = new MainFlux();
+		// mainFlux.init();
 		// mainFlux.initLecture();
 		// mainFlux.initEcriture();
-		mainFlux.initImage();
+		// mainFlux.initImage();
+		mainFlux.initObjet();
+		mainFlux.initLireObjet();
+	}
+
+	public void initLireObjet() throws Exception {
+
+		File file = new File("./src/com/formation/beba/data/terme1.ser");
+		ObjectInputStream IStream = null;
+
+		try {
+			IStream = new ObjectInputStream(new FileInputStream(file));
+			// Object a = IStream.readObject();
+			while (true) {
+				Object a = IStream.readObject();
+				if (a instanceof String) {
+					String s = (String) a;
+					System.out.println(s);
+				} else if (a instanceof Terme) {
+					Terme s = (Terme) a;
+
+					System.out.println(s);
+				}
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (EOFException e) {
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+			IStream.close();
+
+		}
+
+	}
+
+	public void initObjet() throws Exception {
+		Terme terme = new Terme("wagon", new Point(2, 9), Terme.HORIZONTAL);
+		File file = new File("./src/com/formation/beba/data/terme1.ser");
+		ObjectOutputStream oStream = null;
+
+		try {
+			oStream = new ObjectOutputStream(new FileOutputStream(file));
+			oStream.writeObject("Gros Sac");
+			oStream.writeObject(terme);
+			oStream.writeObject("test");
+			oStream.writeObject("rien");
+			oStream.writeObject(terme);
+			oStream.flush();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				oStream.close();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+
+		}
+
 	}
 
 	public void initImage() throws Exception {
 		FileInputStream fluxDentree = null;
 		FileOutputStream fluxDeSortie = null;
-		File fileIn = new File("./src/com/formation/beba/data/image2.bmp");
-		File fileOut = new File("./src/com/formation/phva/data/image.bmp");
+		File fileIn = new File("./src/com/formation/phva/data/image2.bmp");
+		File fileOut = new File("./src/com/formation/beba/data/image.bmp");
 		int readUnByte;
-
+		int nb = 0;
 		try {
 			fluxDentree = new FileInputStream(fileIn);
 			fluxDeSortie = new FileOutputStream(fileOut);
 			readUnByte = fluxDentree.read();
 			while (readUnByte != -1) {
+				if (nb >= 256) {
+					readUnByte = (-1 ^ readUnByte) & 0xFF;
+				}
+				nb++;
 				fluxDeSortie.write(readUnByte);
 				readUnByte = fluxDentree.read();
 			}
