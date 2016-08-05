1ab63b93f4a9bcef4e70ed5e1426e8b0d1f82d1a
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-05 08:46:01 +0200

    ExoAlgo4 etga

--- ProjectActivFormationParis/src/com/formation/etga/exo/ExoAlgo4.java
+++ ProjectActivFormationParis/src/com/formation/etga/exo/ExoAlgo4.java
@@ -1,5 +1,6 @@
 package com.formation.etga.exo;
 
+import java.awt.Point;
 import java.util.ArrayList;
 
 import com.formation.phva.exception.CruciException;
@@ -23,18 +24,29 @@ public class ExoAlgo4 implements com.formation.phva.exo.InterAlgo4 {
 	 */
 	@Override
 	public char[][] solution(int width, int height, ArrayList<Terme> lst) throws CruciException {
+		Terme t1 = new Terme("AB", new Point(0, 0), Terme.HORIZONTAL);
+		lst.add(t1);
+		Terme t2 = new Terme("AC", new Point(0, 0), Terme.VERTICAL);
+		lst.add(t2);
+		Terme t3 = new Terme("CD", new Point(0, 1), Terme.HORIZONTAL);
+		lst.add(t3);
+		Terme t4 = new Terme("BD", new Point(1, 1), Terme.VERTICAL);
+		lst.add(t4);
+
+		for (int i = 0; i < lst.size(); i++) {
+			char[] nomChar = lst.get(i).getNom().toCharArray();
+		}
 
 		char[][] tabSolution = new char[height][width];
+		int val = 0;
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				//				tabSolution[i][j] = lst.get(val).getNom();
+				val++;
+			}
 
-//		int val = 0;
-//		for (int i = 0; i < width; i++) {
-//			for (int j = 0; j < height; j++) {
-//				//				tabSolution[i][j] = lst.get(val).getNom();
-//				val++;
-//			}
-//
-//		}
+		}
 
-		return tabSolution;
+		return null;
 	}
 }
