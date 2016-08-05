404293cfec3ad9fe158490984aa1f2b5553be927
active_admin <active_admin@LAPTOP-2V3MRONP>
2016-08-05 10:06:01 +0200

    com  etga

---
+++ ProjectActivFormationParis/src/com/formation/etga/exo/ExoException1.java
@@ -0,0 +1,63 @@
+package com.formation.etga.exo;
+
+import java.util.ArrayList;
+
+import com.formation.phva.exception.CruciDebordeException;
+import com.formation.phva.exception.CruciException;
+import com.formation.phva.exception.CruciHeightException;
+import com.formation.phva.exception.CruciNullException;
+import com.formation.phva.exception.CruciWidthException;
+import com.formation.phva.exo.InterException1;
+import com.formation.phva.exo.Terme;
+
+public class ExoException1 implements InterException1 {
+
+	public char[][] solution(int width, int height, ArrayList<com.formation.phva.exo.Terme> lst) throws CruciException {
+
+		char[][] cruci = new char[height][width];
+
+		try {
+			if (height <= 0) {
+				throw new CruciHeightException();
+			}
+			if (width <= 0) {
+				throw new CruciWidthException();
+			}
+
+			for (int i = 0; i < cruci.length; i++) {
+				for (int j = 0; j < cruci[0].length; j++) {
+					cruci[i][j] = ' ';
+				}
+			}
+
+			if (lst == null) {
+				throw new CruciNullException();
+			}
+
+			for (Terme terme : lst) {
+				char[] tab = terme.getNom().toCharArray();
+
+				if (terme.isSens() == Terme.HORIZONTAL) {
+					for (int i = 0; i < tab.length; i++) {
+						cruci[terme.getPos().y][terme.getPos().x + i] = tab[i];
+						if ((tab.length > width) || (terme.getPos().y + tab.length > width)) {
+							throw new CruciDebordeException();
+						}
+					}
+				} else {
+					for (int i = 0; i < tab.length; i++) {
+						cruci[terme.getPos().y + i][terme.getPos().x] = tab[i];
+						if ((tab.length > height) || (terme.getPos().x + tab.length > height)) {
+							throw new CruciDebordeException();
+						}
+					}
+				}
+			}
+		} catch (CruciHeightException e) {
+		} catch (CruciWidthException e) {
+		} catch (CruciNullException e) {
+		} catch (CruciDebordeException e) {
+		}
+		return cruci;
+	}
+}
--- ProjectFlux/src/com/formation/etga/main/MainFlux.java
+++ ProjectFlux/src/com/formation/etga/main/MainFlux.java
@@ -1,27 +1,138 @@
 package com.formation.etga.main;
 
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+
+import com.formation.etga.metier.Terme;
 
 public class MainFlux {
 
 	public static void main(String[] args) {
 		MainFlux m = new MainFlux();
 		//		m.init();
-		m.initEcriture();
-		m.initLecture();
+		//		m.initEcriture();
+		//		m.initLecture();
+		//		m.initImage();
+		m.initEcritureObject();
+		m.initLectureObject();
+	}
+
+	private void initLectureObject() {
+		Terme terme = new Terme("WAGON", new Point(2, 9), Terme.HORIZONTAL);
+		File file = new File("./src/com/formation/etga/data/terme1.ser");
+		ObjectInputStream iStream = null;
+
+		try {
+			iStream = new ObjectInputStream(new FileInputStream(file));
+			while (true) {
+				Object o = iStream.readObject();
+				if (o instanceof String) {
+					String s = (String) o;
+					System.out.println(s);
+				} else if (o instanceof Terme) {
+					Terme t = (Terme) o;
+					System.out.println(t);
+				}
+			}
+			// Ancienne manière de faire
+			//			Object obj1 = iStream.readObject();
+			//			if (obj1 instanceof String) {
+			//				String s = (String) obj1;
+			//				System.out.println(s);
+			//			}
+			//			Object obj2 = iStream.readObject();
+			//			if (obj2 instanceof Terme) {
+			//				Terme t = (Terme) obj2;
+			//				System.out.println(t);
+			//			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				iStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void initEcritureObject() {
+		Terme terme = new Terme("WAGON", new Point(2, 9), Terme.HORIZONTAL);
+		File file = new File("./src/com/formation/etga/data/terme1.ser");
+		ObjectOutputStream oStream = null;
+		try {
+			oStream = new ObjectOutputStream(new FileOutputStream(file));
+			oStream.writeObject("Etienne");
+			oStream.writeObject(terme);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				oStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void initImage() {
+		File fileIn = new File("./src/com/formation/phva/data/image2.bmp");
+		File fileOut = new File("./src/com/formation/etga/data/image2.bmp");
+		FileInputStream fluxDentree = null; // lecture d'un file
+		FileOutputStream fluxDeSortie = null;
+		int readUnByte;
+		//		int nb= 0;
+		try {
+			fluxDentree = new FileInputStream(fileIn);
+			fluxDeSortie = new FileOutputStream(fileOut);
+
+			readUnByte = fluxDentree.read(); // on regarde si ca existe voir si on rentre dans la boucle
+			while (readUnByte != -1) {
+				readUnByte = readUnByte;
+				//				if (nb >=1024) {
+				//				readUnByte = (-1^readUnByte) & 0xFF; // on change la couleur et tout
+				//				}
+				//				nb++;
+				fluxDeSortie.write(readUnByte);
+				readUnByte = fluxDentree.read();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fluxDentree.close();
+				fluxDeSortie.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
 	}
 
 	public void initLecture() {
-		File file = new File("./src/com/formation/thde/data/texte.txt");
+		File file = new File("./src/com/formation/phva/data/texte.txt");
 		BufferedReader bIn = null;
+		InputStreamReader inputStreamReader = null;
 		try {
-			bIn = new BufferedReader(new FileReader(file));
+			inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+			bIn = new BufferedReader(inputStreamReader);
 			String line = bIn.readLine();
 			while (line != null) {
 				System.out.println(line);
@@ -48,7 +159,7 @@ public class MainFlux {
 			out = new PrintWriter(file);
 			out.println("Bonjour thomas.");
 			out.println("Ceci doit apparaitre dans le fichier");
-			out.flush(); // transmission du flux sur le p�riph correspondant pour ne pas bloquer le programme
+			out.flush(); // transmission du flux sur le périph correspondant pour ne pas bloquer le programme
 			out.print("Suite de la phrase");
 		} catch (IOException e) {
 			System.out.println("Recommencer le traitement");
