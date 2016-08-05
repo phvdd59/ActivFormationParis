595c6e0933bd8bbb2542a95ebd2a6bcb86df9f36
activ_admin <activ_admin@activ_admin-PC>
2016-08-05 12:14:51 +0200

    yy

--- ProjectXml/src/com/formation/joca/main/MainXml.java
+++ ProjectXml/src/com/formation/joca/main/MainXml.java
@@ -1,10 +1,43 @@
 package com.formation.joca.main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainXml {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MainXml main = new MainXml();
+		main.lectureXml();
+	}
 
+	public void lectureXml() {
+		File fichier = new File("C:/DevFormation/ActivFormationParis/ProjectXml.src/com/formation/joca/xml/ListeTerme.xml");
+		BufferedReader bIn = null;
+		
+		try {
+			InputStreamReader input = new InputStreamReader(new FileInputStream(fichier));
+			bIn = new BufferedReader(input);
+			String line = bIn.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = bIn.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bIn.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
