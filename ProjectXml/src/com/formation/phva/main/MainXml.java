c70361061cfd62beac6c1abe6a57f686fe5502ac
francoise <francoise@192.168.1.101>
2016-08-06 15:12:57 +0200

    autre Modif de fr
    Signed-off-by: francoise <francoise@192.168.1.101>

--- ProjectXml/src/com/formation/phva/main/MainXml.java
+++ ProjectXml/src/com/formation/phva/main/MainXml.java
@@ -5,7 +5,7 @@ public class MainXml {
 	public static void main(String[] args) {
 		MainXml main = new MainXml();
 		// commentaire fr
-		// Autre commentaire
+		// Autre commentaires
 	}
 	
 	public void lectureXml() {
