9cfa216eb784d901a61d1d1eaf581e5c9ca0ec26
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-06 19:55:50 +0200

    Signed-off-by: phvdd59 <philippe.vandendriessche59@gmail.com>

--- ProjectXml/src/com/formation/frva/main/MainXml.java
+++ ProjectXml/src/com/formation/frva/main/MainXml.java
@@ -4,13 +4,6 @@ public class MainXml {
 
 	public static void main(String[] args) {
 		MainXml main = new MainXml();
-		// commentaire fr
-		// Autre commentaires
-		// bonjour
-		// coucou
-		// beubeu
-		// francoise fait une bonne modif 
-		// petite modif
 	}
 	
 	public void lectureXml() {
