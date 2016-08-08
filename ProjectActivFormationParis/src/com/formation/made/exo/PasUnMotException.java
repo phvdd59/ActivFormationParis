88690410cb196ae86471abbf0fa1cdc277d715eb
VDDMichel <VDDMichel@VDDMichel-PC>
2016-08-08 11:31:43 +0200

    made

---
+++ ProjectActivFormationParis/src/com/formation/made/exo/PasUnMotException.java
@@ -0,0 +1,8 @@
+package com.formation.made.exo;
+
+public class PasUnMotException extends Exception{
+
+	public PasUnMotException(){
+		System.out.println("le mot choisi n'est pas un mot accepté");
+	}
+}
