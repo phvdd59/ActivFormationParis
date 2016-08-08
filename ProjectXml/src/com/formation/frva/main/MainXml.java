e24dba4f1713fc968ea194527f05310ed192b43d
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-08 14:30:40 +0200

    Commit

--- ProjectXml/src/com/formation/frva/main/MainXml.java
+++ ProjectXml/src/com/formation/frva/main/MainXml.java
@@ -4,6 +4,16 @@ public class MainXml {
 
 	public static void main(String[] args) {
 		MainXml main = new MainXml();
+<<<<<<< HEAD
+=======
+		// commentaire fr
+		// Autre commentaires
+		// bonjour
+		// coucou
+		// beubeu
+		// francoise fait une bonne modif 
+		// Ma modif
+>>>>>>> branch 'master' of https://github.com/phvdd59/ActivFormationParis.git
 	}
 	
 	public void lectureXml() {
--- ProjectXml/src/com/formation/joca/main/MainXml.java
+++ ProjectXml/src/com/formation/joca/main/MainXml.java
@@ -1,11 +1,15 @@
 package com.formation.joca.main;
 
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import com.formation.joca.metier.Terme;
 
 public class MainXml {
 
@@ -15,15 +19,40 @@ public class MainXml {
 	}
 
 	public void lectureXml() {
-		File fichier = new File("C:/DevFormation/ActivFormationParis/ProjectXml.src/com/formation/joca/xml/toto.txt");
+		File fichier = new File(
+				"C:/DevFormation/ActivFormationParis/ProjectXml/src/com/formation/joca/xml/ListeTerme.xml");
 		BufferedReader bIn = null;
-		
+		ArrayList<Terme> lst = new ArrayList<Terme>();
+		Terme terme = null;
+
 		try {
-			InputStreamReader input = new InputStreamReader(new FileInputStream(fichier));
+			InputStreamReader input = new InputStreamReader(new FileInputStream(fichier), "UTF-8");
 			bIn = new BufferedReader(input);
 			String line = bIn.readLine();
+			String sens = "";
+			boolean trouve = false;
 			while (line != null) {
 				System.out.println(line);
+				if (line.contains("<Terme ")) {
+					terme = new Terme();
+					trouve = true;
+					String posX = line.substring(line.lastIndexOf("x=") + 3, line.lastIndexOf("y=") - 2);
+					String posY = line.substring(line.lastIndexOf("y=") + 3, line.lastIndexOf("sens=") - 2);
+					Point pos = new Point(Integer.parseInt(posX), Integer.parseInt(posY));
+					terme.setPos(pos);
+					sens = line.substring(line.lastIndexOf("s") + 3, line.lastIndexOf("'"));
+					if (sens.equals("VERTICAL")) {
+						terme.setSens(Terme.VERTICAL);
+					} else if (sens.equals("HORIZONTAL")) {
+						terme.setSens(Terme.HORIZONTAL);
+					}
+				} else if (trouve) {
+					terme.setNom(line);
+					trouve = false;
+					lst.add(terme);
+					System.out.println(terme.toString());
+
+				}
 				line = bIn.readLine();
 			}
 
@@ -37,6 +66,9 @@ public class MainXml {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+		for (Terme huhu : lst) {
+			System.out.println(huhu.toString());
 		}
 	}
 
