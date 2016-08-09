8c9caf18f688bfcb514fb33a83c6380230d3ad63
activ_admin <activ_admin@activ_admin-PC>
2016-08-09 11:26:33 +0200

    joca

--- ProjectThread/src/com/formation/joca/main/MainThread.java
+++ ProjectThread/src/com/formation/joca/main/MainThread.java
@@ -8,15 +8,11 @@ public class MainThread {
 		System.out.println("bonjour main");
 		MonProcessus monProcessus = new MonProcessus();
 		Thread essai = new Thread(monProcessus, "PremierThread");
-		essai.start();
-		try {
-			essai.sleep(10000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 		Thread essai2 = new Thread(monProcessus, "DeuxiemeThread");
+		essai.start();
 		essai2.start();
-		System.out.println("salut main");
+
+		System.out.println("ciao main");
 	}
 
 }
--- ProjectThread/src/com/formation/joca/metier/MonProcessus.java
+++ ProjectThread/src/com/formation/joca/metier/MonProcessus.java
@@ -4,8 +4,23 @@ public class MonProcessus implements Runnable {
 
 	@Override
 	public void run() {
-		System.out.println("bonjour monProcessus");
-		System.out.println("salut monProcessus");
+		System.out.println("bonjour monProcessus" + Thread.currentThread().getName());
+		if (Thread.currentThread().getName().equals("DeuxiemeThread")) {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("delai de 5 secondes");
+		} else if (Thread.currentThread().getName().equals("PremierThread")){
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("delai de 10 secondes");
+		}
+		System.out.println("ciao monProcessus" + Thread.currentThread().getName());
 
 	}
 
