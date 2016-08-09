2ad2b47a06af1d653c14c17abf2979d7824f7b64
activ_admin <activ_admin@activ_admin-PC>
2016-08-09 17:18:36 +0200

    joca

--- ProjectAscenseur/src/com/formation/joca/main/MainAscenseur.java
+++ ProjectAscenseur/src/com/formation/joca/main/MainAscenseur.java
@@ -1,7 +1,21 @@
 package com.formation.joca.main;
 
+import com.formation.joca.metier.ListePersonne;
+
 public class MainAscenseur {
 public static void main(String[] args) {
+	
+	ListePersonne lst = new ListePersonne();
+	Thread essai=new Thread(lst);
+	essai.start();
+	try {
+		Thread.currentThread().sleep(10);
+	} catch (InterruptedException e) {
+	}
+	lst.setSortie(true);
+	for (int i=0;i<lst.size();i++){
+		System.out.println(lst.get(i).toString());
+	}
 	
 }
 }
--- ProjectAscenseur/src/com/formation/joca/metier/ListePersonne.java
+++ ProjectAscenseur/src/com/formation/joca/metier/ListePersonne.java
@@ -5,12 +5,11 @@ import java.util.ArrayList;
 public class ListePersonne extends ArrayList<Personne> implements Runnable {
 
 	private boolean sortie;
-	
+
 	public ListePersonne() {
-		this.sortie=false;
+		this.sortie = false;
 	}
-	
-	
+
 	public boolean isSortie() {
 		return sortie;
 	}
@@ -19,18 +18,22 @@ public class ListePersonne extends ArrayList<Personne> implements Runnable {
 		this.sortie = sortie;
 	}
 
-
-
 	@Override
 	public void run() {
+		int nb = 0;
+		while (!sortie) {
+			Personne personne = new Personne(String.valueOf(nb), ETAT.ETAT_ATTENTE.ordinal(), (int) (Math.random() * 20),
+					(int) (Math.random() * 20));
+			System.out.println("j'ai créé la personne " + nb);
+			this.add(personne);
+			nb++;
+		}
 
 	}
-
 
 	@Override
 	public String toString() {
-		return "ListePersonne [sortie=" + sortie + "]";
+		return super.toString();
 	}
-	
-	
+
 }
