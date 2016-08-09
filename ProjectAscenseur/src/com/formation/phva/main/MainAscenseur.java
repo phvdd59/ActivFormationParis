4c9e1822d42d0d7e93f7c3cacbfbd7db5e0d5c5e
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-09 16:03:28 +0200

    a

--- ProjectAscenseur/src/com/formation/phva/main/MainAscenseur.java
+++ ProjectAscenseur/src/com/formation/phva/main/MainAscenseur.java
@@ -1,7 +1,11 @@
 package com.formation.phva.main;
 
 public class MainAscenseur {
-public static void main(String[] args) {
-	
-}
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
 }
