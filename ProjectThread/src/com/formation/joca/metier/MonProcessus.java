8f30370bb0a73cddb274d3296ff48a8601309aac
activ_admin <activ_admin@activ_admin-PC>
2016-08-09 10:32:26 +0200

    joca

---
+++ ProjectThread/src/com/formation/joca/metier/MonProcessus.java
@@ -0,0 +1,12 @@
+package com.formation.joca.metier;
+
+public class MonProcessus implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("bonjour monProcessus");
+		System.out.println("salut monProcessus");
+
+	}
+
+}
