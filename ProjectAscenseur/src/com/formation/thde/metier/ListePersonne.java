26621da96f9ce811f38ef2a021df1564c28b5ee8
active_admin <active_admin@LAPTOP-FP1TSI7G>
2016-08-10 09:43:36 +0200

    thde

--- ProjectAscenseur/src/com/formation/thde/metier/ListePersonne.java
+++ ProjectAscenseur/src/com/formation/thde/metier/ListePersonne.java
@@ -6,14 +6,18 @@ public class ListePersonne extends ArrayList<Personne> implements Runnable {
 	private boolean sortie;
 
 	public ListePersonne() {
-		// * faire un générateur de personnes aléatoires
-		// * - étage de départ
-		// * - étage d'arrivé
-		// * création personne à des temps aléatoires
-		// *
-		// * @param args
+		//		 * faire un générateur de personnes aléatoires
+		//		 * 		- étage de départ
+		//		 * 		- étage d'arrivé
+		//		 * 		création personne à des temps aléatoires
+		//		 *  
+		//		 * @param args
+		this.sortie = sortie;
+	}
 
-		for (int i = 0; i < 20; i++) {
+	@Override
+	public void run() {
+		for (int i = 1; i <= 20; i++) {
 			String nom = "personne " + i;
 			int etat = 0;
 			int depart = (int) (Math.random() * 20);
@@ -21,12 +25,14 @@ public class ListePersonne extends ArrayList<Personne> implements Runnable {
 			while (arrive == depart) {
 				arrive = (int) (Math.random()) * 20;
 			}
+			//System.out.println(new Personne(nom, etat, depart, arrive));
 			this.add(new Personne(nom, etat, depart, arrive));
+			Personne.CPT++;
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+			}
 		}
-	}
-
-	@Override
-	public void run() {
 	}
 
 	public boolean isSortie() {
