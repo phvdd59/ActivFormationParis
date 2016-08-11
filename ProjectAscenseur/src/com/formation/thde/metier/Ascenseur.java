3c2063697366923f7fe9f67e4d19d4a952407f4b
active_admin <active_admin@LAPTOP-FP1TSI7G>
2016-08-11 09:29:20 +0200

    thde

--- ProjectAscenseur/src/com/formation/thde/metier/Ascenseur.java
+++ ProjectAscenseur/src/com/formation/thde/metier/Ascenseur.java
@@ -1,23 +1,98 @@
 package com.formation.thde.metier;
 
+import java.util.ArrayList;
+
 public class Ascenseur extends Thread {
 
 	public static int CPT = 0;
 	public static int TEMPS = 10;
 	public static int HAUTEUR_ETAGE = 30;
 	private int etage;
-	private int progression;
+	private int progression = 0;
 	private boolean fin;
 	private Personne personne;
+	public ListePersonne listPersonne;
 
 	public Ascenseur() {
 	}
 
-	public Ascenseur(int etage, int progression, boolean fin, Personne personne) {
+	public Ascenseur(int etage, int progression, boolean fin, Personne personne, ListePersonne listPersonne) {
 		this.etage = etage;
 		this.progression = progression;
 		this.fin = fin;
 		this.personne = personne;
+		this.listPersonne = listPersonne;
+	}
+
+	@Override
+	public void run() {
+		while (this.fin == false) {
+			try {
+				Thread.sleep(TEMPS);
+			} catch (InterruptedException e) {
+			}
+			if (this.personne == null) {
+
+				if (listPersonne.size() == 0 && listPersonne.isSortie() == true) {
+					this.fin = true;
+					System.err.println("FIN DU THREAD : " + this.getName());
+					try {
+						Thread.sleep(9);
+					} catch (InterruptedException e) {
+					}
+				}
+				synchronized (listPersonne) {
+					if (listPersonne.size() > 0) {
+						for (int i = 0; i < listPersonne.size(); i++) {
+							if (listPersonne.get(i).getEtat() == ETAT.ATTENTE) {
+								this.personne = listPersonne.remove(i);
+								System.out.println(this.getName() + " trouve " + this.personne.getNom());
+								this.personne.setEtat(ETAT.DEPART);
+								break;
+							}
+						}
+					}
+				}
+			} else if (this.personne.getEtat() == ETAT.DEPART) {
+				if (etage > personne.getDepart()) {
+					progression--;
+					if (progression % 30 != 0) {
+					} else {
+						this.etage--;
+					}
+				} else if (etage < personne.getDepart()) {
+					progression++;
+					if (progression % 30 != 0) {
+					} else {
+						this.etage++;
+					}
+				} else {
+					System.out.println(this.getName() + " : " + this.personne.getNom() + " monte à l'étage " + this.etage);
+					this.personne.setEtat(ETAT.MOV);
+				}
+			} else if (this.personne.getEtat() == ETAT.MOV) {
+
+				if (etage > personne.getArrive()) {
+					progression--;
+					if (progression % 30 != 0) {
+					} else {
+						this.etage--;
+					}
+				} else if (etage < personne.getArrive()) {
+					progression++;
+					if (progression % 30 != 0) {
+					} else {
+						this.etage++;
+					}
+				} else {
+					this.personne.setEtat(ETAT.ARRIVE);
+					System.out.println(this.getName() + " : " + this.personne.getNom() + " descend à l'étage " + this.etage);
+				}
+
+			} else if (this.personne.getEtat() == ETAT.ARRIVE) {
+				this.personne = null;
+			}
+		}
 	}
 
 	public static int getCPT() {
@@ -74,6 +149,14 @@ public class Ascenseur extends Thread {
 
 	public void setPersonne(Personne personne) {
 		this.personne = personne;
+	}
+
+	public ArrayList<Personne> getListPersonne() {
+		return listPersonne;
+	}
+
+	public void setListPersonne(ListePersonne listPersonne) {
+		this.listPersonne = listPersonne;
 	}
 
 	@Override
