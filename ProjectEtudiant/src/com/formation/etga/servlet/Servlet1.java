016b74f220f1dcb8d593299c6a8eff0e6902a3c8
etga <etiennegaspard@hotmail.com>
2016-08-19 11:21:11 +0200

    etga

--- ProjectEtudiant/src/com/formation/etga/servlet/Servlet1.java
+++ ProjectEtudiant/src/com/formation/etga/servlet/Servlet1.java
@@ -1,6 +1,7 @@
 package com.formation.etga.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,23 +14,40 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletEtga")
 public class Servlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Servlet1() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static int VALEUR = 0;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Servlet1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request,
+	 *      HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().println("<HTML><body><h1>Salut Tomcat cest l'ordi d'etga</h1>");
-		response.getWriter().append("<h2>Salut Tomcat, c'est l'ordi d'etga :)</h2></body></HTML>");
-		
+		// récupérer les paramètres.
+		String pseudo = request.getParameter("nom");
+		// Contrôler les params
+		if (pseudo.contains("Gaspard")) {
+			pseudo = pseudo + "PLUS";
+		}
+		//Constituer la nouvelle page 
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println("<html><body><h1>ACTIV-CONSULTING</h1>");
+		response.getWriter().println("<form>" + "<input type='text' value='" + VALEUR + "'>");
+		response.getWriter().println("<input name='question' type='text' " + "value='" + pseudo + "'>");
+		response.getWriter().println("<input name='submit' type='submit' " + "value='OK'>");
+		if (pseudo.contains("Philippe")) {
+			response.getWriter().println("<p style='color:red'>" + pseudo + "</p>");
+		} else {
+			response.getWriter().println("<p style='color:blue'>" + pseudo + "</p>");
+		}
+		VALEUR++;
+		response.getWriter().println("</form></body></html>");
 	}
 
 }
