cbb45e1a46cf2877b1c672662bdb8262a31eb838
emma <activ_admin@activ_admin-PC>
2016-08-19 11:49:49 +0200

    jhbfdhyjf

--- ProjectEtudiant/src/com/formation/emma/servlet/Servlet1.java
+++ ProjectEtudiant/src/com/formation/emma/servlet/Servlet1.java
@@ -1,6 +1,11 @@
 package com.formation.emma.servlet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Servlet1
  */
-@WebServlet("/Servlet1")
+@WebServlet("/Servlet365")
 public class Servlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static int VALEUR = 0;
@@ -21,7 +26,7 @@ public class Servlet1 extends HttpServlet {
 	 */
 	public Servlet1() {
 		super();
-		// TODO Auto-generated constructor stub
+	
 	}
 
 	/**
@@ -29,18 +34,44 @@ public class Servlet1 extends HttpServlet {
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//recuperer les parametres
-		String pseudo = request.getParameter("nom");
-		// controler les parametres
-		if (pseudo.contains("philippe")){
-			pseudo=pseudo+"plus";
+		File file = new File("./src/com/formation/phva/data/texte.txt");
+		BufferedReader bIn = null;
+		InputStreamReader inputStreamReader = null;
+		try {
+			inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8"); 
+			bIn = new BufferedReader(inputStreamReader);
+			String line = bIn.readLine();
+			while (line != null) {
+				response.getWriter().println(line);
+				line = bIn.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bIn.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		// constituer la nouvelle page
-				
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().append("Coucou! ");
-		response.getWriter().println("<form><input type='text' value='" + VALEUR + "'></form>");
-		VALEUR++;
 	}
 
-}
+		
+		
+		//		//recuperer les parametres
+//		String pseudo = request.getParameter("nom");
+//		// controler les parametres
+//		if (pseudo.contains("philippe")) {
+//			pseudo = pseudo + "plus";
+//		}
+//		// constituer la nouvelle page
+//
+//		//response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Coucou! ");
+//		response.getWriter().println("<form><input type='text' value='" + VALEUR + "'></form>");
+//		VALEUR++;
+	}
+
+
