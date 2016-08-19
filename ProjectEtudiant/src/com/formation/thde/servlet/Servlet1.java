98133c77d18548cd6781e35680d31ff842e883bd
thde <thomas.delannoy5@gmail.com>
2016-08-19 16:09:19 +0200

    thde

--- ProjectEtudiant/src/com/formation/thde/servlet/Servlet1.java
+++ ProjectEtudiant/src/com/formation/thde/servlet/Servlet1.java
@@ -1,6 +1,12 @@
 package com.formation.thde.servlet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Servlet1
  */
-@WebServlet("/Servlet1")
+@WebServlet(value = "/ServletThde", name = "ServletTH")
 public class Servlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -20,7 +26,6 @@ public class Servlet1 extends HttpServlet {
 	 */
 	public Servlet1() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -28,21 +33,41 @@ public class Servlet1 extends HttpServlet {
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//recuperer
-		String pseudo = request.getParameter("nom");
-		//controler
-		if (pseudo.contains("Admin")) {
-			pseudo = pseudo + "PLUS";
+
+		File file = new File("C:/DevFormation/GITActivFormationParis/ProjectEtudiant/WebContent/com/formation/thde/page/Login.html");
+		BufferedReader bIn = null;
+		InputStreamReader inputStreamReader = null;
+		try {
+			inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+			bIn = new BufferedReader(new FileReader(file));
+			String line = bIn.readLine();
+			while (line != null) {
+				response.getWriter().println(line);
+				line = bIn.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bIn.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		//faire la nouvelle page
-		response.getWriter().println("<html><body><form>");
-		response.getWriter().println("<input type='text' value=" + pseudo + ">");
-		if (pseudo.contains("Admin")) {
-			response.getWriter().println("<p style='color:red';>" + pseudo + "</p>");
-		} else {
-			response.getWriter().println("<p style='color:blue'>" + pseudo + "</p>");
-		}
-		response.getWriter().println("</form></body></html>");
 	}
+
+	//		//faire la nouvelle page
+	//		response.getWriter().println("<html><body><form>");
+	//		response.getWriter().println("<input type='text' value=" + pseudo + ">");
+	//		if (pseudo.contains("Admin")) {
+	//			response.getWriter().println("<p style='color:red';>" + pseudo + "</p>");
+	//		} else {
+	//			response.getWriter().println("<p style='color:blue'>" + pseudo + "</p>");
+	//		}
+	//		response.getWriter().println("</form></body></html>");
+	//	}
 
 }
