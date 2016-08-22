38145c39fd20f702eb866cd30d5d6c99b5508615
soka <souad.kadri1@gmail.com>
2016-08-22 09:16:24 +0200

    ok

---
+++ ProjectJMST/src/com/formation/soka/servlet/ServletSoka3.java
@@ -0,0 +1,51 @@
+package com.formation.soka.servlet;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletSoka3
+ */
+@WebServlet("/ServletSoka3")
+public class ServletSoka3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletSoka3() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		File file = new File("C:/DevFormation/GITActivFormationParis/" + //
+				"ProjectJMST/WebContent/WEB-INF/" + //
+				"page/pageActiveFormulaire_part4_situation_actuelle.html");
+		BufferedReader bIn = null;
+		bIn = new BufferedReader(new FileReader(file));
+		String line = bIn.readLine();
+			response.getWriter().println(line);
+			line = bIn.readLine();
+		bIn.close();
+	}
+
+}
