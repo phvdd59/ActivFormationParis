45b9e75f614bb30538a737ae5cc6f4a8358e5dcf
Beba <b.basdevant@gmail.com>
2016-08-22 15:58:23 +0200

    yop

--- ProjectEtudiant/src/com/formation/beba/servlet/ServletBEBA2.java
+++ ProjectEtudiant/src/com/formation/beba/servlet/ServletBEBA2.java
@@ -1,12 +1,11 @@
 package com.formation.beba.servlet;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.BufferedReader;
-import java.io.File;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,11 +13,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  * Servlet implementation class Servlet1
  */
-@WebServlet(value="/ServlefsvsbtBeba2", name="servletB2")
+@WebServlet(value = "/ServlefsvsbtBeba2", name = "servletB2")
 public class ServletBEBA2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -37,18 +35,20 @@ public class ServletBEBA2 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// response.getWriter().append("Served at:
 		// ").append(request.getContextPath());
-		File file = new File("C:/DevFormation/GITActivFormationParis/ProjectEtudiant/WebContent/WEB-INF/com/formation/bait/pages/InsciprtionPage5.html");
+		File file = new File("C:/DevFormation/GITActivFormationParis/ProjectEtudiant/WebContent/WEB-INF/com/formation/bait/pages/Login.html");
 		BufferedReader bIn = null;
+		int noSerie = (int) Math.random() * Integer.MAX_VALUE;
 		InputStreamReader inputStreamReader = null;
 		try {
 			inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8"); // pour
-																				// texte
+			// texte
 			bIn = new BufferedReader(inputStreamReader);
 			String line = bIn.readLine();
 			while (line != null) {
+				if (line.contains("%%noSerie%%")) {
+					line = line.replace("%%noSerie%%", Integer.toString(noSerie));
+				}
 				response.getWriter().append(line);
-//				System.out.println(line);
-
 				line = bIn.readLine();
 			}
 		} catch (FileNotFoundException e) {
