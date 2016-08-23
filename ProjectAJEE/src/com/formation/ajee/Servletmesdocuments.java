178c01c701a5b0b6ee8ce16a15dd1d2a204b9b38
etga <etiennegaspard@hotmail.com>
2016-08-23 16:06:42 +0200

    ega

--- ProjectAJEE/src/com/formation/ajee/Servletmesdocuments.java
+++ ProjectAJEE/src/com/formation/ajee/Servletmesdocuments.java
@@ -10,7 +10,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class ServletDocuments
@@ -29,46 +28,46 @@ public class Servletmesdocuments extends HttpServlet {
 	 *      HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		String noSerie = request.getParameter("noSerie");
-//		HttpSession session = request.getSession();
-//		Object oRecupNoSerie = session.getAttribute("noSerie");
-//		String recupNoSerie = (String) oRecupNoSerie;
-//		if (noSerie.equals(recupNoSerie)) {
-//			if (noSerie != null && !noSerie.equals("%%noSerie%%")) {
-				File file = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/WEB-INF/com/formation/ajee/page/Documents.html");
-				BufferedReader bufRead = null;
-				bufRead = new BufferedReader(new FileReader(file));
-				String line = bufRead.readLine();
-				while (line != null) {
-					response.getWriter().println(line);
-					line = bufRead.readLine();
-				}
-				bufRead.close();
-//			}
-//		}
+		//		String noSerie = request.getParameter("noSerie");
+		//		HttpSession session = request.getSession();
+		//		Object oRecupNoSerie = session.getAttribute("noSerie");
+		//		String recupNoSerie = (String) oRecupNoSerie;
+		//		if (noSerie.equals(recupNoSerie)) {
+		//			if (noSerie != null && !noSerie.equals("%%noSerie%%")) {
+		File file = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/WEB-INF/com/formation/ajee/page/Documents.html");
+		BufferedReader bufRead = null;
+		bufRead = new BufferedReader(new FileReader(file));
+		String line = bufRead.readLine();
+		while (line != null) {
+			response.getWriter().println(line);
+			line = bufRead.readLine();
+		}
+		bufRead.close();
+		//			}
+		//		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request,
 	 *      HttpServletResponse response)
 	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		String noSerie = request.getParameter("noSerie");
-//		HttpSession session = request.getSession();
-//		Object oRecupNoSerie = session.getAttribute("noSerie");
-//		String recupNoSerie = (String) oRecupNoSerie;
-//		if (noSerie.equals(recupNoSerie)) {
-//			if (noSerie != null && !noSerie.equals("%%noSerie%%")) {
-//				File file = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/WEB-INF/com/formation/ajee/page/Documents.html");
-//				BufferedReader bufRead = null;
-//				bufRead = new BufferedReader(new FileReader(file));
-//				String line = bufRead.readLine();
-//				while (line != null) {
-//					response.getWriter().println(line);
-//					line = bufRead.readLine();
-//				}
-//				bufRead.close();
-//			}
-//		}
-//	}
+	//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//		String noSerie = request.getParameter("noSerie");
+	//		HttpSession session = request.getSession();
+	//		Object oRecupNoSerie = session.getAttribute("noSerie");
+	//		String recupNoSerie = (String) oRecupNoSerie;
+	//		if (noSerie.equals(recupNoSerie)) {
+	//			if (noSerie != null && !noSerie.equals("%%noSerie%%")) {
+	//				File file = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/WEB-INF/com/formation/ajee/page/Documents.html");
+	//				BufferedReader bufRead = null;
+	//				bufRead = new BufferedReader(new FileReader(file));
+	//				String line = bufRead.readLine();
+	//				while (line != null) {
+	//					response.getWriter().println(line);
+	//					line = bufRead.readLine();
+	//				}
+	//				bufRead.close();
+	//			}
+	//		}
+	//	}
 }
