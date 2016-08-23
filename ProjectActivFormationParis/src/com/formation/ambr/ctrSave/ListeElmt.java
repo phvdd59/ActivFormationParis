37264f0c22880d7aeecf39209ecfe1a6a7b503db
ambr <ambrault@gmail.com>
2016-08-23 09:06:44 +0200

    ctrsave

---
+++ ProjectActivFormationParis/src/com/formation/ambr/ctrSave/ListeElmt.java
@@ -0,0 +1,12 @@
+package com.formation.ambr.ctrSave;
+
+import java.util.ArrayList;
+
+public class ListeElmt extends ArrayList<Elmt>{
+	
+	private TRI tri;
+	
+	public ListeElmt (TRI tri) {
+		
+	}
+}
