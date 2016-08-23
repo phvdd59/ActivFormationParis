491a85dd270ec288fddc6e0c175c47c94b6e563f
joca <johanncassagne@gmail.com>
2016-08-23 12:06:40 +0200

    joca

--- ProjectEtudiant/src/com/formation/joca/servlet/ServletJOCA2.java
+++ ProjectEtudiant/src/com/formation/joca/servlet/ServletJOCA2.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletJOCA2
  */
-@WebServlet("/ServletJOCA2")
+@WebServlet("/ServletJOCA20")
 public class ServletJOCA2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
