0fe49dd7e3f353c92cdbb6a8f37d1548fd001d92
joca <johanncassagne@gmail.com>
2016-08-23 16:02:20 +0200

    joca

--- ProjectJMST/src/com/formation/joca/servlet/ServletJOCA2.java
+++ ProjectJMST/src/com/formation/joca/servlet/ServletJOCA2.java
@@ -78,14 +78,4 @@ public class ServletJOCA2 extends HttpServlet {
 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		doGet(request, response);
-	}
-
 }
