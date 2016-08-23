cf22d7c39a3d46169bc1cdd7ee3b0e1d896acc22
jeci <jeremyciamous@gmail.com>
2016-08-23 15:25:05 +0200

    o

--- ProjectEtudiant/src/com/formation/jeci/servlet/Servletjeci1.java
+++ ProjectEtudiant/src/com/formation/jeci/servlet/Servletjeci1.java
@@ -1,11 +1,15 @@
 package com.formation.jeci.servlet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Servletjeci1
@@ -22,20 +26,38 @@ public class Servletjeci1 extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		HttpSession session = request.getSession();
+//		String noSerie = request.getParameter("noSerie");
+//		String pseudo = request.getParameter("identifiant");
+//		Object oRecupNoSerie=session.getAttribute("noSerie");
+//		String recupNoSerie=(String) oRecupNoSerie;
+		
+		//Constituer la nouvelle page
+//		if (pseudo != null && noSerie != null && !noSerie.equals("%%noserie%%")) {
+			//			if (pseudo.equals("Philippe")) {
+			File f = new File("C:/DevFormation/" + "GITActivFormationParis/ProjectEtudiant/" + "WebContent/WEB-INF/com/formation/ajee/" + "page/Documents.html");
+			BufferedReader bIn = new BufferedReader(new FileReader(f));
+			String l = bIn.readLine();
+			while (l != null) {
+				response.getWriter().println(l);
+				l = bIn.readLine();
+			}
+			bIn.close();
+			File f1 = new File("C:/DevFormation/" + "GITActivFormationParis/ProjectEtudiant/" + "WebContent/WEB-INF/com/formation/ajee/" + "page/bandeauAdmin.html");
+			BufferedReader bIn1 = new BufferedReader(new FileReader(f1));
+			String l1 = bIn.readLine();
+			while (l1 != null) {
+				response.getWriter().println(l);
+				l1 = bIn1.readLine();
+			}
+			bIn1.close();
+		}
 	}
 
-}
+
