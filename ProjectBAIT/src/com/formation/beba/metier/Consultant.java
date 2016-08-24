98fd876f5c04439332dc4aff14d73b6d60a0682f
Beba <b.basdevant@gmail.com>
2016-08-24 11:09:55 +0200

    class admin consultant

---
+++ ProjectBAIT/src/com/formation/beba/metier/Consultant.java
@@ -0,0 +1,20 @@
+package com.formation.beba.metier;
+
+public class Consultant {
+	String identifiant = new String();
+	String prenom = new String();
+	String nom = new String();
+	String mdp = new String();
+	
+	
+	public Consultant(String identifiant, String prenom, String nom, String mdp) {
+
+		this.identifiant = identifiant;
+		this.prenom = prenom;
+		this.nom = nom;
+		this.mdp = mdp;
+	}
+
+	
+	
+}
---
+++ ProjectBAIT/src/com/formation/beba/metier/ListeConsultant.java
@@ -0,0 +1,11 @@
+package com.formation.beba.metier;
+
+import java.util.ArrayList;
+
+public class ListeConsultant extends ArrayList<Admin>{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
