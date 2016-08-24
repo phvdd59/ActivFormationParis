bc65529770b0ca7f6c424a4afe636dc51272cc22
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-08-24 12:03:24 +0200

    Project Etudiant Test

---
+++ ProjectEtudiant/src/com/formation/phva/metier/Personne.java
@@ -0,0 +1,40 @@
+package com.formation.phva.metier;
+
+public class Personne {
+	
+	private String nom;
+	private String prenom;
+	private boolean sexe;
+
+	public Personne(String nom, String prenom, boolean sexe) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.sexe = sexe;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public boolean isSexe() {
+		return sexe;
+	}
+
+	public void setSexe(boolean sexe) {
+		this.sexe = sexe;
+	}
+
+}
