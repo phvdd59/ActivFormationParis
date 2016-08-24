a30c57e54d71fb2cf0252345ba610c89318b756f
Beba <b.basdevant@gmail.com>
2016-08-24 11:09:55 +0200

    class admin consultant

---
+++ ProjectBAIT/src/com/formation/beba/metier/ListeAdmin.java
@@ -0,0 +1,11 @@
+package com.formation.beba.metier;
+
+import java.util.ArrayList;
+
+public class ListeAdmin extends ArrayList<Admin>{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
