c50f29cdfb407729f068ad036005b3ba7500afeb
joca <johanncassagne@gmail.com>
2016-08-30 09:33:09 +0200

    jocq

--- ProjectJMST/src/com/formation/thcr/metier/Personne.java
+++ ProjectJMST/src/com/formation/thcr/metier/Personne.java
@@ -1,6 +1,9 @@
 package com.formation.thcr.metier;
 
+import java.sql.Timestamp;
+
 public class Personne {
+
 
 	private String nom;
 	private String prenom;
@@ -14,290 +17,197 @@ public class Personne {
 	private String fax;
 	private String ville;
 	private String codePostal;
-	private String dateNaissance;
+	private Timestamp dateNaissance;
 	private String lieuNaissance;
 	private String numSecu;
+	private int id;
 	private String position;
 	private String coeff;
 	private String salaire;
-	private String visiteMedicale;
+	private Timestamp visiteMedicale;
 	private String montantTransport;
-	private String nbCV;
+	private int nbCV;
 	private String nbKm;
-	private boolean freelance;
-	private boolean retraite;
-	private boolean salarie;
-	private boolean autoEntrepreneur;
-	private boolean chomage;
-	private boolean autre;
-	private boolean voiture;
-	private boolean cadre;
-	private boolean mutuelle;
-	private boolean ticketResto;
+	private String situation;
+	private Boolean voiture;
+	private Boolean cadre;
+	private Boolean mutuelle;
+	private Boolean ticketResto;
 	private boolean admin;
-
-	public boolean isAdmin() {
-		return admin;
-	}
-
-	public void setAdmin(boolean admin) {
-		this.admin = admin;
-	}
 
 	public String getNom() {
 		return nom;
 	}
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getMotDePasse() {
-		return motDePasse;
-	}
-
-	public void setMotDePasse(String motDePasse) {
-		this.motDePasse = motDePasse;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
 	public String getPrenom() {
 		return prenom;
 	}
-
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
-
-	public String getTelFixe() {
-		return telFixe;
+	public String getLogin() {
+		return login;
 	}
-
-	public void setTelFixe(String telFixe) {
-		this.telFixe = telFixe;
+	public void setLogin(String login) {
+		this.login = login;
 	}
-
-	public String getTelPort() {
-		return telPort;
+	public String getMotDePasse() {
+		return motDePasse;
 	}
-
-	public void setTelPort(String telPort) {
-		this.telPort = telPort;
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
 	}
-
-	public String getFax() {
-		return fax;
+	public String getMail() {
+		return mail;
 	}
-
-	public void setFax(String fax) {
-		this.fax = fax;
+	public void setMail(String mail) {
+		this.mail = mail;
 	}
-
-	public String getVille() {
-		return ville;
-	}
-
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
-
-	public String getCodePostal() {
-		return codePostal;
-	}
-
-	public void setCodePostal(String codePostal) {
-		this.codePostal = codePostal;
-	}
-
-	public String getDateNaissance() {
-		return dateNaissance;
-	}
-
-	public void setDateNaissance(String dateNaissance) {
-		this.dateNaissance = dateNaissance;
-	}
-
-	public String getLieuNaissance() {
-		return lieuNaissance;
-	}
-
-	public void setLieuNaissance(String lieuNaissance) {
-		this.lieuNaissance = lieuNaissance;
-	}
-
-	public String getNumSecu() {
-		return numSecu;
-	}
-
-	public void setNumSecu(String numSecu) {
-		this.numSecu = numSecu;
-	}
-
-	public String getPosition() {
-		return position;
-	}
-
-	public void setPosition(String position) {
-		this.position = position;
-	}
-
-	public String getCoeff() {
-		return coeff;
-	}
-
-	public void setCoeff(String coeff) {
-		this.coeff = coeff;
-	}
-
-	public String getSalaire() {
-		return salaire;
-	}
-
-	public void setSalaire(String salaire) {
-		this.salaire = salaire;
-	}
-
-	public String getVisiteMedicale() {
-		return visiteMedicale;
-	}
-
-	public void setVisiteMedicale(String visiteMedicale) {
-		this.visiteMedicale = visiteMedicale;
-	}
-
-	public String getMontantTransport() {
-		return montantTransport;
-	}
-
-	public void setMontantTransport(String montantTransport) {
-		this.montantTransport = montantTransport;
-	}
-
-	public String getNbCV() {
-		return nbCV;
-	}
-
-	public void setNbCV(String nbCV) {
-		this.nbCV = nbCV;
-	}
-
-	public String getNbKm() {
-		return nbKm;
-	}
-
-	public void setNbKm(String nbKm) {
-		this.nbKm = nbKm;
-	}
-
-	public boolean isVoiture() {
-		return voiture;
-	}
-
-	public void setVoiture(boolean voiture) {
-		this.voiture = voiture;
-	}
-
-	public boolean isCadre() {
-		return cadre;
-	}
-
-	public void setCadre(boolean cadre) {
-		this.cadre = cadre;
-	}
-
-	public boolean isMutuelle() {
-		return mutuelle;
-	}
-
-	public void setMutuelle(boolean mutuelle) {
-		this.mutuelle = mutuelle;
-	}
-
-	public boolean isTicketResto() {
-		return ticketResto;
-	}
-
-	public void setTicketResto(boolean ticketResto) {
-		this.ticketResto = ticketResto;
-	}
-
-	public String getAdresse() {
-		return adresse;
-	}
-
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-
 	public String getNationalite() {
 		return nationalite;
 	}
-
 	public void setNationalite(String nationalite) {
 		this.nationalite = nationalite;
 	}
-
-	public boolean isFreelance() {
-		return freelance;
+	public String getAdresse() {
+		return adresse;
 	}
-
-	public void setFreelance(boolean freelance) {
-		this.freelance = freelance;
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
 	}
-
-	public boolean isRetraite() {
-		return retraite;
+	public String getTelFixe() {
+		return telFixe;
 	}
-
-	public void setRetraite(boolean retraite) {
-		this.retraite = retraite;
+	public void setTelFixe(String telFixe) {
+		this.telFixe = telFixe;
 	}
-
-	public boolean isSalarie() {
-		return salarie;
+	public String getTelPort() {
+		return telPort;
 	}
-
-	public void setSalarie(boolean salarie) {
-		this.salarie = salarie;
+	public void setTelPort(String telPort) {
+		this.telPort = telPort;
 	}
-
-	public boolean isAutoEntrepreneur() {
-		return autoEntrepreneur;
+	public String getFax() {
+		return fax;
 	}
-
-	public void setAutoEntrepreneur(boolean autoEntrepreneur) {
-		this.autoEntrepreneur = autoEntrepreneur;
+	public void setFax(String fax) {
+		this.fax = fax;
 	}
-
-	public boolean isChomage() {
-		return chomage;
+	public String getVille() {
+		return ville;
 	}
-
-	public void setChomage(boolean chomage) {
-		this.chomage = chomage;
+	public void setVille(String ville) {
+		this.ville = ville;
 	}
-
-	public boolean isAutre() {
-		return autre;
+	public String getCodePostal() {
+		return codePostal;
 	}
-
-	public void setAutre(boolean autre) {
-		this.autre = autre;
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	public Timestamp getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Timestamp dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getLieuNaissance() {
+		return lieuNaissance;
+	}
+	public void setLieuNaissance(String lieuNaissance) {
+		this.lieuNaissance = lieuNaissance;
+	}
+	public String getNumSecu() {
+		return numSecu;
+	}
+	public void setNumSecu(String numSecu) {
+		this.numSecu = numSecu;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getCoeff() {
+		return coeff;
+	}
+	public void setCoeff(String coeff) {
+		this.coeff = coeff;
+	}
+	public String getSalaire() {
+		return salaire;
+	}
+	public void setSalaire(String salaire) {
+		this.salaire = salaire;
+	}
+	public Timestamp getVisiteMedicale() {
+		return visiteMedicale;
+	}
+	public void setVisiteMedicale(Timestamp visiteMedicale) {
+		this.visiteMedicale = visiteMedicale;
+	}
+	public String getMontantTransport() {
+		return montantTransport;
+	}
+	public void setMontantTransport(String montantTransport) {
+		this.montantTransport = montantTransport;
+	}
+	public int getNbCV() {
+		return nbCV;
+	}
+	public void setNbCV(int nbCV) {
+		this.nbCV = nbCV;
+	}
+	public String getNbKm() {
+		return nbKm;
+	}
+	public void setNbKm(String nbKm) {
+		this.nbKm = nbKm;
+	}
+	public String getSituation() {
+		return situation;
+	}
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+	public Boolean getVoiture() {
+		return voiture;
+	}
+	public void setVoiture(Boolean voiture) {
+		this.voiture = voiture;
+	}
+	public Boolean getCadre() {
+		return cadre;
+	}
+	public void setCadre(Boolean cadre) {
+		this.cadre = cadre;
+	}
+	public Boolean getMutuelle() {
+		return mutuelle;
+	}
+	public void setMutuelle(Boolean mutuelle) {
+		this.mutuelle = mutuelle;
+	}
+	public Boolean getTicketResto() {
+		return ticketResto;
+	}
+	public void setTicketResto(Boolean ticketResto) {
+		this.ticketResto = ticketResto;
+	}
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
 	}
 
 }
