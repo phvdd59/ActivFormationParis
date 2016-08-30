fe3f18dc5cdfba8bb323cf0dc7e0ca9048c27bf5
Beba <b.basdevant@gmail.com>
2016-08-30 16:52:35 +0200

    a jour

--- ProjectJDBC/src/com/formation/beba/main/MainJDBCref.java
+++ ProjectJDBC/src/com/formation/beba/main/MainJDBCref.java
@@ -5,28 +5,72 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Enumeration;
 
-public class MainJDBC {
+public class MainJDBCref {
 	public final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	public final static String DB_URL = "jdbc:mysql://localhost/test";
+	public final static String DB_URL = "jdbc:mysql://localhost/marchandphva";
 
 	public String user = "root";
 	public String pass = "";
 
 	public static void main(String[] args) {
-		MainJDBC m = new MainJDBC();
-		// m.init();
-//		 m.create2();
-//		m.update();
-//		m.create();
-//		m.creTable();
-		m.insertTable();
-		
+		MainJDBCref m = new MainJDBCref();
+		//m.init();
+		//m.create();
+		//m.creTable();
+		//m.selectTable();
+		//m.insertTable();
+		//m.selectMetaData();
+		m.updateTable();
+		m.selectTable();
 	}
-	
+
+	public void create3() {
+		Connection conn = null;
+		Statement stat = null;
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, user, pass);
+			stat = conn.createStatement();
+
+			String sql = "CREATE DATABASE marchandphva";
+
+			stat.executeUpdate(sql);
+			System.out.println("FIN");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void create() {
+		Connection conn = null;
+		Statement stat = null;
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, user, pass);
+			stat = conn.createStatement();
+
+			String sql = "CREATE TABLE marchand ( "//
+					+ "ID BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, "//
+					+ "NOM VARCHAR(30) NOT NULL"//
+					+ ", PRENOM VARCHAR(30) NOT NULL"//
+					+ ", AGE VARCHAR(10) NOT NULL);";
+
+			stat.executeUpdate(sql);
+			System.out.println("FIN");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void insertTable() {
 		Connection conn = null;
 		Statement stat = null;
@@ -35,27 +79,13 @@ public class MainJDBC {
 			conn = DriverManager.getConnection(DB_URL, user, pass);
 			stat = conn.createStatement();
 
-			String[] tabMarchand = { "ambr", "anfr", "beba", "emma", "etga", "issa", "jeci", "made", "soka", "thcr", "thde" };
-			String[] tabfruit ={"kiwi","orange","pomme","citron","noisette"};
-			for (int i = 0; i < tabMarchand.length; i++) {
-				String noM=new String();
-				if (i==2) {
-					noM="NiceGuy";
-				}else {
-					noM="Connard";
-				}
+			String[] tabEtudiant = { "ambr", "anfr", "beba", "emma", "etga", "issa", "jeci", "made", "soka", "thcr", "thde" };
+
+			for (int i = 0; i < tabEtudiant.length; i++) {
 				String sql = "INSERT INTO marchand " //
-						+ "(nom, prenom, age)" + "VALUES ('Connard','" + tabMarchand[i] + "','" + i + "');";			
+						+ "(nom, prenom, age)" + "VALUES ('Active','" + tabEtudiant[i] + "','" + i + "');";
+
 				int resultat = stat.executeUpdate(sql);
-				
-				String sql3 = "SELECT ID FROM marchand where prenom='"+tabMarchand[i]+"'; ";
-				ResultSet resultat2 = stat.executeQuery(sql3);
-				resultat2.first();
-				String idM=resultat2.getString("ID");
-				for (int j = 0; j < tabfruit.length; j++) {
-					String sql2 = "INSERT INTO produit " + "(IDMARCHAND, NOMPRODUIT, qteproduit)" + "VALUES ('" +idM + "','" + tabfruit[j] + "',"+Math.random()*250+");";
-					stat.executeUpdate(sql2);
-				}
 				if (resultat >= 0) {
 					System.out.println("OK");
 				} else {
@@ -69,11 +99,7 @@ public class MainJDBC {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	
-	
+
 	public void updateTable() {
 		Connection conn = null;
 		Statement stat = null;
@@ -100,12 +126,8 @@ public class MainJDBC {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	
-	
-	public void create() {
+
+	public void selectTable() {
 		Connection conn = null;
 		Statement stat = null;
 		try {
@@ -113,13 +135,15 @@ public class MainJDBC {
 			conn = DriverManager.getConnection(DB_URL, user, pass);
 			stat = conn.createStatement();
 
-			String sql = "CREATE TABLE produit ( "//
-					+ "IDPRODUIT INTEGER(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, "//
-					+ "IDMARCHAND BIGINT"//
-					+ ", NOMPRODUIT VARCHAR(255) "//
-					+ ", qteproduit INT);";
+			String sql = "SELECT * FROM marchand; ";
+			ResultSet resultat = stat.executeQuery(sql);
+			while (resultat.next()) {
+				String sNom = resultat.getString("nom");
+				String sPrenom = resultat.getString("prenom");
+				String sAge = resultat.getString("age");
+				System.out.println(sNom + " " + sPrenom + " " + sAge);
+			}
 
-			stat.executeUpdate(sql);
 			System.out.println("FIN");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -127,35 +151,8 @@ public class MainJDBC {
 			e.printStackTrace();
 		}
 	}
-	
-	
 
-	public void update() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-//	public void create() {
-//		Connection conn = null;
-//		Statement stat = null;
-//		try {
-//			Class.forName(JDBC_DRIVER);
-//			conn = DriverManager.getConnection(DB_URL, user, pass);
-//			stat = conn.createStatement();
-//
-//			String sql = "CREATE DATABASE marchandphva";
-//
-//			stat.executeUpdate(sql);
-//			System.out.println("FIN");
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-	public void creTable() {
+	public void selectMetaData() {
 		Connection conn = null;
 		Statement stat = null;
 		try {
@@ -163,47 +160,26 @@ public class MainJDBC {
 			conn = DriverManager.getConnection(DB_URL, user, pass);
 			stat = conn.createStatement();
 
-			String sql = "CREATE TABLE marchand ( "//
-					+ "ID BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, "//
-					+ "NOM VARCHAR(30) NOT NULL"//
-					+ ", PRENOM VARCHAR(30) NOT NULL"//
-					+ ", AGE VARCHAR(10) NOT NULL);";
+			String sql = "SELECT * FROM marchand where prenom='ambr'; ";
+			ResultSet resultat = stat.executeQuery(sql);
+			ResultSetMetaData meta = resultat.getMetaData();
+			int nb = meta.getColumnCount();
+			for (int i = 0; i < nb; i++) {
+				System.out.println(meta.getColumnName(i+1)+" "+
+						meta.getColumnTypeName(i+1));
+			}
 
-			stat.executeUpdate(sql);
 			System.out.println("FIN");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	
-	
-
-	public void create2() {
-		Statement stat = null;
-		Connection conn = null;
-		try {
-			Class.forName(JDBC_DRIVER);
-			conn = DriverManager.getConnection(DB_URL, user, pass);
-			stat = conn.createStatement();
-
-			String sql = "CREATE DATABASE Activprov";
-			stat.executeUpdate(sql);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
 
 	public void init() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(JDBC_DRIVER);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -213,18 +189,17 @@ public class MainJDBC {
 			Driver driver = (Driver) e.nextElement();
 			int majorVersion = driver.getMajorVersion();
 			int minorVersion = driver.getMinorVersion();
-			System.out.println("pilote = " + driver.getClass() + " version = " + minorVersion + "  " + majorVersion);
+			System.out.println("Pilote = " + driver.getClass() + " version " + minorVersion + " " + majorVersion);
 			DriverPropertyInfo[] props;
 			try {
 				props = driver.getPropertyInfo("url JDBC", null);
-
 				for (int i = 0; i < props.length; i++) {
 					DriverPropertyInfo prop = props[i];
-					System.out.println("prop = " + prop.name);
+					System.out.println("Prop : " + prop.name);
 					String[] choix = prop.choices;
 					if (choix != null) {
 						for (int j = 0; j < choix.length; j++) {
-							System.out.println("prop choix = " + j + " " + choix[j]);
+							System.out.println("\tprop Choix " + j + " " + choix[j]);
 						}
 					}
 				}
