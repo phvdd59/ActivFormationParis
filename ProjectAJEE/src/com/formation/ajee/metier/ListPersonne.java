e1453519036fba8b539b96763c378afb5ee9ba90
etga <etiennegaspard@hotmail.com>
2016-08-30 11:45:41 +0200

    etga

---
+++ ProjectAJEE/src/com/formation/ajee/metier/ListPersonne.java
@@ -0,0 +1,11 @@
+package com.formation.ajee.metier;
+
+import java.util.ArrayList;
+
+public class ListPersonne extends ArrayList<Personne> {
+	
+	private Personne personne;
+
+	public ListPersonne() {
+	}
+}
