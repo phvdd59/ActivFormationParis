f6916b42afd137ba05d309a601f18822b5ac20da
ambr <ambrault@gmail.com>
2016-08-30 09:49:21 +0200

    ExoAlgo6

---
+++ ProjectActivFormationParis/src/com/formation/ambr/metier/ExoAlgo6New.java
@@ -0,0 +1,76 @@
+package com.formation.ambr.metier;
+
+import com.formation.phva.inter.InterAlgo6;
+
+public class ExoAlgo6New implements InterAlgo6{
+	/**
+	* Chronologiquement le nombre de vente est stocké dans un tableau. 
+	* IKAE souhaite connaîre la période la plus faste des ventes.
+	* Nous avons un tableau tab constitué d'entiers positifs.
+	* Afin de ne pas être affecté par des valeurs ponctuelles anormales 
+	* il sera nécessaire de lisser chaque élement du tableau par ses "n" voisins à gauche 
+	* et à droite les plus proches,  
+	* avant de calculer sur une fenêtre de "f" valeurs correspondantes à une période
+	* celle la plus favorable (ie celle dont la somme la plus importante).
+	* Attention on ne s'intéressera pas aux n premiers éléments et aux n derniers éléments.
+	* 
+	* example :
+	* n=2
+	* f=3
+	*              24                   27                      20
+	*       ----------------     ----------------        ----------------
+	*       |      .       |     |      .       |        |      .       |
+	* tab = [1 ,4 ,7 ,8 ,4 ,3 ,7 ,2 ,8 ,9 ,2 ,6 ,3 ,4 ,1 ,7 ,9 ,1 ,1 ,2 ] 
+	*                             .  .  .                 .  .  .
+	*                             29 28 27                22 19 20         
+	*                            |        |              |        |
+	*                            ----------              ----------
+	*                             .                       .
+	*                             84                      61
+	* 
+	* --> résultat indice :  7 --> valeurs maxi = 84 ([3+7+2+8+9]+[7+2+8+9+2]+[2+8+9+2+6])
+	* 
+	* Donnez le premier indice du tableau "tab" en partant de la gauche 
+	* 	correspondant à la fenêtre "f" la plus favorable avec un lissage de n de part et d'autre.
+	* 	En cas d'impossibilité renvoyer -1 
+	*/
+	
+	public int solution(int[] tab, int n, int f) {
+		int indice = -1;
+		
+		if (tab!=null) {
+			if (tab.length>(n*2)) {
+				int[] tabN = new int[tab.length-(n*2)];
+				if (tabN.length>f) {
+					int[] tabF = new int[tabN.length-f];
+					if (n>=0) {
+						if (f>0) {
+							for (int i = n; i < tab.length-n; i++) {
+								int somme=0;
+								for (int j = (i-n); j <(i+n+1); j++) {
+									somme += tab[j];
+								}
+								tabN[i-n]=somme;
+							}
+							//
+							for (int i = 0; i < tabN.length-f; i++) {
+								int sommeN=0;
+								for (int j = i; j < i+f; j++) {
+									sommeN += tabN[j];
+								}
+								tabF[i]=sommeN;
+							}
+							//
+							for (int i = 0; i < tabF.length; i++) {
+								if (tabF[i]>indice) {
+									indice=tabF[i];
+								}	
+							}
+						}
+					}
+				}
+			}
+		}
+		return indice;
+	}
+}
---
+++ ProjectActivFormationParis/src/com/formation/ambr/metier/MainExoAlgo6.java
@@ -0,0 +1,18 @@
+package com.formation.ambr.metier;
+
+public class MainExoAlgo6 {
+
+	public static void main(String[] args) {
+		MainExoAlgo6 m = new MainExoAlgo6();
+		m.init();
+	}
+
+	private void init() {
+		ExoAlgo6New exo = new ExoAlgo6New();
+		int n = 2;
+		int f = 3;
+		int[] tab = {1 ,4 ,7 ,8 ,4 ,3 ,7 ,2 ,8 ,9 ,2 ,6 ,3 ,4 ,1 ,7 ,9 ,1 ,1 ,2};
+		System.out.println(exo.solution(tab, n, f));
+	}
+
+}
