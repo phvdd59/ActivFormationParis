ada81c2af7a86a302eb56cb779a4435eeade49e0
thde <thomas.delannoy5@gmail.com>
2016-08-31 15:52:27 +0200

    thde

--- ProjectSql/Scripts/THDE/Script.sql
+++ ProjectSql/Scripts/THDE/Script.sql
@@ -2,5 +2,9 @@
 alter table produit add constraint idMFK
 foreign key (idMarchand) references marchand (idMarchand);
 /* ca va lier marchand et produit. Produit doit avoir une idMarchand */
+
 delete marchand where idMarchand = 2;
 /* marche pas si le marchand a des produits */
+
+update marchand set idMarchand = 40 where idMarchand=2;
+/* là encore, marche pas si le marchand a des produits */
