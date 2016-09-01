c0a1d74258d97b244d29ab315bbac6151e00ef13
thde <thomas.delannoy5@gmail.com>
2016-09-01 09:21:05 +0200

    thde

---
+++ ProjectBAIT/src/com/formation/bait/metier/ListeDoc.java
@@ -0,0 +1,7 @@
+package com.formation.bait.metier;
+
+import java.util.ArrayList;
+
+public class ListeDoc extends ArrayList<DocPerso> {
+
+}
--- ProjectSql/Scripts/THDE/Script.sql
+++ ProjectSql/Scripts/THDE/Script.sql
@@ -8,3 +8,13 @@ delete marchand where idMarchand = 2;
 
 update marchand set idMarchand = 40 where idMarchand=2;
 /* là encore, marche pas si le marchand a des produits */
+
+select * from produit as p inner join marchand as m on p.idMarchand = m.idMarchand;
+/* chopper tous les produit qui ont la même ID que le marchand ?*/
+/* outer join aurait chopper tous les marchands sans produits */
+
+select * from produit as p inner join marchand as m on p.idMarchand = m.idMarchand where m.idMarchand=2
+order by p.qteProduit asc limit 5 offset 1; 
+/* dadada on organise */
+
+select p.nomProduit, count (m.agreMarchand)from produit as p inner join marchand as m on (p.idMarchand = m.idMarchand) group by p.idMarchand;
