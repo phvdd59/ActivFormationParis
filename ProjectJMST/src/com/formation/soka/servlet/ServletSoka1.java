92834811405bd6b975a16991004ec30e565330f6
thcr <cresson111@gmail.com>
2016-09-04 18:59:16 +0200

    thcr

--- ProjectJMST/src/com/formation/soka/servlet/ServletSoka1.java
+++ ProjectJMST/src/com/formation/soka/servlet/ServletSoka1.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.formation.joca.controleur.CtrlPersonne;
+import com.formation.thcr.dao.DAOPersonne;
+import com.formation.thcr.metier.ListPersonne;
 import com.formation.thcr.metier.Personne;
 
 /**
@@ -37,39 +39,6 @@ public class ServletSoka1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		File file = new File("C:/DevFormation/" + //
-				"GITActivFormationParis/ProjectJMST/" + //
-				"WebContent/WEB-INF/" + //
-				"page/pageActiveFormulaire_part1_ident_mdp.html");
-		BufferedReader bIn = null;
-		bIn = new BufferedReader(new FileReader(file));
-		String noSerieHtml = request.getParameter("noSerie");
-		String noSerie = (String) session.getAttribute("noSerie");
-
-		if (noSerieHtml.equals(noSerie)) {
-			noSerie = "20_" + noSerie;
-			session.setAttribute("noSerie", noSerie);
-			String line = bIn.readLine();
-			while (line != null) {
-				if (line.contains("%%noSerie%%")) {
-					line.replace("%%noSerie%%", noSerie);
-				}
-				response.getWriter().println(line);
-				line = bIn.readLine();
-			}
-			bIn.close();
-		}
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// String pseudo = request.getParameter("identifiant");
-		// String mdp = request.getParameter("mdp");
-		HttpSession session = request.getSession();
 		
 		Object personne = session.getAttribute("Personne");
 		Personne perso = null;
@@ -81,16 +50,29 @@ public class ServletSoka1 extends HttpServlet {
 		 * RECUP DONNEE FORMULAIRE
 		 */
 		String sIdentifiant = request.getParameter("identifiant");
-		String sMdp = request.getParameter("mdp");
+		String sMdp1 = request.getParameter("mdp1");
+		String sMdp2 = request.getParameter("mdp2");
 		/***************************
 		 * CONTROLE ET CONVERSION
 		 */
 		CtrlPersonne ctrl = new CtrlPersonne();
 //		if(ctrl.ctrlIdentifiant(sIdentifiant) && ctrl.ctrlMdp(sMdp)){
+		if(sMdp1.equals(sMdp2))
 			perso.setIdentifiant(sIdentifiant);
-			perso.setMdp(sMdp);
+			perso.setMdp(sMdp1);
 //		}
 		
+		DAOPersonne dao = new DAOPersonne();
+		ListPersonne list = dao.read();
+		
+		boolean idExist = false;
+		
+		for (Personne p : list) {
+			if(p.getIdentifiant().equals(perso.getIdentifiant())){
+				idExist = true;
+			}
+		}
+			
 		File file = new File("C:/DevFormation/" + //
 				"GITActivFormationParis/ProjectJMST/" + //
 				"WebContent/WEB-INF/" + //
@@ -100,7 +82,7 @@ public class ServletSoka1 extends HttpServlet {
 		String noSerieHtml = request.getParameter("noSerie");
 		String noSerie = (String) session.getAttribute("noSerie");
 
-		if (noSerieHtml.equals(noSerie)) {
+		if (noSerieHtml.equals(noSerie) && !idExist) {
 			noSerie = "21_" + noSerie;
 			session.setAttribute("noSerie", noSerie);
 			String line = bIn.readLine();
@@ -112,6 +94,35 @@ public class ServletSoka1 extends HttpServlet {
 				line = bIn.readLine();
 			}
 			bIn.close();
+		} else if (noSerieHtml.equals(noSerie) && idExist){
+			File file2 = new File("C:/DevFormation/" + //
+					"GITActivFormationParis/ProjectJMST/" + //
+					"WebContent/WEB-INF/" + //
+					"page/pageActiveFormulaire_part1_ident_mdp.html");
+			BufferedReader bIn2 = null;
+			bIn2 = new BufferedReader(new FileReader(file2));
+			String line = bIn2.readLine();
+			while (line != null) {
+				if (line.contains("%%noSerie%%")) {
+					line=line.replace("%%noSerie%%", noSerie);
+				}
+				if (line.contains("name=\"identifiant\"")){
+					line = line.replace("</th>", "<p style=\"color: red;\">login déja existant</p></th>");
+				}
+				response.getWriter().println(line);
+				line = bIn2.readLine();
+			}
+			bIn2.close();
 		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
 	}
 }
