87c35cf0db1ef9b480dbb363756d26b0ad49b2b5
etga <etiennegaspard@hotmail.com>
2016-09-06 15:38:13 +0200

    etga

--- ProjectAJEE/src/com/formation/ajee/servlet/Servletmesdocuments.java
+++ ProjectAJEE/src/com/formation/ajee/servlet/Servletmesdocuments.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.formation.ajee.metier.DocPerso;
+import com.formation.ajee.metier.ListPersonne;
 import com.formation.ajee.metier.ListeDoc;
 import com.formation.ajee.metier.Personne;
 import com.formation.ajee.veriffom.Vue;
@@ -35,10 +36,11 @@ public class Servletmesdocuments extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
-
+		
 		HttpSession session = request.getSession();
 		Object oNoSerie = session.getAttribute("noSerie");
 		//		Object oPseudo = session.getAttribute("pseudo");
+		ListPersonne lstPersonne = new ListPersonne();
 		Personne utilisateur = (Personne) session.getAttribute("utilisateur");
 		Personne personne = (Personne) session.getAttribute("personne");
 		Vue vue = new Vue();
@@ -46,13 +48,53 @@ public class Servletmesdocuments extends HttpServlet {
 		if (oNoSerie != null && personne.getIdentifiant() != null) {
 			if (personne.getIdentifiant().equals("Admin")) {
 				if (utilisateur.equals(null)) {
-					RequestDispatcher rd = request.getRequestDispatcher("//Servletaccueilchargementprofil");
-					rd.forward(request, response);
+					/** Lecture Haut de page HTML */
+					vue.lecturePage(response, "HautPage");
+					vue.lecturePage(response, "JSAccueilAdmin");
+					vue.lecturePage(response, "JSBandeauAdmin");
+					vue.lecturePage(response, "MenuActiv");
+
+					File fileDoc = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/BandeauAdmin.html");
+					BufferedReader bufReadDoc = null;
+					bufReadDoc = new BufferedReader(new FileReader(fileDoc));
+					String lineDoc = bufReadDoc.readLine();
+					while (lineDoc != null) {
+						if (lineDoc.contains("%pseudo%")) {
+							lineDoc = lineDoc.replace("%pseudo%", personne.getIdentifiant());
+						}
+						if (lineDoc.contains("%utilisateur%")) {
+							lineDoc = lineDoc.replace("%utilisateur%", ((utilisateur == null) ? "" : utilisateur.getIdentifiant()));
+						}
+						response.getWriter().println(lineDoc);
+						lineDoc = bufReadDoc.readLine();
+					}
+					bufReadDoc.close();
+
+					File fileDoc1 = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/AccueilAdmin.html");
+					BufferedReader bufReadDoc1 = null;
+					bufReadDoc1 = new BufferedReader(new FileReader(fileDoc1));
+					String lineDoc1 = bufReadDoc1.readLine();
+					while (lineDoc1 != null) {
+						if (lineDoc1.contains("%listepersonne%")) {
+							for (int i = 0; i < lstPersonne.size(); i++) {
+								if (lineDoc1.contains("%listepersonne%") && i < lstPersonne.size() - 1) {
+									lineDoc1 = lineDoc1.replace("%listepersonne%", lstPersonne.get(i).getIdentifiant()) + "<option value=\"%listepersonne%\">";
+								} else if (lineDoc1.contains("%listepersonne%") && i == lstPersonne.size() - 1) {
+									lineDoc1 = lineDoc1.replace("%listepersonne%", lstPersonne.get(i).getIdentifiant());
+								}
+							}
+						}
+						response.getWriter().println(lineDoc1);
+						lineDoc1 = bufReadDoc1.readLine();
+					}
+					bufReadDoc1.close();
+
+					/** Lecture bas de page */
+					vue.lecturePage(response, "BasPage");
 				} else {
 					
 					/** Lecture Haut de page HTML */
 					vue.lecturePage(response, "HautPage");
-					
 				
 					/** Lecture page ActivConsulting */
 					vue.lecturePage(response, "MenuActiv");
