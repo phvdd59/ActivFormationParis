5438495498fd958b217f95c5926db8d30894e96b
Beba <b.basdevant@gmail.com>
2016-09-06 10:00:14 +0200

    maj forualire

--- ProjectBAIT/src/com/formation/bait/servlet/ServletPageRemuneration2.java
+++ ProjectBAIT/src/com/formation/bait/servlet/ServletPageRemuneration2.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.formation.bait.metier.FonctionsCommune;
+import com.formation.bait.metier.Personne;
 
 /**
  * Servlet implementation class Servlet1
@@ -62,6 +63,7 @@ public class ServletPageRemuneration2 extends HttpServlet {
 			File file2 = new File(
 					"C:/DevFormation/GITActivFormationParis/ProjectBAIT/WebContent/WEB-INF/bait/pages/Remuneration.html");
 			BufferedReader bIn2 = null;
+			Personne personne = (Personne) session.getAttribute("Personne");
 			InputStreamReader inputStreamReader2 = null;
 			try {
 				inputStreamReader2 = new InputStreamReader(new FileInputStream(file2), "UTF-8");
@@ -69,7 +71,57 @@ public class ServletPageRemuneration2 extends HttpServlet {
 				String line2 = bIn2.readLine();
 				while (line2 != null) {
 					// System.out.println(line);
+					if (!personne.isCadre()) {
+						line2=line2.replace("type=\"radio\" checked=\"checked\"", "type=\"radio\" checked=\"unchecked\"");
+						
+						line2=line2.replace("checked=\"unchecked\" type=\"radio\"", "checked=\"checked\" type=\"radio\"");
+					
+					}
+					
+					if (!personne.isMutuelle()) {
+						line2=line2.replace("checked=\"unchecked\" type=\"radio\" ", "checked=\"checked\" type=\"radio\" ");
+						
+						line2=line2.replace("type=\"radio\" checked=\"checked\"", "type=\"radio\" checked=\"unchecked\"");
+					
+					}
+					
+					if (!personne.isTicket()) {
+						line2=line2.replace("checked=\"unchecked\" type=\"radio\" ", "checked=\"checked\" type=\"radio\" ");
+						
+						line2=line2.replace("type=\"radio\" checked=\"checked\"", "type=\"radio\" checked=\"unchecked\"");
+					
+					}
+					
+					if (!personne.isTicket()) {
+						line2=line2.replace("checked=\"unchecked\" type=\"radio\" ", "checked=\"checked\" type=\"radio\" ");
+						
+						line2=line2.replace("type=\"radio\" checked=\"checked\"", "type=\"radio\" checked=\"unchecked\"");
+					
+					}
+					
+					line2=line2.replace("%%fonction%%", personne.getFonction());
+					
+					line2=line2.replace("%%position%%", personne.getPosition());
+					
+					line2=line2.replace("%%salaire%%", personne.getSalaire());
+					
+					line2=line2.replace("%%visiteMedicale%%", personne.getVisiteMedicale());
+					
+					line2=line2.replace("%%montantTransport%%", personne.getMontantTransport());
+					
+					line2=line2.replace("%%nbKm%%", personne.getNdKm());
+					
+					line2=line2.replace("%%nbCV%%", personne.getNbCV());
+					
+					line2=line2.replace("%%coefficient%%", personne.getCoeff());
+					
+					
+					
 					response.getWriter().append(line2 + "\n");
+					
+					
+					
+					
 					line2 = bIn2.readLine();
 				}
 			} catch (FileNotFoundException e) {
