ba8873900c1a7c9bd7e0d1480065d25bef25d678
jeci <jeremyciamous@gmail.com>
2016-09-06 12:21:21 +0200

    sqf

--- ProjectAJEE/src/com/formation/ajee/servlet/Admin/Servletaccueilchargementprofil.java
+++ ProjectAJEE/src/com/formation/ajee/servlet/Admin/Servletaccueilchargementprofil.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpSession;
 
 import com.formation.ajee.metier.ListPersonne;
 import com.formation.ajee.metier.Personne;
+import com.formation.ajee.veriffom.Vue;
 
 /**
  * Servlet implementation class Servletjeci1
@@ -40,60 +41,26 @@ public class Servletaccueilchargementprofil extends HttpServlet {
 
 		Object noSerie = session.getAttribute("noSerie");
 
-ListPersonne lstPersonne=new ListPersonne();
-		Personne utilisateur =(Personne) session.getAttribute("utilisateur");
+		ListPersonne lstPersonne = new ListPersonne();
+		Personne utilisateur = (Personne) session.getAttribute("utilisateur");
 		Personne personne = (Personne) session.getAttribute("personne");
-		
-		
+		Vue vue = new Vue();
+
 		if (noSerie != null && personne.getIdentifiant() != null) {
 
 			if (personne.getIdentifiant().equals("Admin")) {
 
 				/** Lecture Haut de page HTML */
-				File fileHaut = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/HautPage.html");
-				BufferedReader bufReadHaut = null;
-				bufReadHaut = new BufferedReader(new FileReader(fileHaut));
-				String lineHaut = bufReadHaut.readLine();
-				while (lineHaut != null) {
-					if (lineHaut.contains("<title>Espace Personnel ActivConsulting</title>")) {
-						lineHaut = lineHaut.replace("<title>Espace Personnel ActivConsulting</title>", "<title>Chargement</title>");
-					}
-					response.getWriter().println(lineHaut);
-					lineHaut = bufReadHaut.readLine();
-				}
-				bufReadHaut.close();
+				vue.lecturePage(response, "HautPage");
 
 				/** A modifier seulement si notre page contient du JavaScript */
-				File fileJS = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/js/JSAccueilAdmin.html");
-				BufferedReader bufReadJS = null;
-				bufReadJS = new BufferedReader(new FileReader(fileJS));
-				String lineJS = bufReadJS.readLine();
-				while (lineJS != null) {
-					response.getWriter().println(lineJS);
-					lineJS = bufReadJS.readLine();
-				}
-				bufReadJS.close();
 
-				File fileJS1 = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/js/JSBandeauAdmin.html");
-				BufferedReader bufReadJS1 = null;
-				bufReadJS1 = new BufferedReader(new FileReader(fileJS1));
-				String lineJS1 = bufReadJS1.readLine();
-				while (lineJS1 != null) {
-					response.getWriter().println(lineJS1);
-					lineJS1 = bufReadJS1.readLine();
-				}
-				bufReadJS1.close();
+				vue.lecturePage(response, "JSAccueilAdmin");
+
+				vue.lecturePage(response, "JSBandeauAdmin");
 
 				/** Lecture page ActivConsulting */
-				File fileActiv = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/MenuActiv.html");
-				BufferedReader bufReadActiv = null;
-				bufReadActiv = new BufferedReader(new FileReader(fileActiv));
-				String lineActiv = bufReadActiv.readLine();
-				while (lineActiv != null) {
-					response.getWriter().println(lineActiv);
-					lineActiv = bufReadActiv.readLine();
-				}
-				bufReadActiv.close();
+				vue.lecturePage(response, "MenuActiv");
 
 				/**
 				 * Seule Partie qui va vraiment changer selon les pages (penser
@@ -120,55 +87,33 @@ ListPersonne lstPersonne=new ListPersonne();
 				bufReadDoc1 = new BufferedReader(new FileReader(fileDoc1));
 				String lineDoc1 = bufReadDoc1.readLine();
 				while (lineDoc1 != null) {
-					if (lineDoc1.contains("%listepersonne%")){
-					for (int i = 0; i <lstPersonne.size(); i++) {
-						if (lineDoc1.contains("%listepersonne%")&&i<lstPersonne.size()-1) {
-							lineDoc1 = lineDoc1.replace("%listepersonne%", lstPersonne.get(i).getIdentifiant())+"<option value=\"%listepersonne%\">";
-						}else if (lineDoc1.contains("%listepersonne%")&&i==lstPersonne.size()-1) {
-							lineDoc1 = lineDoc1.replace("%listepersonne%", lstPersonne.get(i).getIdentifiant());
+					if (lineDoc1.contains("%listepersonne%")) {
+						for (int i = 0; i < lstPersonne.size(); i++) {
+							if (lineDoc1.contains("%listepersonne%") && i < lstPersonne.size() - 1) {
+								lineDoc1 = lineDoc1.replace("%listepersonne%", lstPersonne.get(i).getIdentifiant()) + "<option value=\"%listepersonne%\">";
+							} else if (lineDoc1.contains("%listepersonne%") && i == lstPersonne.size() - 1) {
+								lineDoc1 = lineDoc1.replace("%listepersonne%", lstPersonne.get(i).getIdentifiant());
+							}
 						}
-					}}
+					}
 					response.getWriter().println(lineDoc1);
 					lineDoc1 = bufReadDoc1.readLine();
 				}
 				bufReadDoc1.close();
 
 				/** Lecture bas de page */
-				File fileBas = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/BasPage.html");
-				BufferedReader bufReadBas = null;
-				bufReadBas = new BufferedReader(new FileReader(fileBas));
-				String lineBas = bufReadBas.readLine();
-				while (lineBas != null) {
-					response.getWriter().println(lineBas);
-					lineBas = bufReadBas.readLine();
-				}
-				bufReadBas.close();
+				vue.lecturePage(response, "BasPage");
+
 			}
 
-			else if (personne.getIdentifiant().isEmpty()==false) {
+			else if (personne.getIdentifiant().isEmpty() == false) {
 				/** Lecture Haut de page HTML */
-				File fileHaut = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/HautPage.html");
-				BufferedReader bufReadHaut = null;
-				bufReadHaut = new BufferedReader(new FileReader(fileHaut));
-				String lineHaut = bufReadHaut.readLine();
-				while (lineHaut != null) {
-					response.getWriter().println(lineHaut);
-					lineHaut = bufReadHaut.readLine();
-				}
-				bufReadHaut.close();
+				vue.lecturePage(response, "HautPage");
 
 				/** A modifier seulement si notre page contient du JavaScript */
 
 				/** Lecture page ActivConsulting */
-				File fileActiv = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/MenuActiv.html");
-				BufferedReader bufReadActiv = null;
-				bufReadActiv = new BufferedReader(new FileReader(fileActiv));
-				String lineActiv = bufReadActiv.readLine();
-				while (lineActiv != null) {
-					response.getWriter().println(lineActiv);
-					lineActiv = bufReadActiv.readLine();
-				}
-				bufReadActiv.close();
+				vue.lecturePage(response, "MenuActiv");
 
 				/**
 				 * Seule Partie qui va vraiment changer selon les pages (penser
@@ -187,26 +132,10 @@ ListPersonne lstPersonne=new ListPersonne();
 				}
 				bufReadDoc.close();
 
-				File fileDoc1 = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/Accueil.html");
-				BufferedReader bufReadDoc1 = null;
-				bufReadDoc1 = new BufferedReader(new FileReader(fileDoc1));
-				String lineDoc1 = bufReadDoc1.readLine();
-				while (lineDoc1 != null) {
-					response.getWriter().println(lineDoc1);
-					lineDoc1 = bufReadDoc1.readLine();
-				}
-				bufReadDoc1.close();
+				vue.lecturePage(response, "Accueil");
 
 				/** Lecture bas de page */
-				File fileBas = new File("C:/DevFormation/GITActivFormationParis/ProjectAJEE/WebContent/ajee/page1/BasPage.html");
-				BufferedReader bufReadBas = null;
-				bufReadBas = new BufferedReader(new FileReader(fileBas));
-				String lineBas = bufReadBas.readLine();
-				while (lineBas != null) {
-					response.getWriter().println(lineBas);
-					lineBas = bufReadBas.readLine();
-				}
-				bufReadBas.close();
+				vue.lecturePage(response, "BasPage");
 			}
 
 		} else {
