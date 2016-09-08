534576593d81410a6eea265064b9a79cd84f5099
jeci <jeremyciamous@gmail.com>
2016-09-08 10:12:32 +0200

    ol

---
+++ ProjectAJEE/src/com/formation/ajee/servlet/serviceweb/ServletBait.java
@@ -0,0 +1,43 @@
+package com.formation.ajee.servlet.serviceweb;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.formation.ajee.metier.ListPersonne;
+
+/**
+ * Servlet implementation class ServletBait
+ */
+@WebServlet("/ServletBait")
+public class ServletBait extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletBait() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ListPersonne lstPersonne=new ListPersonne();
+		
+	}
+
+}
