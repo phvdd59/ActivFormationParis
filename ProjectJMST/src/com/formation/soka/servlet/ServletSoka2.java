f53590bb5d13d56a1414fbaa294aeb40e2bd4891
soka <souad.kadri1@gmail.com>
2016-09-08 16:35:45 +0200

    ok

--- ProjectJMST/src/com/formation/soka/servlet/ServletSoka2.java
+++ ProjectJMST/src/com/formation/soka/servlet/ServletSoka2.java
@@ -35,8 +35,7 @@ public class ServletSoka2 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
 		ConversionPersonne convPers = new ConversionPersonne();
@@ -53,52 +52,56 @@ public class ServletSoka2 extends HttpServlet {
 		/***************************
 		 * RECUP DONNEE FORMULAIRE
 		 */
-		if (perso.getPrenom().equals("")) {
-			String sNom = request.getParameter("nom");
-			String sPrenom = request.getParameter("prenom");
-			String sAdresse = request.getParameter("adresse");
-			String sTelFixe = request.getParameter("telfixe");
-			String sTelPort = request.getParameter("telportable");
-			String sFax = request.getParameter("fax");
-			String sCp = request.getParameter("codepostale");
-			String sVille = request.getParameter("ville");
-			String sEmail = request.getParameter("mail");
-			/***************************
-			 * CONTROLE ET CONVERSION
-			 */
-			CtrlPersonne ctrl = new CtrlPersonne();
+		if (noSerieHtml != null || noSerieHtml.equals(noSerie)) {
+			
+			try {
+				if (perso.getPrenom()!=null && perso.getPrenom().equals("")) {
+					String sNom = request.getParameter("nom");
+					String sPrenom = request.getParameter("prenom");
+					String sAdresse = request.getParameter("adresse");
+					String sTelFixe = request.getParameter("telfixe");
+					String sTelPort = request.getParameter("telportable");
+					String sFax = request.getParameter("fax");
+					String sCp = request.getParameter("codepostale");
+					String sVille = request.getParameter("ville");
+					String sEmail = request.getParameter("mail");
+					/***************************
+					 * CONTROLE ET CONVERSION
+					 */
+					CtrlPersonne ctrl = new CtrlPersonne();
 
-			if (ctrl.ctrlNom(sNom) && ctrl.ctrlPrenom(sPrenom) && ctrl.ctrlAdresse(sAdresse)
-					&& ctrl.ctrlTelFixe(sTelFixe) && ctrl.ctrlTelPort(sTelPort) && ctrl.ctrlCp(sCp)
-					&& ctrl.ctrlVille(sVille) && ctrl.ctrlFax(sFax) && ctrl.ctrlEmail(sEmail)) {
-				perso.setNom(sNom);
-				perso.setPrenom(sPrenom);
-				perso.setAdresse(sAdresse);
-				perso.setTelFixe(sTelFixe);
-				perso.setTelPort(sTelPort);
-				perso.setFax(sFax);
-				perso.setCp(sCp);
-				perso.setVille(sVille);
-				perso.setEmail(sEmail);
-			} else {
-				// JSP DECO
+					if (ctrl.ctrlNom(sNom) && ctrl.ctrlPrenom(sPrenom) && ctrl.ctrlAdresse(sAdresse) && ctrl.ctrlTelFixe(sTelFixe) && ctrl.ctrlTelPort(sTelPort) && ctrl.ctrlCp(sCp) && ctrl.ctrlVille(sVille) && ctrl.ctrlFax(sFax) && ctrl.ctrlEmail(sEmail)) {
+						perso.setNom(sNom);
+						perso.setPrenom(sPrenom);
+						perso.setAdresse(sAdresse);
+						perso.setTelFixe(sTelFixe);
+						perso.setTelPort(sTelPort);
+						perso.setFax(sFax);
+						perso.setCp(sCp);
+						perso.setVille(sVille);
+						perso.setEmail(sEmail);
+					} else {
+						// JSP DECO
+					}
+				} else {
+					request.getRequestDispatcher("/ServletDeco").forward(request, response);
+				}
+			} catch (Exception e) {
+				request.getRequestDispatcher("/ServletDeco").forward(request, response);
 			}
-		}
 
-		File file = new File("C:/DevFormation/" + //
-				"GITActivFormationParis/ProjectJMST/" + //
-				"WebContent/WEB-INF/" + //
-				"page/pageActiveFormulaire_part3_etat_civil.html");
-		BufferedReader bIn = null;
-		bIn = new BufferedReader(new FileReader(file));
+			File file = new File("C:/DevFormation/" + //
+					"GITActivFormationParis/ProjectJMST/" + //
+					"WebContent/WEB-INF/" + //
+					"page/pageActiveFormulaire_part3_etat_civil.html");
+			BufferedReader bIn = null;
+			bIn = new BufferedReader(new FileReader(file));
 
-		if (noSerieHtml == null || noSerie == null || noSerieHtml.equals(noSerie)) {
 			session.setAttribute("noSerie", noSerie);
 			String line = bIn.readLine();
 			while (line != null) {
 				line = line.replace("%%noSerie%%", noSerie);
-				line = line.replace("%%datenaissance%%",
-						convPers.conversionSQLToUtil(new java.sql.Date(perso.getDateNaissance().getTime())));
+				line = line.replace("%%datenaissance%%", convPers.conversionSQLToUtil(new java.sql.Date(perso.getDateNaissance().getTime())));
 				line = line.replace("%%lieunaissance%%", perso.getLieuNaissance());
 				line = line.replace("%%numsecu%%", perso.getNumSecu());
 				line = line.replace("%%nationalite%%", perso.getNationalite());
@@ -136,8 +139,7 @@ public class ServletSoka2 extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
