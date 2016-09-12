36ae6959d69ed5074e1f067bf6e14a5f9316e445
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-09-12 12:03:09 +0200

    spring

---
+++ ProjectSpringSimple/src/com/formation/phva/beans/Developpeur.java
@@ -0,0 +1,20 @@
+package com.formation.phva.beans;
+
+public class Developpeur {
+
+	private String nom;
+	private int anneesExperience;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getAnneesExperience() {
+		return anneesExperience;
+	}
+	public void setAnneesExperience(int anneesExperience) {
+		this.anneesExperience = anneesExperience;
+	}
+	
+}
