f2de93d6b50d5712e3b8c84ab3ee6bf2f00fbea2
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-09-14 07:20:29 +0200

    conflit

--- ProjectSql/Scripts/Script.sql
+++
@@ -1,8 +0,0 @@
-UPDATE marchand set nomMarchand="ambr", ageMarchand=5 , ageMarchand=3 WHERE idMarchand=1;
-
-UPDATE marchand set nomMarchand="ambr", ageMarchand=5 , ageMarchand=3 WHERE idMarchand=1;
-
-use listeUser;
-UPDATE listUser set ticketResto=0, admin=1 WHERE identifiant=53;
-
-UPDATE listeUser set identifiant='vddph', mdp='vdd000', email='philippe.vandendriessche59@gmail.com', nom='Vandendriessche', prenom='Philippe', adresse='rue du d'haluin', cp='59200', ville='Tourcoing', telFixe='0320275032', telPort='0602050402', fax='', dateNaissance='08/07/1970', lieuNaissance='', numSecu='', Nationalite='', situation='salarie', fonction='Chef de Projet', positionEntreprise='', cadre='0', coeff='', salaire='', visiteMedicale='', montantTransport='', voiture='0', nbCV='', nbKm='', mutuelle='0', ticketResto='0', admin='0', dateCreation='', dateModification='', bloque='0', raisonBlocage='' WHERE IDPersonne=56;
